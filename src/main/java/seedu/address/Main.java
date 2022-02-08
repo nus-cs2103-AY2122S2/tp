c4e34df8b0b6f878b6d8f8db91e7e0e88ac9e0e2
ddx-510 <dai.tianle@dhs.sg>
2022-02-08 21:01:27 +0800

    Dummy commit

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -21,5 +21,5 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(MainApp.class, args);
-    }
+    } // dummy commit
 }
