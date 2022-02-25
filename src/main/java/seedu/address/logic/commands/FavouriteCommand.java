a8441a9ff6f8eef2295be7625ea1a0eee8ce4d8c
hsiaojietng <hsiaojietng@gmail.com>
2022-02-25 15:20:14 +0800

    Favourite command's result reflected in the app

--- src/main/java/seedu/address/logic/commands/FavouriteCommand.java
+++ src/main/java/seedu/address/logic/commands/FavouriteCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Favourite;
 import seedu.address.model.person.Person;
 
 /**
@@ -22,7 +24,8 @@ public class FavouriteCommand extends Command {
             + "by the index number used in the last client listing. "
             + "Parameters: INDEX (must be a positive integer) "
             + "Example: " + COMMAND_WORD + " 1 ";
-    public static final String MESSAGE_FAVOURITE_PERSON_SUCCESS = "Favourited Client: %1$s";
+    public static final String MESSAGE_FAVOURITE_PERSON_SUCCESS =
+            "Favourited Client! Check your favourites list by navigating to it from the toolbar!";
 
     public FavouriteCommand(Index index) {
         requireAllNonNull(index);
@@ -38,8 +41,24 @@ public class FavouriteCommand extends Command {
         }
 
         Person personToFavourite = lastShownList.get(index.getZeroBased());
-        //model.setPerson(personToFavourite, favouritedPerson);
-        return new CommandResult(String.format(MESSAGE_FAVOURITE_PERSON_SUCCESS, personToFavourite));
+        Person favouritedPerson = new Person(personToFavourite.getName(), personToFavourite.getPhone(), personToFavourite.getEmail(),
+                new Favourite(true), personToFavourite.getAddress(), personToFavourite.getTags());
+        model.setPerson(personToFavourite, favouritedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(generateSuccessMessage(favouritedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether
+     * the client is favourited or not
+     * {@code personToFavourite}.
+     */
+    private String generateSuccessMessage(Person personToFavourite) {
+        String message = "";
+        if (personToFavourite.getFavourite().toString().equals("favourited")) {
+            message = MESSAGE_FAVOURITE_PERSON_SUCCESS;
+        }
+        return String.format(message);
     }
 
     @Override
