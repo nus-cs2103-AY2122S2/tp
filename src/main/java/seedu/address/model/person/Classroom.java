8c2d6660f9aa89ec0356087bdc730430d171658e
Joshua <e0544333@u.nus.edu>
2022-02-27 14:07:54 +0800

    Add classroom class to models
    - Added 2 template methods that follows the model implementation

---
+++ src/main/java/seedu/address/model/person/Classroom.java
@@ -0,0 +1,30 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Student's classroom in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidClassroom(String)}
+ */
+public class Classroom {
+
+
+    /**
+     * Constructs a {@code Classroom}.
+     *
+     * @param classroom A valid classroom.
+     */
+    public Classroom(String classroom) {
+        requireNonNull(classroom);
+//        checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
+//        value = phone;
+    }
+
+    /**
+     * Returns true if a given string is a valid classroom.
+     */
+    public static boolean isValidClassroom(String test) {
+        return true;
+    }
+}
