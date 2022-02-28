72cf039cd4a3d4d3cb663f2e8fb0214d026594d7
cwq2326 <cwq2326@gmail.com>
2022-02-28 19:18:57 +0800

    Add documentation to UiAlert class and add header message as a parameter to the makeConfirmationAlert method

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -15,7 +15,8 @@ import seedu.address.ui.UiAlert;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_CONFIRM = "Are you sure you want to clear all of Tracey's content?";
+    public static final String MESSAGE_ALERT_TITLE = "You are about to clear Tracey's data";
+    public static final String MESSAGE_ALERT_CONFIRM = "Are you sure you want to clear all of Tracey's content?";
     public static final String MESSAGE_SUCCESS = "Tracey has been cleared!";
     public static final String MESSAGE_UNSUCCESS = "Clear command cancelled";
 
@@ -23,7 +24,7 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        Alert alert = UiAlert.makeConfirmationAlert(MESSAGE_CONFIRM);
+        Alert alert = UiAlert.makeConfirmationAlert(MESSAGE_ALERT_TITLE, MESSAGE_ALERT_CONFIRM);
         if (alert.getResult() == ButtonType.OK) {
             model.setAddressBook(new AddressBook());
             return new CommandResult(MESSAGE_SUCCESS);
