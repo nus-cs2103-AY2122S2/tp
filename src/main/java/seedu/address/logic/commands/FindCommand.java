76d71f06ccdf95469287363d040680548437c690
janald99 <e0543657@u.nus.edu>
2022-03-02 17:00:35 +0800

    Change the Find Commange Message displayed to user when invalid find format is used

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -21,17 +21,16 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+//    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+//            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+//            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+//            + "Example: " + COMMAND_WORD + " alice bob charlie";
 
-    /*
-    private final NameContainsKeywordsPredicate namePredicate;
-    private final EmailContainsKeywordsPredicate emailPredicate;
-    private final AddressContainsKeywordsPredicate addressPredicate;
-    private final PhoneContainsKeywordsPredicate PhonePredicate;
-     */
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all clients whose fields contain the specified " +
+     "keywords in the specified fields, and displays them as a list with index numbers.\n"
+     + "Parameters: PREFIX KEYWORD [MORE_KEYWORDS]...\n"
+     + "Example: " + COMMAND_WORD + " n/ john";
+
     private final Predicate<Person> predicate;
 
 
