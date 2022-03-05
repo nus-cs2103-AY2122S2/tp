565590f77a6ec6ac949ab337c4787b1f750597f1
wei jun ang <ang.weijun1999@gmail.com>
2022-03-05 23:20:22 +0800

    Add DuplicatedShowException and ShowNotFoundException
    This will help us to catch certain runtime errors that may occur.
    1. DuplicatedShowException is thrown when there is a duplicated shows.
    2. ShowNotFoundException is thrown when no show can be found.

---
+++ src/main/java/seedu/address/model/person/exceptions/DuplicatedShowException.java
@@ -0,0 +1,8 @@
+package seedu.address.model.person.exceptions;
+
+public class DuplicatedShowException {
+
+    public DuplicatedShowException() {
+        super("Operation would result in duplicate shows");
+    }
+}
