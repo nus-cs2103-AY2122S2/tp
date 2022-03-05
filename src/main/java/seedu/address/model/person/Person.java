f0045429cb949fada3a8a6c98d93ea9b9b6161b7
bryans17 <>
2022-03-05 15:40:58 +0800

    Add Telegram attribute to Person.java methods

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -38,6 +38,7 @@ public class Person {
         this.address = address;
         this.tags.addAll(tags);
         this.githubUsername = githubUsername;
+        this.telegram = telegram;
     }
 
     public Name getName() {
@@ -54,6 +55,10 @@ public class Person {
 
     public GithubUsername getGithubUsername() {
         return githubUsername;
+    }
+
+    public Telegram getTelegram() {
+        return telegram;
     }
 
     public Address getAddress() {
@@ -101,13 +106,14 @@ public class Person {
                 && otherPerson.getEmail().equals(getEmail())
                 && otherPerson.getAddress().equals(getAddress())
                 && otherPerson.getTags().equals(getTags())
-                && otherPerson.getGithubUsername().equals(getGithubUsername());
+                && otherPerson.getGithubUsername().equals(getGithubUsername())
+                && otherPerson.getTelegram().equals(getTelegram());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags, githubUsername);
+        return Objects.hash(name, phone, email, address, tags, githubUsername, telegram);
     }
 
     @Override
@@ -121,7 +127,9 @@ public class Person {
                 .append("; Address: ")
                 .append(getAddress())
                 .append("; Github: ")
-                .append(getGithubUsername());
+                .append(getGithubUsername())
+                .append("; Telegram: ")
+                .append(getTelegram());
 
         Set<Tag> tags = getTags();
         if (!tags.isEmpty()) {
