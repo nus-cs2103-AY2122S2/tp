c46f64afdcd84a4becf102d02cc735ccae8a9e8a
DavidTan0527 <david_weiucheng@hotmail.com>
2022-03-06 13:58:06 +0800

    Recover original AddressBook, add IBook class

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -5,16 +5,16 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import javafx.collections.ObservableList;
-import seedu.address.model.product.Product;
-import seedu.address.model.product.UniqueProductList;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
 
 /**
  * Wraps all data at the address-book level
- * Duplicates are not allowed (by .isSameProduct comparison)
+ * Duplicates are not allowed (by .isSamePerson comparison)
  */
 public class AddressBook implements ReadOnlyAddressBook {
 
-    private final UniqueProductList products;
+    private final UniquePersonList persons;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -24,13 +24,13 @@ public class AddressBook implements ReadOnlyAddressBook {
      *   among constructors.
      */
     {
-        products = new UniqueProductList();
+        persons = new UniquePersonList();
     }
 
     public AddressBook() {}
 
     /**
-     * Creates an AddressBook using the Products in the {@code toBeCopied}
+     * Creates an AddressBook using the Persons in the {@code toBeCopied}
      */
     public AddressBook(ReadOnlyAddressBook toBeCopied) {
         this();
@@ -40,11 +40,11 @@ public class AddressBook implements ReadOnlyAddressBook {
     //// list overwrite operations
 
     /**
-     * Replaces the contents of the product list with {@code products}.
-     * {@code products} must not contain duplicate products.
+     * Replaces the contents of the person list with {@code persons}.
+     * {@code persons} must not contain duplicate persons.
      */
-    public void setProducts(List<Product> products) {
-        this.products.setProducts(products);
+    public void setPersons(List<Person> persons) {
+        this.persons.setPersons(persons);
     }
 
     /**
@@ -53,68 +53,68 @@ public class AddressBook implements ReadOnlyAddressBook {
     public void resetData(ReadOnlyAddressBook newData) {
         requireNonNull(newData);
 
-        setProducts(newData.getProductList());
+        setPersons(newData.getPersonList());
     }
 
-    //// product-level operations
+    //// person-level operations
 
     /**
-     * Returns true if a product with the same identity as {@code product} exists in the address book.
+     * Returns true if a person with the same identity as {@code person} exists in the address book.
      */
-    public boolean hasProduct(Product product) {
-        requireNonNull(product);
-        return products.contains(product);
+    public boolean hasPerson(Person person) {
+        requireNonNull(person);
+        return persons.contains(person);
     }
 
     /**
-     * Adds a product to the address book.
-     * The product must not already exist in the address book.
+     * Adds a person to the address book.
+     * The person must not already exist in the address book.
      */
-    public void addProduct(Product p) {
-        products.add(p);
+    public void addPerson(Person p) {
+        persons.add(p);
     }
 
     /**
-     * Replaces the given product {@code target} in the list with {@code editedProduct}.
+     * Replaces the given person {@code target} in the list with {@code editedPerson}.
      * {@code target} must exist in the address book.
-     * The product identity of {@code editedProduct} must not be the same as another existing product in the address book.
+     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
      */
-    public void setProduct(Product target, Product editedProduct) {
-        requireNonNull(editedProduct);
+    public void setPerson(Person target, Person editedPerson) {
+        requireNonNull(editedPerson);
 
-        products.setProduct(target, editedProduct);
+        persons.setPerson(target, editedPerson);
     }
 
     /**
      * Removes {@code key} from this {@code AddressBook}.
      * {@code key} must exist in the address book.
      */
-    public void removeProduct(Product key) {
-        products.remove(key);
+    public void removePerson(Person key) {
+        persons.remove(key);
     }
 
     //// util methods
 
     @Override
     public String toString() {
-        return products.asUnmodifiableObservableList().size() + " products";
+        return persons.asUnmodifiableObservableList().size() + " persons";
         // TODO: refine later
     }
 
     @Override
-    public ObservableList<Product> getProductList() {
-        return products.asUnmodifiableObservableList();
+    public ObservableList<Person> getPersonList() {
+        return persons.asUnmodifiableObservableList();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof AddressBook // instanceof handles nulls
-                && products.equals(((AddressBook) other).products));
+                && persons.equals(((AddressBook) other).persons));
     }
 
     @Override
     public int hashCode() {
-        return products.hashCode();
+        return persons.hashCode();
     }
 }
--- src/main/java/seedu/address/model/product/UniqueProductList.java
+++ src/main/java/seedu/address/model/product/UniqueProductList.java
@@ -1,22 +1,22 @@
 package seedu.address.model.product;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.product.exceptions.DuplicateProductException;
 import seedu.address.model.product.exceptions.ProductNotFoundException;
 
-import java.util.Iterator;
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
 /**
  * A list of products that enforces uniqueness between its elements and does not allow nulls.
- * A product is considered unique by comparing using {@code Product#isSameProduct(Product)}. As such, adding and updating of
- * products uses Product#isSameProduct(Product) for equality so as to ensure that the product being added or updated is
- * unique in terms of identity in the UniqueProductList. However, the removal of a product uses Product#equals(Object) so
- * as to ensure that the product with exactly the same fields will be removed.
+ * A product is considered unique by comparing using {@code Product#isSameProduct(Product)}. As such, adding and
+ * updating of products uses Product#isSameProduct(Product) for equality as to ensure that the product being added or
+ * updated is unique in terms of identity in the UniqueProductList. However, the removal of a product uses
+ * Product#equals(Object) as to ensure that the product with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
