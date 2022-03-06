a9a04f7e20d5c79816ddbd0dd475a35ee7c37ad3
Lai Mei Tin <e0550504@u.nus.edu>
2022-03-06 18:17:43 +0800

    Update Storage interface

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -6,13 +6,14 @@ import java.util.Optional;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyIBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 
 /**
  * API of the Storage component
  */
-public interface Storage extends AddressBookStorage, UserPrefsStorage {
+public interface Storage extends AddressBookStorage, IBookStorage, UserPrefsStorage {
 
     @Override
     Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException;
@@ -20,13 +21,23 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
     @Override
     void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException;
 
+    // TODO : delete start
     @Override
     Path getAddressBookFilePath();
-
+    
     @Override
     Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException;
 
     @Override
     void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+    // TODO : delete end
+    
+    @Override
+    Path getIBookFilePath();
 
+    @Override
+    Optional<ReadOnlyIBook> readIBook() throws DataConversionException, IOException;
+
+    @Override
+    void saveIBook(ReadOnlyIBook iBook) throws IOException;
 }
