9df5d16ec862c0fdf7c97eb0129679ae9312551d
channne <cccx2303@gmail.com>
2022-03-06 18:44:00 +0800

    More edits to tests

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -67,9 +67,9 @@ public class NameContainsKeywordsPredicateTest {
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("Carol"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
 
-        // Keywords match phone, email and address, but does not match name
-        predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
-        assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345")
-                .withEmail("alice@email.com").withAddress("Main Street").build()));
+        // Keywords match telegram, email and github, but does not match name
+        predicate = new NameContainsKeywordsPredicate(Arrays.asList("alice_P", "alice@email.com", "aliceP"));
+        assertFalse(predicate.test(new PersonBuilder().withName("Alice").withTelegram("alice_P")
+                .withEmail("alice@email.com").withGithub("aliceP").build()));
     }
 }
