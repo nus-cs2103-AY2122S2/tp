364ce5eaeeb7de48de51a2342d9f8e7357570b1d
wei jun ang <ang.weijun1999@gmail.com>
2022-03-06 23:29:43 +0800

    Move exception to under show package
    Move both exceptions into their a new package under exceptions

--- src/main/java/seedu/address/model/show/exceptions/DuplicatedShowException.java
+++ src/main/java/seedu/address/model/show/exceptions/DuplicatedShowException.java
@@ -1,6 +1,6 @@
-package seedu.address.model.person.exceptions;
+package seedu.address.model.show.exceptions;
 
-public class DuplicatedShowException {
+public class DuplicatedShowException extends RuntimeException {
 
     public DuplicatedShowException() {
         super("Operation would result in duplicate shows");
