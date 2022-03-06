65c289b231327d7396ec6576d24a485d206cadf7
jxt00 <e0556267@u.nus.edu>
2022-03-06 16:04:22 +0800

    Add list class to manage Class Groups
    Add some exception classes to handle invalid and duplicate
    class groups

---
+++ src/main/java/seedu/address/model/classgroup/UniqueClassGroupList.java
@@ -0,0 +1,119 @@
+package seedu.address.model.classgroup;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.classgroup.exceptions.ClassGroupNotFoundException;
+import seedu.address.model.classgroup.exceptions.DuplicateClassGroupException;
+
+/**
+ * A list of class groups that enforces uniqueness between its elements and does not allow nulls.
+ * A class group is considered unique by comparing using {@code ClassGroup#equals(ClassGroup)}.
+ * As such, adding and updating of class groups uses ClassGroup#equals(ClassGroup) for equality so as to
+ * ensure that the class group being added or updated is unique in terms of identity in the UniqueClassGroupList.
+ * However, the removal of a class group uses ClassGroup#equals(Object) so as to ensure that
+ * the class group with exactly the same fields will be removed.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see ClassGroup#equals(Object)
+ */
+public class UniqueClassGroupList implements Iterable<ClassGroup> {
+
+    private final ObservableList<ClassGroup> internalList = FXCollections.observableArrayList();
+    private final ObservableList<ClassGroup> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent class group as the given argument.
+     */
+    public boolean contains(ClassGroup toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::equals);
+    }
+
+    /**
+     * Adds a class group to the list.
+     * The class group must not already exist in the list.
+     */
+    public void add(ClassGroup toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateClassGroupException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Removes the equivalent class group from the list.
+     * The class group must exist in the list.
+     */
+    public void remove(int index) {
+        requireNonNull(index);
+        if (index < 1 || index >= internalList.size()) {
+            throw new ClassGroupNotFoundException();
+        }
+        internalList.remove(index - 1);
+    }
+
+    public void setClassGroups(UniqueClassGroupList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code classGroups}.
+     * {@code classGroups} must not contain duplicate class groups.
+     */
+    public void setClassGroups(List<ClassGroup> classGroups) {
+        requireAllNonNull(classGroups);
+        if (!lessonsAreUnique(classGroups)) {
+            throw new DuplicateClassGroupException();
+        }
+
+        internalList.setAll(classGroups);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<ClassGroup> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<ClassGroup> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof UniqueClassGroupList
+                && internalList.equals(((UniqueClassGroupList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code classGroups} contains only unique class groups.
+     */
+    private boolean lessonsAreUnique(List<ClassGroup> classGroups) {
+        for (int i = 0; i < classGroups.size() - 1; i++) {
+            for (int j = i + 1; j < classGroups.size(); j++) {
+                if (classGroups.get(i).equals(classGroups.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/seedu/address/model/classgroup/exceptions/ClassGroupNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.classgroup.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified class group.
+ */
+public class ClassGroupNotFoundException extends RuntimeException {
+}
