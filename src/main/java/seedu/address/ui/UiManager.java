c4054895b8e812c13cbbcfe25cb858f7f53a3512
lkldev <lkldev@outlook.com>
2022-03-08 04:25:02 +0800

    Update MainApp to use modified Logic and Model
    Logic still contain traces of AB3 (getFilteredPersonList method),
    to be removed once UI does not need this metho

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.StringUtil;
-import seedu.address.logic.AB3Logic;
+import seedu.address.logic.Logic;
 
 /**
  * The manager of the UI component.
@@ -22,13 +22,13 @@ public class UiManager implements Ui {
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
     private static final String ICON_APPLICATION = "/images/address_book_32.png";
 
-    private AB3Logic logic;
+    private Logic logic;
     private MainWindow mainWindow;
 
     /**
      * Creates a {@code UiManager} with the given {@code Logic}.
      */
-    public UiManager(AB3Logic logic) {
+    public UiManager(Logic logic) {
         this.logic = logic;
     }
 
