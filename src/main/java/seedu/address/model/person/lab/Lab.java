48baab51dd7e76fc19162c3f833c2f2889542bd6
bryans17 <>
2022-03-07 18:20:13 +0800

    Add gradeLab and submitLab functions

--- src/main/java/seedu/address/model/person/lab/Lab.java
+++ src/main/java/seedu/address/model/person/lab/Lab.java
@@ -3,10 +3,14 @@ package seedu.address.model.person.lab;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import seedu.address.model.person.exceptions.LabAlreadyGradedException;
+import seedu.address.model.person.exceptions.LabAlreadySubmittedException;
+import seedu.address.model.person.exceptions.LabNotSubmittedException;
+
 
 /**
  * Represents a Lab entry in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidLab(String)}
+ * Is valid as declared in {@link #isValidLab(String)}
  */
 public class Lab {
 
@@ -20,18 +24,18 @@ public class Lab {
 
     public final int labNumber;
 
-    private final LabStatus labStatus;
+    private LabStatus labStatus;
 
     /**
      * Constructs an {@code Lab}.
      *
-     * @param labNumber A valid address.
+     * @param labNumber A valid lab number.
      */
     public Lab(String labNumber) {
         requireNonNull(labNumber);
         checkArgument(isValidLab(labNumber), MESSAGE_CONSTRAINTS);
         this.labNumber = Integer.parseInt(labNumber);
-        this.labStatus = LabStatus.UNSUBMITTED; // labStatus is always initialized to {@code LabStatus.UNSUBMITTED}
+        setLabStatus(LabStatus.UNSUBMITTED); // labStatus is always initialized to {@code LabStatus.UNSUBMITTED}
     }
 
     /**
@@ -39,6 +43,36 @@ public class Lab {
      */
     public static boolean isValidLab(String test) {
         return test.matches(VALIDATION_REGEX);
+    }
+
+    private void setLabStatus(LabStatus labStatus) {
+        this.labStatus = labStatus;
+    }
+
+    /**
+     * Sets the lab status of the lab to {@code LabStatus.SUBMITTED}
+     */
+    public void submitLab() {
+        if (this.labStatus == LabStatus.SUBMITTED) {
+            throw new LabAlreadySubmittedException(labNumber);
+        }
+
+        setLabStatus(LabStatus.GRADED);
+    }
+
+    /**
+     * Sets the lab status of the lab to {@code LabStatus.GRADED}
+     */
+    public void gradeLab() {
+        if (this.labStatus == LabStatus.UNSUBMITTED) {
+            throw new LabNotSubmittedException(labNumber);
+        }
+
+        if (this.labStatus == LabStatus.GRADED) {
+            throw new LabAlreadyGradedException(labNumber);
+        }
+
+        setLabStatus(LabStatus.GRADED);
     }
 
     @Override
