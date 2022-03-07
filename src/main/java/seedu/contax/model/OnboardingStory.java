f2f8f843529ca8da57c3983896732139cd8ad246
Wei-Xinn <E0544325@u.nus.edu>
2022-03-08 02:14:39 +0800

    Add OnboardingStory class

---
+++ src/main/java/seedu/contax/model/OnboardingStory.java
@@ -0,0 +1,23 @@
+package seedu.contax.model;
+
+import java.util.ArrayList;
+
+public class OnboardingStory {
+    ArrayList<OnboardingStep> story;
+
+    public OnboardingStory() {
+        story = new ArrayList<>();
+    }
+
+    public void addStory(OnboardingStep os) {
+        story.add(os);
+    }
+
+    public OnboardingStep getStep(int index) {
+        return story.get(index);
+    }
+
+    public int getSize() {
+        return story.size();
+    }
+}
