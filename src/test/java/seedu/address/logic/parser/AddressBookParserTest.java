a03989b7b59139cbd5a3b90792dfb7ea47754fb8
redpelican2108 <xinwei2108@gmail.com>
2022-03-07 20:12:13 +0800

    Adhere checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -93,6 +93,5 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_unknownCommand_throwsParseException() {
         assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND, () -> parser.parseCommand("unknownCommand"));
-    }
-     */
+    }*/
 }
