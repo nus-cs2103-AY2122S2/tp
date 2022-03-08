f29d40c4ee1f5c73e2b48a5acf8d997f49f2d605
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-03-08 20:39:50 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
@@ -14,7 +13,6 @@ import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.event.Event;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
 /**
