f1ae82f5807142d434886030545dc7a893dd22f9
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-03-08 13:49:01 +0800

    Add skeleton for Event

---
+++ src/main/java/seedu/address/model/event/Event.java
@@ -0,0 +1,62 @@
+package seedu.address.model.event;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import seedu.address.model.person.Name;
+
+public class Event {
+
+    // Identity fields
+    private final Name name;
+
+
+    // Data fields
+    private final LocalDateTime localDateTime;
+    private final String description;
+
+    public Event(Name name, String description, LocalDateTime localDateTime) {
+        requireAllNonNull(name, localDateTime);
+        this.name = name;
+        this.localDateTime = localDateTime;
+        this.description = description;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Event)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+        return otherEvent.getName().equals(getName())
+                && otherEvent.getLocalDateTime().equals(getLocalDateTime());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, localDateTime);
+    }
+}
