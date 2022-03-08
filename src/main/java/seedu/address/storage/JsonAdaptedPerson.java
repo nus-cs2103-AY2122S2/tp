6d2c2df0a9006257cdd5d0365e2f4ebf0be0a6c0
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-08 20:35:13 +0800

    Modify JasonAdaptedPerson.java to reflect information for student academic major

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -27,7 +27,7 @@ class JsonAdaptedPerson {
     private final String name;
     private final String phone;
     private final String email;
-    private final String address;
+    private final String academicMajor;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
 
     /**
@@ -35,12 +35,12 @@ class JsonAdaptedPerson {
      */
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-            @JsonProperty("email") String email, @JsonProperty("address") String address,
+            @JsonProperty("email") String email, @JsonProperty("academic major") String academicMajor,
             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
         this.name = name;
         this.phone = phone;
         this.email = email;
-        this.address = address;
+        this.academicMajor = academicMajor;
         if (tagged != null) {
             this.tagged.addAll(tagged);
         }
@@ -53,7 +53,7 @@ class JsonAdaptedPerson {
         name = source.getName().fullName;
         phone = source.getPhone().value;
         email = source.getEmail().value;
-        address = source.getAcademicMajor().value;
+        academicMajor = source.getAcademicMajor().value;
         tagged.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
@@ -94,16 +94,16 @@ class JsonAdaptedPerson {
         }
         final Email modelEmail = new Email(email);
 
-        if (address == null) {
+        if (academicMajor == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, AcademicMajor.class.getSimpleName()));
         }
-        if (!AcademicMajor.isValidAcademicMajor(address)) {
+        if (!AcademicMajor.isValidAcademicMajor(academicMajor)) {
             throw new IllegalValueException(AcademicMajor.MESSAGE_CONSTRAINTS);
         }
-        final AcademicMajor modelAddress = new AcademicMajor(address);
+        final AcademicMajor modelAcademicMajor = new AcademicMajor(academicMajor);
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        return new Person(modelName, modelPhone, modelEmail, modelAcademicMajor, modelTags);
     }
 
 }
