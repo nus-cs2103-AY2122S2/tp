4a8acee1e32df9e5b4327b90247e8a22cfa72079
DannyDakota <51451719+DannyDakota@users.noreply.github.com>
2022-03-08 23:19:47 +0800

    Update helpWindow by showcasing possible command format and examples

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -15,11 +15,70 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
+    public static final String USERGUIDE_URL = "https://ay2122s2-cs2103t-t12-3.github.io/tp/UserGuide.html";
+    public static final String NEWLINE_AND_INDENTATION = "\n     ";
+    public static final String HELP_MESSAGE = "Tracey supports the following features: Find, Add, Delete, Edit, Clear, "
+            + "Summarise, List, Help and Filter.\n"
+            + "This guide aims to showcase the syntax used for the aforementioned features.\n"
+            + "For a full comprehensive guide, please refer to " + USERGUIDE_URL
+            + "\n"
+
+            + "\n1. FIND a particular contact in Tracey."
+            + NEWLINE_AND_INDENTATION
+            + "Format: find NAME"
+            + NEWLINE_AND_INDENTATION
+            + "Example 1: find John\n"
+
+            + "\n2. ADD a student with relevant details into Tracey."
+            + NEWLINE_AND_INDENTATION
+            + "Format: add n/NAME p/PHONENUMBER e/EMAIL ..."
+            + NEWLINE_AND_INDENTATION
+            + "Example 1: add n/Melvin f/SOC cs/ Negative"
+            + NEWLINE_AND_INDENTATION
+            + "Example 2: add e/student69@u.nus.edu n/ Martin\n"
+
+            + "\n3.DELETE a contact at a specific INDEX. The index refers to the index number shown in the "
+            + "displayed person list."
+            + NEWLINE_AND_INDENTATION
+            + "Format: delete INDEX"
+            + NEWLINE_AND_INDENTATION
+            + "Example 1: Delete 2"
+            + NEWLINE_AND_INDENTATION
+            + "Example 2: Delete 4\n"
+
+            + "\n4. EDIT the person at the specified INDEX. The index refers to the index number shown in the "
+            + "displayed person list."
+            + NEWLINE_AND_INDENTATION
+            + "Format: edit INDEX n/NAME ..."
+            + NEWLINE_AND_INDENTATION
+            + "Example 1: edit 1 p/91234567 e/johndoe@example.com"
+            + NEWLINE_AND_INDENTATION
+            + "Example 2: edit 2 n/David Limpeh t/\n"
+
+            + "\n5. CLEAR all the data inside Tracey"
+            + NEWLINE_AND_INDENTATION
+            + "Format: clear\n"
+
+            + "\n6. SUMMARISE the record inside Tracey that an overview of the data,"
+            + " such as number of students who are covid-positive"
+            + NEWLINE_AND_INDENTATION
+            + "Format:summarise record\n"
+
+            + "\n7. LIST the full record, which displays all the student’s data that are logged into Tracey"
+            + NEWLINE_AND_INDENTATION
+            + "Format: list\n"
+
+            + "\n8. Display syntax examples through HELP"
+            + NEWLINE_AND_INDENTATION
+            + "Format: help\n"
+
+            + "\n9. FILTER student based on health statuses"
+            + NEWLINE_AND_INDENTATION
+            + "Format: filter covid-positive";
+
 
     @FXML
     private Button copyButton;
