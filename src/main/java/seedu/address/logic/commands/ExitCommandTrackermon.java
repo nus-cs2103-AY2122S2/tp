84b16fb504d5cd1af956129736eafdf142254c96
Zachary Chan <trollinsir@hotmail.com>
2022-03-08 23:27:05 +0800

    Modify Exit, Help, and List Commands
    Modified AB3 commands by duplicating them and renaming the
    duplicates to have "Trackermon" postfix.

--- src/main/java/seedu/address/logic/commands/ExitCommandTrackermon.java
+++ src/main/java/seedu/address/logic/commands/ExitCommandTrackermon.java
@@ -1,4 +1,19 @@
 package seedu.address.logic.commands;
 
-public class ExitCommandTrackermon {
+import seedu.address.model.ModelTrackermon;
+
+/**
+ * Terminates Trackermon.
+ */
+public class ExitCommandTrackermon extends CommandTrackermon {
+
+    public static final String COMMAND_WORD = "exit";
+
+    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Trackermon as requested ...";
+
+    @Override
+    public CommandResultTrackermon execute(ModelTrackermon model) {
+        return new CommandResultTrackermon(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+    }
+
 }
--- src/main/java/seedu/address/logic/commands/ListCommandTrackermon.java
+++ src/main/java/seedu/address/logic/commands/ListCommandTrackermon.java
@@ -1,4 +1,24 @@
 package seedu.address.logic.commands;
 
-public class ListCommandTrackermon {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.ModelTrackermon.PREDICATE_SHOW_ALL_SHOWS;
+
+import seedu.address.model.ModelTrackermon;
+
+/**
+ * Lists all shows in Trackermon to the user.
+ */
+public class ListCommandTrackermon extends CommandTrackermon {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_SUCCESS = "Listed all shows";
+
+
+    @Override
+    public CommandResultTrackermon execute(ModelTrackermon model) {
+        requireNonNull(model);
+        model.updateFilteredShowList(PREDICATE_SHOW_ALL_SHOWS);
+        return new CommandResultTrackermon(MESSAGE_SUCCESS);
+    }
 }
