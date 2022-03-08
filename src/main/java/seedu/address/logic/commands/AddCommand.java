7da1f4b1faf0f52b98818f1fde8fab0dff4bcb06
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-08 18:44:09 +0800

    First commit

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Adds a person to the address book.
+ * Adds a student contact to ArchDuke.
  */
 public class AddCommand extends Command {
 
