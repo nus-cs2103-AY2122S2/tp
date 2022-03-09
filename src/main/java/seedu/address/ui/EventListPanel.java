1124a91f942a95d1164812f1afbdf1095cd39ae4
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-03-09 23:54:34 +0800

    Checkstyle changes along with reviewer comments

--- src/main/java/seedu/address/ui/EventListPanel.java
+++ src/main/java/seedu/address/ui/EventListPanel.java
@@ -11,7 +11,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.event.Event;
 
 /**
- * Panel containing the list of persons.
+ * Panel containing the list of Events.
  */
 public class EventListPanel extends UiPart<Region> {
     private static final String FXML = "EventListPanel.fxml";
@@ -21,7 +21,7 @@ public class EventListPanel extends UiPart<Region> {
     private ListView<Event> eventListView;
 
     /**
-     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     * Creates a {@code EventListPanel} with the given {@code ObservableList}.
      */
     public EventListPanel(ObservableList<Event> eventList) {
         super(FXML);
@@ -30,9 +30,9 @@ public class EventListPanel extends UiPart<Region> {
     }
 
     /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     * Custom {@code ListCell} that displays the graphics of a {@code Event} using a {@code EventCard}.
      */
-    class EventListViewCell extends ListCell<Event> {
+    private class EventListViewCell extends ListCell<Event> {
         @Override
         protected void updateItem(Event event, boolean empty) {
             super.updateItem(event, empty);
