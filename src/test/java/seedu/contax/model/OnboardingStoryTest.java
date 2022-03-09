77c9cc996df8897e9682d30442ba4fb20f555d82
Wei-Xinn <E0544325@u.nus.edu>
2022-03-09 15:17:09 +0800

    Add new line for import

--- src/test/java/seedu/contax/model/OnboardingStoryTest.java
+++ src/test/java/seedu/contax/model/OnboardingStoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.contax.model.onboarding.OnboardingStep;
 import seedu.contax.model.onboarding.OnboardingStory;
 
