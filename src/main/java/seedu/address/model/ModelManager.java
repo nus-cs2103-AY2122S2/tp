37ae6fd6c8347973353936c71023f7ff468bfa5f
Saravanan Anuja Harish <77202887+Harish-coding@users.noreply.github.com>
2022-03-10 12:23:02 +0800

    Updated ModelManager.java
    Changed the implementation of sortFilteredPersonList method.
    
    Co-authored-by: Brandon <46317008+brelkh@users.noreply.github.com>

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -130,7 +130,7 @@ public class ModelManager implements Model {
 
     @Override
     public void sortFilteredPersonList() {
-        this.addressBook.sortPersonList();
+        addressBook.sortPersonList();
     }
 
     @Override
