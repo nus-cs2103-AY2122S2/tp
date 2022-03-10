43b7454c3e55ec0b7e4bef98b5bd43a51940f72c
SethCKL <chongkokleong1998@gmail.com>
2022-03-10 23:41:55 +0800

    Update respective classes to correct checkstyle errors

--- src/main/java/seedu/address/model/applicant/UniquePersonList.java
+++ src/main/java/seedu/address/model/applicant/UniquePersonList.java
@@ -13,10 +13,10 @@ import seedu.address.model.applicant.exceptions.PersonNotFoundException;
 
 /**
  * A list of persons that enforces uniqueness between its elements and does not allow nulls.
- * A applicant is considered unique by comparing using {@code Applicant#isSamePerson(Applicant)}. As such, adding and updating of
- * persons uses Applicant#isSamePerson(Applicant) for equality so as to ensure that the applicant being added or updated is
- * unique in terms of identity in the UniquePersonList. However, the removal of a applicant uses Applicant#equals(Object) so
- * as to ensure that the applicant with exactly the same fields will be removed.
+ * A applicant is considered unique by comparing using {@code Applicant#isSamePerson(Applicant)}. As such, adding and
+ * updating of persons uses Applicant#isSamePerson(Applicant) for equality so as to ensure that the applicant being
+ * added or updated is unique in terms of identity in the UniquePersonList. However, the removal of a applicant uses
+ * Applicant#equals(Object) so as to ensure that the applicant with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
