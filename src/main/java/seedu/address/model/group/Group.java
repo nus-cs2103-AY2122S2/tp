65119546f9e155cfb486f7a8e9705b9f4d8e7fcd
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-10 20:31:58 +0800

    Create a new GroupName class to represent a group name

--- src/main/java/seedu/address/model/group/Group.java
+++ src/main/java/seedu/address/model/group/Group.java
@@ -1,4 +1,13 @@
 package seedu.address.model.group;
 
+import seedu.address.model.person.Name;
+
+/**
+ * Represents a Group in ArchDuke.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Group {
+
+    // Identity fields
+    private final GroupName groupName;
 }
