467dbb0d45195e34c44b1789d726a387619dcba5
Bryan Ong <68813082+likeabowx@users.noreply.github.com>
2022-03-11 21:56:33 +0800

    Update DeleteApplicantCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/DeleteApplicantCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteApplicantCommandParserTest.java
@@ -29,7 +29,7 @@ public class DeleteApplicantCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser,FLAG_APPLICANT + "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, 
-                DeleteCommand.MESSAGE_USAGE));
+        assertParseFailure(parser,FLAG_APPLICANT + "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT
+                                                                      , DeleteCommand.MESSAGE_USAGE));
     }
 }
