8894f3129ac1de7cf274246f3897ac2387a398ba
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-11 12:14:11 +0800

    Modify address book to contain groups

--- src/test/java/seedu/address/testutil/TypicalGroups.java
+++ src/test/java/seedu/address/testutil/TypicalGroups.java
@@ -1,8 +1,31 @@
 package seedu.address.testutil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.group.Group;
+
 /**
  * A utility class containing a list of {@code Group} objects to be used in tests.
  */
 public class TypicalGroups {
-    
+    public static final Group NUS_FINTECH_SOCIETY = new GroupBuilder().withGroupName("NUS Fintech Society").build();
+    public static final Group RVRC_RUNNERS = new GroupBuilder().withGroupName("RVRC Runners").build();
+
+    /**
+     * Returns an {@code AddressBook} with all the typical groups.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Group group : getTypicalGroups()) {
+            ab.addGroup(group);
+        }
+        return ab;
+    }
+
+    public static List<Group> getTypicalGroups() {
+        return new ArrayList<>(Arrays.asList(NUS_FINTECH_SOCIETY, RVRC_RUNNERS));
+    }
 }
