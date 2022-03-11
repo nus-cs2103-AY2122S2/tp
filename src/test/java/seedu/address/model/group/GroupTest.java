25bcee9edf6a8acec17f4b0ded1761992ebf5e7c
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-11 11:44:06 +0800

    Add TypicalGroups class to contain Group objects to be used in tests

--- src/test/java/seedu/address/model/group/GroupTest.java
+++ src/test/java/seedu/address/model/group/GroupTest.java
@@ -1,4 +1,17 @@
 package seedu.address.model.group;
 
+
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
+import seedu.address.testutil.GroupBuilder;
+
+
 public class GroupTest {
+    @Test
+    public void isSameGroup() {
+
+    }
 }
---
+++ src/test/java/seedu/address/testutil/TypicalGroups.java
@@ -0,0 +1,8 @@
+package seedu.address.testutil;
+
+/**
+ * A utility class containing a list of {@code Group} objects to be used in tests.
+ */
+public class TypicalGroups {
+    
+}
