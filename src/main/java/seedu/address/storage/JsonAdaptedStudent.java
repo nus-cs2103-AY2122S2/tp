765a6d933511e2e0e0825a5db005dedf3ea0b727
wxliong <39647325+wxliong@users.noreply.github.com>
2022-03-12 16:04:50 +0800

    Create JsonAdaptedStudent for storage component

---
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -0,0 +1,89 @@
+package seedu.address.storage;
+
+import java.util.Optional;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.StudentId;
+import seedu.address.model.student.Email;
+import seedu.address.model.student.Name;
+import seedu.address.model.student.Student;
+import seedu.address.model.student.Telegram;
+
+/**
+ * Jackson-friendly version of {@link Student}.
+ */
+class JsonAdaptedStudent {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Student's %s field is missing!";
+
+    private final String studentId;
+    private final String name;
+    private final String email;
+    private final String telegram;
+
+    /**
+     * Constructs a {@code JsonAdaptedStudent} with the given student details.
+     */
+    @JsonCreator
+    public JsonAdaptedStudent(@JsonProperty("studentId") String studentId, @JsonProperty("name") String name,
+                              @JsonProperty("email") String email, @JsonProperty("telegram") String telegram) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.telegram = telegram;
+    }
+
+    /**
+     * Converts a given {@code Student} into this class for Jackson use.
+     */
+    public JsonAdaptedStudent(Student source) {
+        studentId = source.getStudentId().value;
+        name = source.getName().fullName;
+        email = source.getEmail().value;
+        telegram = String.valueOf(source.getTelegram());
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted student object into the model's {@code Student} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted student.
+     */
+    public Student toModelType() throws IllegalValueException {
+
+        if (studentId == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentId.class.getSimpleName()));
+        }
+        if (!StudentId.isValidStudentId(studentId)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final StudentId modelStudentId = new StudentId(studentId);
+
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        if (email == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+        }
+        if (!Email.isValidEmail(email)) {
+            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+        }
+        final Email modelEmail = new Email(email);
+
+        if (!Telegram.isValidTelegram(telegram)) {
+            throw new IllegalValueException(Telegram.MESSAGE_CONSTRAINTS);
+        }
+        final Telegram modelTelegram = new Telegram(telegram);
+
+        return new Student(modelStudentId, modelName, modelEmail, Optional.of(modelTelegram));
+    }
+
+}
+
