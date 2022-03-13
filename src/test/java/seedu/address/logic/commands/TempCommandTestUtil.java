50d8f2c023e86508f0302ad7440b70df09c82ea3
weijunang <weijun.ang99@gmail.com>
2022-03-13 21:00:06 +0800

    Add ShowBuilder, TypicalShows and Modified CommandTestUtil Class
    ShowBuilder, TypicalShows and CommandTestUtil is used to aid in the testing of other class.
    TempCommandTestUtil is added for further reference.

--- src/test/java/seedu/address/logic/commands/TempCommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/TempCommandTestUtil.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+//package seedu.address.logic.commands;
 //
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,17 +17,7 @@ package seedu.address.logic.commands;
 ///**
 // * Contains helper methods for testing commands.
 // */
-public class CommandTestUtil {
-    
-    public static final String VALID_NAME_YOU = "You";
-    public static final String VALID_NAME_ME = "ME";
-    
-    public static final String VALID_STATUS_COMPLETED = "completed";
-    public static final String VALID_STATUS_WATCHING = "watching";
-
-    public static final String VALID_TAG_MOVIE = "movie";
-    public static final String VALID_TAG_SERIES = "series";
-    
+//public class CommandTestUtil {
 //
 //    public static final String VALID_NAME_AMY = "Amy Bee";
 //    public static final String VALID_NAME_BOB = "Bob Choo";
@@ -37,6 +27,8 @@ public class CommandTestUtil {
 //    public static final String VALID_EMAIL_BOB = "bob@example.com";
 //    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
 //    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+//    public static final String VALID_TAG_HUSBAND = "husband";
+//    public static final String VALID_TAG_FRIEND = "friend";
 //
 //    public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
 //    public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
@@ -122,4 +114,4 @@ public class CommandTestUtil {
 //        assertEquals(1, model.getFilteredPersonList().size());
 //    }
 //
-}
+//}
