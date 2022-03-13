8cf3106b6fc57de4c65fe6ebad6ead8abc3a902b
nnmoq <77339343+nnmoq@users.noreply.github.com>
2022-03-13 14:58:17 +0800

    Remove support for duplicate prefixes
    Having duplicate prefixes may confuse the user as they might not remember that the newest prefix of the same type overrides the older one. Thus, removing this functionality may improve user experience.

--- src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -1,17 +1,22 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_TAG;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import seedu.address.logic.parser.exceptions.ParseException;
+
 /**
  * Stores mapping of prefixes to their respective arguments.
- * Each key may be associated with multiple argument values.
+ * Each key may be associated with only one argument value except for tags which can be associated with
+ * multiple argument values.
  * Values for a given key are stored in a list, and the insertion ordering is maintained.
- * Keys are unique, but the list of argument values may contain duplicate argument values, i.e. the same argument value
- * can be inserted multiple times for the same prefix.
+ * Keys are unique, but the list of argument values for tags may contain duplicate argument values,
+ * i.e. the same argument value can be inserted multiple times for tags.
  */
 public class ArgumentMultimap {
 
@@ -20,14 +25,17 @@ public class ArgumentMultimap {
 
     /**
      * Associates the specified argument value with {@code prefix} key in this map.
-     * If the map previously contained a mapping for the key, the new value is appended to the list of existing values.
+     * If the map previously contained a mapping for the key, a ParseException is thrown when the prefix is not a tag.
      *
      * @param prefix   Prefix key with which the specified argument value is to be associated
      * @param argValue Argument value to be associated with the specified prefix key
      */
-    public void put(Prefix prefix, String argValue) {
+    public void put(Prefix prefix, String argValue) throws ParseException {
         List<String> argValues = getAllValues(prefix);
         argValues.add(argValue);
+        if (!prefix.equals(PREFIX_STUDENT_TAG) && argMultimap.containsKey(prefix)) {
+            throw new ParseException(String.format("Duplicate prefix %s is not allowed", prefix.getPrefix()));
+        }
         argMultimap.put(prefix, argValues);
     }
 
