c195cec66d0a604fd9964cfe4cd8755e5234fb3b
wei jun ang <ang.weijun1999@gmail.com>
2022-03-14 03:13:44 +0800

    Refractor CommandTestUtil
    Deleted unnecessary commented out import statements

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -1,27 +1,16 @@
 package seedu.address.logic.commands;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-//import static seedu.address.testutil.Assert.assertThrows;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//import seedu.address.commons.core.index.Index;
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.Model;
-//import seedu.address.testutil.EditPersonDescriptorBuilder;
-//
+
 /**
  * Contains helper methods for testing commands.
  */
+
 public class CommandTestUtil {
+
     public static final String VALID_NAME_YOU = "You";
     public static final String VALID_NAME_ME = "ME";
     public static final String VALID_STATUS_COMPLETED = "completed";
     public static final String VALID_STATUS_WATCHING = "watching";
     public static final String VALID_TAG_MOVIE = "movie";
     public static final String VALID_TAG_SERIES = "series";
+
 }
