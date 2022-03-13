e8053e4fd9d929a7e1ba4ab317572ad62c412946
Alfred Koh <alfredkohkj@gmail.com>
2022-03-14 04:30:29 +0800

    Updated Json, to store all needed fields for Tasks

--- src/main/java/manageezpz/storage/JsonAdaptedTask.java
+++ src/main/java/manageezpz/storage/JsonAdaptedTask.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import manageezpz.commons.exceptions.IllegalValueException;
+import manageezpz.model.task.Deadline;
 import manageezpz.model.task.Description;
+import manageezpz.model.task.Event;
 import manageezpz.model.task.Task;
+import manageezpz.model.task.Todo;
+
 /**
  * Jackson-friendly version of {@link Task}.
  */
@@ -15,22 +19,37 @@ class JsonAdaptedTask {
 
     private final String description;
     private final String type;
+    private final String date;
+    private final String time;
+    private final String startTime;
+    private final String endTime;
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedTask(@JsonProperty("type") String type, @JsonProperty("description") String description) {
+    public JsonAdaptedTask(@JsonProperty("type") String type, @JsonProperty("description") String description,
+    @JsonProperty("date") String date, @JsonProperty("time") String time,
+                           @JsonProperty("startTime") String startTime, @JsonProperty("endTime") String endTime) {
         this.description = new Description(description).toString();
         this.type = type;
+        this.date = date;
+        this.time = time;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     /**
      * Converts a given {@code Task} into this class for Jackson use.
      */
+    // Note to self: Do I need to convert String to LocalTime, so that toModelType can be able to create the task?
     public JsonAdaptedTask(Task source) {
-        description = source.getDescription().toString();
-        type = source.getType();
+        description = source.getDescription().toString(); // Generally for all tasks
+        type = source.getType(); // Generally for all tasks
+        this.date = "Placeholder for source.getDate()"; // For Deadline
+        this.time = "Placeholder for source.getTime()"; // For Deadline
+        this.startTime = "Placeholder for source.getEndTime()"; // For Event
+        this.endTime = "Placeholder for source.getEndTime()"; // For Event
     }
 
     /**
@@ -42,6 +61,14 @@ class JsonAdaptedTask {
         if (description == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Task.class.getSimpleName()));
         }
-        return new Task(new Description(description));
+        Description desc = new Description(description);
+        if(type.equals("todo")) {
+            return new Todo(desc);
+        } else if(type.equals("deadline")) {
+ //           return new Deadline(desc, datetime,datetime);
+        } else if(type.equals("event")) {
+//                return new Event(desc,startTime, endTime);
+        }
+        return new Todo(desc);
     }
 }
