3b9b108a11cd8d41cef7ab3c03cd7b34b700d676
khoahre123 <e0550384@u.nus.edu>
2022-03-14 04:00:36 +0800

    Fix checkstyle problem when refractor

--- src/main/java/seedu/address/logic/commands/help/DetailHelpCommand.java
+++ src/main/java/seedu/address/logic/commands/help/DetailHelpCommand.java
@@ -3,7 +3,7 @@ package seedu.address.logic.commands.help;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
-public class DetailHelpCommand extends HelpCommand{
+public class DetailHelpCommand extends HelpCommand {
     @Override
     public CommandResult execute(Model model) {
         return null;
--- src/main/java/seedu/address/logic/commands/help/OverallHelpCommand.java
+++ src/main/java/seedu/address/logic/commands/help/OverallHelpCommand.java
@@ -3,7 +3,7 @@ package seedu.address.logic.commands.help;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
-public class OverallHelpCommand extends HelpCommand{
+public class OverallHelpCommand extends HelpCommand {
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
