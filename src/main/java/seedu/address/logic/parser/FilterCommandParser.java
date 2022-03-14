d187b92cef0f244c31f53061533bfa5c1ebc78ee
RYAN <geetanteng93@gmail.com>
2022-03-14 18:09:17 +0800

    Remove redundant components on FilterCommand

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,33 +1,17 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddLabCommand;
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.lab.Lab;
 import seedu.address.model.person.lab.LabStatus;
 import seedu.address.model.person.lab.StudentHasLabPredicate;
 
-import java.util.Hashtable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.*;
 
 public class FilterCommandParser implements Parser<FilterCommand> {
-
-    private static final Pattern LAB_SPECIFICATION_FORMAT = Pattern.compile("l/(?<lab>\\d)\\s+s/(?<labStatus>\\w+)");
-
-    /**
-     * Identifies possible {@code userInputs} that indicates
-     */
-    private static final Hashtable<String, String> LAB_STATUS_IDENTIFIER = new Hashtable<>() {{
-        put("g", "GRADED");
-        put("s", "SUBMITTED");
-        put("u", "UNSUBMITTED");
-    }};
 
     /**
      * Parses the given {@code String} of arguments in the context of the FilterCommand
