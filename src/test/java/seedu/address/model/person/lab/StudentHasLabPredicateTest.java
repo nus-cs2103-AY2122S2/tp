350a6f6cfab1a8c69072e789a86a563f9f70c8e0
RYAN <geetanteng93@gmail.com>
2022-03-15 02:50:04 +0800

    Add StudentHasLabPredicateTest

---
+++ src/test/java/seedu/address/model/person/lab/StudentHasLabPredicateTest.java
@@ -0,0 +1,55 @@
+package seedu.address.model.person.lab;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.testutil.PersonBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StudentHasLabPredicateTest {
+
+    @Test
+    public void equals() {
+        Lab firstLab = (new Lab("1")).thatIs(LabStatus.SUBMITTED);
+        Lab secondLab = (new Lab("2")).thatIs(LabStatus.GRADED);
+
+        StudentHasLabPredicate firstPredicate = new StudentHasLabPredicate(firstLab);
+        StudentHasLabPredicate secondPredicate = new StudentHasLabPredicate(secondLab);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        StudentHasLabPredicate firstPredicateCopy = new StudentHasLabPredicate(firstLab);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_studentHasSpecifiedLab_returnsTrue() {
+        StudentHasLabPredicate predicate = new StudentHasLabPredicate((new Lab("1").thatIs(LabStatus.UNSUBMITTED)));
+        assertTrue(predicate.test(new PersonBuilder().withLabs("1", "2", "3").build()));
+    }
+
+    @Test
+    public void test_studentDoesNotHaveSpecifiedLab_returnsFalse() {
+        // Wrong LabStatus
+        StudentHasLabPredicate predicate = new StudentHasLabPredicate((new Lab("1").thatIs(LabStatus.SUBMITTED)));
+        assertFalse(predicate.test(new PersonBuilder().withLabs("1", "2", "3").build()));
+
+        // Wrong LabNumber
+        predicate = new StudentHasLabPredicate((new Lab("4").thatIs(LabStatus.UNSUBMITTED)));
+        assertFalse(predicate.test(new PersonBuilder().withLabs("1", "2", "3").build()));
+        
+    }
+
+}
