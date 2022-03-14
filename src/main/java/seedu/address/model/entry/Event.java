068c1065cf66f400c18dad3c62d5d2b7e65af01c
Albert Sutiono <albertsutz123@gmail.com>
2022-03-14 21:39:07 +0800

    Create basic Event class

---
+++ src/main/java/seedu/address/model/entry/Event.java
@@ -0,0 +1,106 @@
+package seedu.address.model.entry;
+
+import seedu.address.model.tag.Tag;
+
+
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+public class Event extends Entry {
+
+    //Identity fields
+    private final String company;
+
+    // Data fields
+    private final String date;
+    private final String time;
+    private final String platform;
+
+    public Event(Name name, String company, String date, String time, String platform, Set<Tag> tags) {
+        super(name, tags);
+        requireAllNonNull(company, date, time, platform);
+        this.company = company;
+        this.date = date;
+        this.time = time;
+        this.platform = platform;
+    }
+
+    public String getCompany() {
+        return this.company;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public String getPlatform() {
+        return this.platform;
+    }
+
+    @Override
+    public boolean isSameEntry(Entry otherEntry) {
+        if(otherEntry == this) {
+            return true;
+        }
+
+        if(!(otherEntry instanceof Event)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) otherEntry;
+        return otherEvent.getName().equals(getName());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == this) {
+            return true;
+        }
+
+        if(!(other instanceof Event)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+        return otherEvent.getName().equals(getName())
+                && otherEvent.getCompany().equals(getCompany())
+                && otherEvent.getDate().equals(getDate())
+                && otherEvent.getTime().equals(getTime())
+                && otherEvent.getPlatform().equals(getPlatform())
+                && otherEvent.getTags().equals(getTags());
+    }
+
+    @Override
+    public int hashCode() {
+        // Use all Event's attributes to get a unique hashCode
+        return Objects.hash(getName(), company, date, time, platform, getTags());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Company: ")
+                .append(getCompany())
+                .append("; Date: ")
+                .append(getDate())
+                .append("; Time: ")
+                .append(getTime())
+                .append("; Platform: ")
+                .append(getPlatform());
+
+        Set<Tag> tags = getTags();
+        if(!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+
+        return builder.toString();
+    }
+}
