bae7247625a535abd07ad93b3f7bb8d7b13b92c7
sharpstorm <chimchar17@gmail.com>
2022-03-14 14:41:06 +0800

    Change logic of DateRangePredicate

--- src/main/java/seedu/contax/model/appointment/DateRangePredicate.java
+++ src/main/java/seedu/contax/model/appointment/DateRangePredicate.java
@@ -1,7 +1,11 @@
 package seedu.contax.model.appointment;
 
+import static java.util.Objects.requireNonNull;
+
 import java.time.LocalDateTime;
 import java.util.function.Predicate;
+
+import seedu.contax.commons.util.DateUtil;
 
 /**
  * Tests that a {@code Appointment} falls between the given date time range.
@@ -19,6 +23,9 @@ public class DateRangePredicate implements Predicate<Appointment> {
      * @param end The end of the range to filter.
      */
     public DateRangePredicate(LocalDateTime start, LocalDateTime end) {
+        requireNonNull(start);
+        requireNonNull(end);
+
         this.start = start;
         this.end = end;
     }
@@ -28,9 +35,9 @@ public class DateRangePredicate implements Predicate<Appointment> {
         LocalDateTime appointmentStart = appointment.getStartDateTime().value;
         LocalDateTime appointmentEnd = appointment.getEndDateTime();
 
-        return (appointmentStart.isBefore(start) && appointmentEnd.isAfter(end))
-                || (appointmentStart.isAfter(start) && appointmentEnd.isBefore(end))
-                || (appointmentStart.isAfter(start) && appointmentStart.isBefore(end))
-                || (appointmentEnd.isAfter(start) && appointmentEnd.isBefore(end));
+        return (DateUtil.isBeforeOrEqual(appointmentStart, start) && DateUtil.isAfterOrEqual(appointmentEnd, end))
+                || (DateUtil.isAfterOrEqual(appointmentStart, start) && DateUtil.isBeforeOrEqual(appointmentEnd, end))
+                || (DateUtil.isAfterOrEqual(appointmentStart, start) && DateUtil.isBeforeOrEqual(appointmentStart, end))
+                || (DateUtil.isAfterOrEqual(appointmentEnd, start) && DateUtil.isBeforeOrEqual(appointmentEnd, end));
     }
 }
