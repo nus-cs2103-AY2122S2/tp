2dec323181e8016a81878304e98e2fb051139161
chinkiat <e0544173@u.nus.edu>
2022-03-14 23:22:12 +0800

    Add test cases for integration testing of FilterCommandParser.java

---
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.person.CovidStatus;
+
+public class FilterCommandParserTest {
+
+    private FilterCommandParser parser = new FilterCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(CovidStatus.CovidStatusTier.POSITIVE.toString());
+        assertParseSuccess(parser, "positive", expectedFilterCommand);
+
+        // with leading and trailing whitespaces
+        assertParseSuccess(parser, " \n positive  \t", expectedFilterCommand);
+
+        // with varying cases
+        assertParseSuccess(parser, " \n pOsITive  \t", expectedFilterCommand);
+    }
+
+}
