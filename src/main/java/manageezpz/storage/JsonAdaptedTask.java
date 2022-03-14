11c3c37094dcd875f6e19e4f87e15de838604d28
Alfred Koh <alfredkohkj@gmail.com>
2022-03-14 17:18:01 +0800

    Completed Storage functionality for all Tasks

--- src/main/java/manageezpz/storage/JsonAdaptedTask.java
+++ src/main/java/manageezpz/storage/JsonAdaptedTask.java
@@ -4,39 +4,44 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import manageezpz.commons.exceptions.IllegalValueException;
+import manageezpz.model.task.Date;
 import manageezpz.model.task.Deadline;
 import manageezpz.model.task.Description;
 import manageezpz.model.task.Event;
 import manageezpz.model.task.Task;
+import manageezpz.model.task.Time;
 import manageezpz.model.task.Todo;
+
+
 
 /**
  * Jackson-friendly version of {@link Task}.
  */
 class JsonAdaptedTask {
 
-    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Task's %s field is missing!";
 
     private final String description;
     private final String type;
     private final String date;
-    private final String time;
-    private final String startTime;
-    private final String endTime;
+    private String deadlineTime;
+    private String eventStartTime;
+    private String eventEndTime;
 
     /**
-     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+     * Constructs a {@code JsonAdaptedTask} with the given person details.
      */
     @JsonCreator
     public JsonAdaptedTask(@JsonProperty("type") String type, @JsonProperty("description") String description,
-    @JsonProperty("date") String date, @JsonProperty("time") String time,
-                           @JsonProperty("startTime") String startTime, @JsonProperty("endTime") String endTime) {
+                           @JsonProperty("date") String date, @JsonProperty("deadlineTime") String deadlineTime,
+                           @JsonProperty("eventStartTime") String eventStartTime,
+                           @JsonProperty("eventEndTime") String eventEndTime) {
         this.description = new Description(description).toString();
         this.type = type;
         this.date = date;
-        this.time = time;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.deadlineTime = deadlineTime;
+        this.eventStartTime = eventStartTime;
+        this.eventEndTime = eventEndTime;
     }
 
     /**
@@ -47,10 +52,20 @@ class JsonAdaptedTask {
         // For future reference, this.marked = source.getMark()?
         description = source.getDescription().toString(); // Generally for all tasks
         type = source.getType(); // Generally for all tasks
-        this.date = "Placeholder for source.getDate()"; // For Deadline
-        this.time = "Placeholder for source.getTime()"; // For Deadline
-        this.startTime = "Placeholder for source.getEndTime()"; // For Event
-        this.endTime = "Placeholder for source.getEndTime()"; // For Event
+
+        if (source instanceof Deadline) {
+            this.date = ((Deadline) source).getDate().getDate(); // For Deadline
+            this.deadlineTime = ((Deadline) source).getTime().getTime(); // For Deadline
+        } else if ((source instanceof Event)) {
+            this.date = ((Event) source).getDate().getDate(); // For Event
+            this.eventStartTime = ((Event) source).getStartTime().getTime(); // For Event
+            this.eventEndTime = ((Event) source).getEndTime().getTime(); // For Event
+        } else {
+            this.date = "";
+            this.deadlineTime = "";
+            this.eventStartTime = "";
+            this.eventEndTime = "";
+        }
     }
 
     /**
@@ -62,13 +77,21 @@ class JsonAdaptedTask {
         if (description == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Task.class.getSimpleName()));
         }
+        if (type == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Task.class.getSimpleName()));
+        }
         Description desc = new Description(description);
-        if(type.equals("todo")) {
+        if (type.equals("todo")) {
             return new Todo(desc);
-        } else if(type.equals("deadline")) {
- //           return new Deadline(desc, datetime,datetime);
-        } else if(type.equals("event")) {
-//                return new Event(desc,startTime, endTime);
+        } else if (type.equals("deadline")) {
+            Date currDeadlineDate = new Date(date);
+            Time currDeadlineTime = new Time(deadlineTime);
+            return new Deadline(desc, currDeadlineDate, currDeadlineTime);
+        } else if (type.equals("event")) {
+            Date currEventDate = new Date(date);
+            Time currEventStartTime = new Time(eventStartTime);
+            Time currEventEndTime = new Time(eventEndTime);
+            return new Event(desc, currEventDate, currEventStartTime, currEventEndTime);
         }
         return new Todo(desc);
     }
