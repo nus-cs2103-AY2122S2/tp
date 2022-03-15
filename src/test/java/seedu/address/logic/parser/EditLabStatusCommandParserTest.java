b66cd7de5a7784a5a90ee622010fb0e4ff0252a4
channne <cccx2303@gmail.com>
2022-03-15 21:52:47 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/parser/EditLabStatusCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditLabStatusCommandParserTest.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LAB;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LABSTATUS;
