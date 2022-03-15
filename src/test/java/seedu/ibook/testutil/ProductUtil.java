6a6516f9ccf6aadf6c9bc534bb2fbb5297981fda
Lai Mei Tin <e0550504@u.nus.edu>
2022-03-15 21:43:12 +0800

    Update testutil

--- src/test/java/seedu/ibook/testutil/ProductUtil.java
+++ src/test/java/seedu/ibook/testutil/ProductUtil.java
@@ -7,6 +7,7 @@ import static seedu.ibook.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ibook.logic.parser.CliSyntax.PREFIX_PRICE;
 
 import seedu.ibook.logic.commands.AddCommand;
+import seedu.ibook.logic.commands.UpdateCommand;
 import seedu.ibook.model.product.Product;
 
 /**
@@ -16,7 +17,6 @@ public class ProductUtil {
     /**
      * Returns an add command string for adding the {@code product}.
      */
-
     public static String getAddCommand(Product product) {
         return AddCommand.COMMAND_WORD + " " + getProductDetails(product);
     }
@@ -24,7 +24,6 @@ public class ProductUtil {
     /**
      * Returns the part of command string for the given {@code product}'s details.
      */
-
     public static String getProductDetails(Product product) {
         StringBuilder sb = new StringBuilder();
         sb.append(PREFIX_NAME + product.getName().toString() + " ");
@@ -34,25 +33,23 @@ public class ProductUtil {
         sb.append(PREFIX_PRICE + product.getPrice().toString() + " ");
         return sb.toString();
     }
-    /*
+
     /**
      * Returns the part of command string for the given {@code EditProductDescriptor}'s details.
-
-    public static String getEditProductDescriptorDetails(EditProductDescriptor descriptor) {
+     */
+    public static String getUpdateProductDescriptorDetails(UpdateCommand.UpdateProductDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
-        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
-        descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
-        descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-        if (descriptor.getTags().isPresent()) {
-            Set<Tag> tags = descriptor.getTags().get();
-            if (tags.isEmpty()) {
-                sb.append(PREFIX_TAG);
-            } else {
-                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
-            }
-        }
+        descriptor.getName().ifPresent(
+            name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
+        descriptor.getCategory().ifPresent(
+            category -> sb.append(PREFIX_CATEGORY).append(category.fullCategoryName).append(" "));
+        descriptor.getExpiryDate().ifPresent(
+            expiryDate -> sb.append(PREFIX_EXPIRY_DATE).append(expiryDate.toString()).append(" "));
+        descriptor.getDescription().ifPresent(
+            description -> sb.append(PREFIX_DESCRIPTION).append(description).append(" "));
+        descriptor.getPrice().ifPresent(
+            price -> sb.append(PREFIX_PRICE).append(price.toString()).append(" "));
         return sb.toString();
     }
-    */
+
 }
