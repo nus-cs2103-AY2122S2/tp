c977220317d0ce098aff3d5edee9846c601d5b78
Zachary Chan <trollinsir@hotmail.com>
2022-03-16 20:47:26 +0800

    Update DG, UG, and EditCommand
    Updated DG to include find user story and use case. Updated UG to
    include find in command summary. Fixed a typo in EditCommand.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -34,7 +34,7 @@ public class EditCommand extends Command {
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
-            + "[" + PREFIX_STATUS + "NAME] "
+            + "[" + PREFIX_STATUS + "STATUS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_NAME + "Attack on Titan "
