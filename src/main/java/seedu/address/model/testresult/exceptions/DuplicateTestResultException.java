fc75ac6f3bfea52612a97308c24e206ba40ce76b
juslam19 <e0552250@u.nus.edu>
2022-03-17 00:56:15 +0800

    Please work

--- src/main/java/seedu/address/model/testresult/exceptions/DuplicateTestResultException.java
+++ src/main/java/seedu/address/model/testresult/exceptions/DuplicateTestResultException.java
@@ -8,4 +8,5 @@ public class DuplicateTestResultException extends RuntimeException {
     public DuplicateTestResultException() {
         super("Operation would result in duplicate test results");
     }
+    
 }
