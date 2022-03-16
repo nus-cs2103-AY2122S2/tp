25ccb2a22c4c3f52c05faa5f0d9e943157083a86
mradhyaf <mradhyaf@gmail.com>
2022-03-16 21:29:29 +0800

    Create delete company command and corresponding error message

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,4 +11,5 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
     public static final String MESSAGE_INVALID_EVENT_DISPLAYED_INDEX = "The event index provided is invalid";
+    public static final String MESSAGE_INVALID_COMPANY_DISPLAYED_INDEX = "The company index provided is invalid";
 }
