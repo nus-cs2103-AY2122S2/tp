a31c7a70479566d8f7648f41dc83e754c40c35f8
Albert Sutiono <albertsutz123@gmail.com>
2022-03-17 02:05:23 +0800

    Refactor all the initial commands to commands specific to person

---
+++ src/main/java/seedu/address/logic/commands/ListPersonCommand.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_NO_COMPANIES;
+import static seedu.address.model.Model.PREDICATE_SHOW_NO_EVENTS;
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ListPersonCommand extends Command {
+
+    public static final String COMMAND_WORD = "listp";
+
+    public static final String MESSAGE_SUCCESS = "Listed all persons";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        //Show all the persons and empty out all the temporary list for events and companies
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredEventList(PREDICATE_SHOW_NO_EVENTS);
+        model.updateFilteredCompanyList(PREDICATE_SHOW_NO_COMPANIES);
+        return new CommandResult(MESSAGE_SUCCESS, false, false, true, false, false);
+    }
+}
