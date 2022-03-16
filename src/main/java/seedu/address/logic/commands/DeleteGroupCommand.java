ae892df46002e84d6ccb1da0243646d0c63095ac
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-17 02:45:03 +0800

    Add delgroup usage

--- src/main/java/seedu/address/logic/commands/DeleteGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteGroupCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -9,6 +12,12 @@ import seedu.address.model.Model;
 public class DeleteGroupCommand extends Command {
 
     public static final String COMMAND_WORD = "delgroup";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes a task from a group."
+            + "Parameters: "
+            + PREFIX_GROUP_NAME + "TASK_NAME \n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_GROUP_NAME + "CS2103-W16-3 \n";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
