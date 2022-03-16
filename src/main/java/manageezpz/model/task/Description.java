dd8aec5955f4989b1679eac419c7ad98186c76fc
ChanWeiJie <30587813+ChanWeiJie@users.noreply.github.com>
2022-03-16 19:28:23 +0800

    Change message constrain for Description class.

--- src/main/java/manageezpz/model/task/Description.java
+++ src/main/java/manageezpz/model/task/Description.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static manageezpz.commons.util.AppUtil.checkArgument;
 
 public class Description {
-    public static final String MESSAGE_CONSTRAINTS = "Description should should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Description should not be blank";
 
     public final String description;
 
