e36d20b57fe9ed786a9d8ef7d33fb5a94e646639
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-17 13:34:44 +0800

    Update MESSAGE_USAGE for deltask command

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -9,6 +12,14 @@ import seedu.address.model.Model;
 public class DeleteTaskCommand extends Command {
 
     public static final String COMMAND_WORD = "deltask";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes an existing task from an existing group."
+            + "Parameters: "
+            + PREFIX_TASK + "TASK_NAME "
+            + PREFIX_GROUP_NAME + "GROUP_NAME \n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TASK + "v1.2 user guide "
+            + PREFIX_GROUP_NAME + "CS2103-W16-3 \n";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
