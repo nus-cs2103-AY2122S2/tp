9379a991baee609ed6cd62ae5c4d1d9a3343d8ba
TypeDefinition <lnxterry@gmail.com>
2022-03-17 15:38:39 +0800

    Set optional fields to appear in a FlowPane instead of a label

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,16 +1,14 @@
 package seedu.address.ui;
 
 import java.util.Comparator;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.Birthday;
+import seedu.address.model.person.Field;
 import seedu.address.model.person.Membership;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Remark;
 
 /**
  * An UI component that displays information of a {@code Person}.
@@ -31,6 +29,8 @@ public class PersonCard extends UiPart<Region> {
 
     @FXML
     private HBox cardPane;
+
+    // Required Fields
     @FXML
     private Label name;
     @FXML
@@ -42,9 +42,7 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
-    private Label birthday;
-    @FXML
-    private Label remark;
+    private FlowPane optionalFields;
     @FXML
     private FlowPane tags;
     @FXML
@@ -67,13 +65,13 @@ public class PersonCard extends UiPart<Region> {
         email.setText(person.getEmail().getValue());
 
         // Optional fields.
-        remark.setText(person.getField(Remark.PREFIX).orElse(Remark.EMPTY_REMARK).getValue());
-        birthday.setText(person.hasField(Birthday.PREFIX) ? person.getField(Birthday.PREFIX).get().getValue() : "");
+        person.getFields().stream().filter((Field f) -> !f.prefix.isRequired()).forEach((Field f) -> {
+            optionalFields.getChildren().add(new Label(f.getValue()));
+        });
 
         // Tags.
-        person.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.value))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.value)));
+        person.getTags().stream().sorted(Comparator.comparing(tag -> tag.value))
+            .forEach(tag -> tags.getChildren().add(new Label(tag.value)));
 
         /*
         // Memberships.
@@ -88,8 +86,7 @@ public class PersonCard extends UiPart<Region> {
         if (person.hasTransaction()) {
             sb.append("Transactions: \n");
         }
-        person.getTransactions()
-                .forEach(transaction -> sb.append(transaction).append("\n"));
+        person.getTransactions().forEach(transaction -> sb.append(transaction).append("\n"));
 
         transactions.setText(sb.toString());
         Membership membership = person.getMembership();
@@ -113,7 +110,6 @@ public class PersonCard extends UiPart<Region> {
 
         // state check
         PersonCard card = (PersonCard) other;
-        return id.getText().equals(card.id.getText())
-                && person.equals(card.person);
+        return id.getText().equals(card.id.getText()) && person.equals(card.person);
     }
 }
