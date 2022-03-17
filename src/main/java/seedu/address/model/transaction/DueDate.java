13b6035195a34bee85672d20c7e04f6988441459
Daffa Fathani Adila <daffa26@gmail.com>
2022-03-17 10:38:51 +0800

    fix checkstyle issues

--- src/main/java/seedu/address/model/transaction/DueDate.java
+++ src/main/java/seedu/address/model/transaction/DueDate.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-import seedu.address.logic.commands.AddTransactionCommand;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
 
