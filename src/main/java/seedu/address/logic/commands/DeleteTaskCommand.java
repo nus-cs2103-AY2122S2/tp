cb851ad0a7058703e00970b7e4878a220b4e0436
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-17 13:22:57 +0800

    Hook deltask to ArchDuke

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -3,10 +3,15 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Deletes an existing task from ArchDuke.
+ */
 public class DeleteTaskCommand extends Command {
+
+    public static final String COMMAND_WORD = "deltask";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        return new CommandResult("Hello from deltask");
     }
 }
