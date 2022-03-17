d005c127471b1064cc1c763efedef661d4559775
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-03-17 09:40:54 +0800

    Incoporate peer review suggestions

--- src/main/java/seedu/address/logic/commands/EditEventCommand.java
+++ src/main/java/seedu/address/logic/commands/EditEventCommand.java
@@ -45,7 +45,7 @@ public class EditEventCommand extends Command {
 
     public static final String MESSAGE_EDIT_EVENT_SUCCESS = "Edited Event: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_EVENT = "This person already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_EVENT = "This event already exists in the address book.";
 
     public final Index index;
     public final EditEventDescriptor editEventDescriptor;
