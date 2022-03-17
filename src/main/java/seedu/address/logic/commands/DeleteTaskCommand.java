cfcf6cf84c4970ac3b342fdf6f8dac2a7a323181
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-17 17:23:22 +0800

    Implement DeleteTaskCommand methods

--- src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTaskCommand.java
@@ -1,10 +1,13 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.group.Group;
+import seedu.address.model.task.Task;
 
 /**
  * Deletes an existing task from ArchDuke.
@@ -21,8 +24,37 @@ public class DeleteTaskCommand extends Command {
             + PREFIX_TASK + "v1.2 user guide "
             + PREFIX_GROUP_NAME + "CS2103-W16-3 \n";
 
+    public static final String MESSAGE_ARGUMENTS = "Task: %1$s, Group: %2$s";
+
+    private final Task task;
+    private final Group group;
+
+    public DeleteTaskCommand(Task task, Group group) {
+        requireAllNonNull(task, group);
+
+        this.task = task;
+        this.group = group;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("Hello from deltask");
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, task, group));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteTaskCommand)) {
+            return false;
+        }
+
+        // state check
+        DeleteTaskCommand e = (DeleteTaskCommand) other;
+        return task.equals(e.task) && group.equals(e.group);
     }
 }
