c497c7fb5559c9984703022bef30e6c2cb514410
YIHERNPC\leeyi <leeyihern99@gmail.com>
2022-03-18 11:38:22 +0800

    Add support for listing positions

--- src/main/java/seedu/address/logic/commands/position/ListPositionCommand.java
+++ src/main/java/seedu/address/logic/commands/position/ListPositionCommand.java
@@ -1,17 +1,24 @@
 package seedu.address.logic.commands.position;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_POSITIONS;
+
 import seedu.address.commons.core.DataType;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.model.Model;
 
+/**
+ * Lists all positions in the address book to the user.
+ */
 public class ListPositionCommand extends ListCommand {
-    public static final String MESSAGE_SUCCESS = ""; //TODO
+    public static final String MESSAGE_SUCCESS = "Listed all Positions";
 
     @Override
     public CommandResult execute(Model model) {
-        //TODO
-        return null;
+        requireNonNull(model);
+        model.updateFilteredPositionList(PREDICATE_SHOW_ALL_POSITIONS);
+        return new CommandResult(MESSAGE_SUCCESS, getCommandDataType());
     }
 
     @Override
