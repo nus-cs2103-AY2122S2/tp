173b4d20fbc64bd74f8b724f3d07473d4e5fc923
Vishwanth2210 <77226742+Vishwanth2210@users.noreply.github.com>
2022-03-19 01:55:01 +0800

    Refactor "addressbook" in tp.main to "hustlebook"
    Currently, the main java folder (tp/src/main) still has reference to "addressbook". This might cause confusion.
    
    This update changes to all occurrences of  "addressbook" to "hustlebook" to match with the application name.

--- src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
@@ -16,7 +16,7 @@ import seedu.address.model.person.Person;
 /**
  * An Immutable HustleBook that is serializable to JSON format.
  */
-@JsonRootName(value = "addressbook")
+@JsonRootName(value = "hustlebook")
 class JsonSerializableHustleBook {
 
     public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
