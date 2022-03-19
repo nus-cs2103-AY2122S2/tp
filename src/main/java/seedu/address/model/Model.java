3f21fbac998204864137ab942c45993ddacc442c
clement0010 <clement0010@hotmail.com>
2022-03-19 16:57:16 +0800

    Discard redundant methods

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -152,40 +152,6 @@ public interface Model {
 
     // Consultation
 
-    /**
-     * Returns true if a consultation with the same identity as {@code consultation} exists in the address book.
-     */
-    boolean hasConsultation(Consultation consultation);
-
-    /**
-     * Deletes the given consultation.
-     * The consultation must exist in the address book.
-     */
-    void deleteConsultation(Consultation target);
-
-    /**
-     * Adds the given consultation.
-     * {@code consultation} must not already exist in the address book.
-     */
-    void addConsultation(Consultation consultation);
-
-    /**
-     * Replaces the given consultation {@code target} with {@code editedConsultation}.
-     * {@code target} must exist in the address book.
-     * The consultation identity of {@code editedConsultation} must not be the same
-     * as another existing consultation in the address book.
-     */
-    void setConsultation(Consultation target, Consultation editedConsultation);
-
-    /** Returns an unmodifiable view of the filtered consultation list */
-    ObservableList<Consultation> getFilteredConsultationList();
-
-    /**
-     * Updates the filter of the filtered consultation list to filter by the given {@code predicate}.
-     * @throws NullPointerException if {@code predicate} is null.
-     */
-    void updateFilteredConsultationList(Predicate<Consultation> predicate);
-
     void updateFilteredPrescriptionList(Predicate<Prescription> predicate);
 
     // Consultation
