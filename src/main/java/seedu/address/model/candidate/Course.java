9f25a2d09b096af074cd4682ba1006a1d6f86d8e
domlimm <dominiclimkj3448@gmail.com>
2022-03-21 22:19:18 +0800

    Enhance course validation

--- src/main/java/seedu/address/model/candidate/Course.java
+++ src/main/java/seedu/address/model/candidate/Course.java
@@ -5,12 +5,15 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class Course {
     public static final String MESSAGE_CONSTRAINTS =
-            "Courses should only contain alphabets, and it should not be blank";
-
-    /*
-     * The format of Course should only contain alphabets case-insensitive.
-     */
-    public static final String VALIDATION_REGEX = "^\\w+( \\w+)*$";
+            "Courses should only contain alphabets, and it should not be blank\n"
+            + "Accepting only exact matches of either Computing courses"
+            + "(Business Analytics, Computer Engineering, "
+            + "Computer Science, Information Security, Information Systems)";
+    private static final String[] COURSES = {
+        "Business Analytics", "Computer Engineering", "Computer Science",
+        "Information Security", "Information Systems" };
+    private static final int[] LENGTHS = { 18, 20, 16, 20, 19 };
+    private static final int MAX_LENGTH = 20;
 
     public final String course;
 
@@ -29,7 +32,17 @@ public class Course {
      * Returns true if a given string is a valid course.
      */
     public static boolean isValidCourse(String test) {
-        return test.matches(VALIDATION_REGEX);
+        if (test.length() > MAX_LENGTH) {
+            return false;
+        }
+
+        for (int index = 0; index < COURSES.length; ++index) {
+            if (test.equals(COURSES[index]) && test.length() == LENGTHS[index]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
