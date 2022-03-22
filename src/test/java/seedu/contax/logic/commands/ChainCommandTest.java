a4d4d5a265d93ac13a4e2c46508815ebe8a9e36b
HanJiyao <hanjiyao0@gmail.com>
2022-03-22 21:54:29 +0800

    test case update

--- src/test/java/seedu/contax/logic/commands/ChainCommandTest.java
+++ src/test/java/seedu/contax/logic/commands/ChainCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.contax.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.contax.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.contax.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.contax.testutil.TypicalPersons.getTypicalAddressBook;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.contax.commons.core.GuiListContentType;
+import seedu.contax.logic.commands.exceptions.CommandException;
 import seedu.contax.model.AddressBook;
 import seedu.contax.model.Model;
 import seedu.contax.model.ModelManager;
@@ -31,14 +34,14 @@ public class ChainCommandTest {
     }
 
     @Test
-    public void execute_chainedList_success() {
-        assertCommandSuccess(new ChainCommand(List.of(new ListPersonCommand())), model,
-                new CommandResult(ListPersonCommand.MESSAGE_SUCCESS,
-                GuiListContentType.PERSON), expectedModel);
+    public void execute_chainedList_success() throws CommandException {
+        ChainCommand testChainCommand = new ChainCommand(List.of(new ListPersonCommand()));
+        assertEquals(testChainCommand.execute(model).getFeedbackToUser().trim(),
+                new ListPersonCommand().execute(model).getFeedbackToUser());
     }
 
     @Test
-    public void execute_chainedEditThenList_success() {
+    public void execute_chainedEditThenList_success() throws CommandException {
         Person editedPerson = new PersonBuilder().build();
         EditPersonCommand.EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
         EditPersonCommand editPersonCommand = new EditPersonCommand(INDEX_FIRST_PERSON, descriptor);
@@ -52,8 +55,10 @@ public class ChainCommandTest {
         commandList.add(editPersonCommand);
         commandList.add(new ListPersonCommand());
 
-        assertCommandSuccess(new ChainCommand(commandList),
-                model, new CommandResult(ListPersonCommand.MESSAGE_SUCCESS,
-                GuiListContentType.PERSON), expectedModel);
+        ChainCommand testChainCommand =  new ChainCommand(commandList);
+        assertEquals(testChainCommand.execute(model).getFeedbackToUser().trim(), ("" +
+                "Edited Person: Amy Bee; Phone: 85355255; Email: amy@gmail.com; " +
+                "Address: 123, Jurong West Ave 6, #08-111\n" +
+                "Listed all persons"));
     }
 }
