9249b94a2a2e7a58f56fffa289eb063386e878f3
dionegoh <e0439283@u.nus.edu>
2022-03-22 15:31:37 +0800

    Implement find friend by log title substring

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code Person}'s {@code name, tags} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code name, tags, logs title} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
@@ -21,12 +21,21 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
         return keywords.stream()
                 .anyMatch(keyword -> {
                     String keyword_to_lower_case = keyword.toLowerCase(Locale.ROOT);
+
+                    //check if (substring) name of a person matches keyword
                     boolean nameMatch = person.getName().fullName.toLowerCase(Locale.ROOT)
                             .contains(keyword_to_lower_case);
+
+                    //check if tag of a person matches keyword
                     boolean tagsMatch = person.getTags().stream().anyMatch(tag ->
                             tag.tagName.toLowerCase(Locale.ROOT).equals(keyword_to_lower_case));
-                    return nameMatch || tagsMatch;
-        });
+
+                    //check if (substring) logs title of a person matches keyword
+                    boolean logsMatch = person.getLogs().stream().anyMatch(log ->
+                            log.getTitle().fullName.toLowerCase(Locale.ROOT).contains(keyword_to_lower_case));
+
+                    return nameMatch || tagsMatch || logsMatch ;
+                });
     }
 
     @Override
