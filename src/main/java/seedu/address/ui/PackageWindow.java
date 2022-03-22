8f7e5dbfa26ca85b4fd6c7d018cf8aff1358697b
michaelseyo <michael.lcw99@gmail.com>
2022-03-23 00:17:24 +0800

    Create base ui for insurance package

---
+++ src/main/java/seedu/address/ui/PackageWindow.java
@@ -0,0 +1,124 @@
+package seedu.address.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.Logic;
+import seedu.address.model.person.InsurancePackage;
+import seedu.address.model.person.Person;
+
+/**
+ * Controller for a package page
+ */
+public class PackageWindow extends UiPart<Stage> {
+    private static final Logger logger = LogsCenter.getLogger(PackageWindow.class);
+    private static final String FXML = "PackageWindow.fxml";
+    private Logic logic;
+
+    @FXML
+    private StackPane packageListPanelPlaceholder;
+    @FXML
+    private Label helpMessage;
+
+    // Independent Ui parts residing in this Ui container
+    private PackageListPanel packageListPanel;
+
+    /**
+     * Creates a new PackageWindow.
+     *
+     * @param root Stage to use as the root of the PackageWindow.
+     */
+    public PackageWindow(Stage root) {
+        super(FXML, root);
+    }
+
+    /**
+     * Creates a new PackageWindow.
+     */
+    public PackageWindow(Logic logic) {
+        this(new Stage());
+        this.logic = logic;
+    }
+
+    /**
+     * Shows the package window.
+     * @throws IllegalStateException
+     * <ul>
+     *     <li>
+     *         if this method is called on a thread other than the JavaFX Application Thread.
+     *     </li>
+     *     <li>
+     *         if this method is called during animation or layout processing.
+     *     </li>
+     *     <li>
+     *         if this method is called on the primary stage.
+     *     </li>
+     *     <li>
+     *         if {@code dialogStage} is already showing.
+     *     </li>
+     * </ul>
+     */
+    public void show() {
+        logger.fine("Showing package page about the insurance packages.");
+        getRoot().show();
+        getRoot().centerOnScreen();
+    }
+
+    /**
+     * Fills up all the placeholders of this window.
+     */
+    public void fillInnerParts() {
+        ObservableList<InsurancePackage> internalList =
+                FXCollections.observableArrayList(new InsurancePackage("Premium"),
+                        new InsurancePackage("Ups Special"),
+                        new InsurancePackage("Golden Dragon"));
+        ObservableList<InsurancePackage> internalUnmodifiableList =
+                FXCollections.unmodifiableObservableList(internalList);
+        ObservableList<InsurancePackage> packageList = new FilteredList<>(internalUnmodifiableList);
+        packageListPanel = new PackageListPanel(packageList);
+        packageListPanelPlaceholder.getChildren().add(packageListPanel.getRoot());
+    }
+
+    /**
+     * Returns true if the help window is currently being shown.
+     */
+    public boolean isShowing() {
+        return getRoot().isShowing();
+    }
+
+    /**
+     * Hides the help window.
+     */
+    public void hide() {
+        getRoot().hide();
+    }
+
+    /**
+     * Focuses on the help window.
+     */
+    public void focus() {
+        getRoot().requestFocus();
+    }
+
+    /**
+     * Copies the URL to the user guide to the clipboard.
+     */
+    /*
+    @FXML
+    private void copyUrl() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(USERGUIDE_URL);
+        clipboard.setContent(url);
+    }
+    */
+}
