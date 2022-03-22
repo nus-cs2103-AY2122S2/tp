987a9d1a9673215835273527887cb34f707956ab
brandonrhan <e0540875@u.nus.edu>
2022-03-22 11:18:37 +0800

    Edits some function

--- src/main/java/seedu/address/model/UniqueLineupList.java
+++ src/main/java/seedu/address/model/UniqueLineupList.java
@@ -22,16 +22,46 @@ public class UniqueLineupList {
         this.list = new ArrayList<Lineup>();
     }
 
+    /**
+     * Adds a lineup to the list
+     *
+     * @param lineup The lineup to be added
+     */
     public void addLineupToList(Lineup lineup) {
         requireNonNull(lineup);
         this.list.add(lineup);
     }
 
+    /**
+     * Deletes a lineup from the list
+     *
+     * @param lineup The lineup to be deleted
+     */
     public void deleteLineupFromList(Lineup lineup) {
         requireNonNull(lineup);
         this.list.remove(lineup);
     }
 
+    /**
+     * Deletes a lineup from the list using lineup name
+     *
+     * @param lineupName The name of the lineup to be deleted
+     */
+    public void deleteLineupNameFromList(LineupName lineupName) {
+        requireNonNull(lineupName);
+        for (Lineup lineup : list) {
+            if (lineup.sameLineupName(lineupName)) {
+                this.list.remove(lineup);
+            }
+        }
+    }
+
+    /**
+     * Checks for the list contains a lineup
+     *
+     * @param lineup The lineup to be checked
+     * @return Boolean represents the existence of the lineup
+     */
     public boolean containsLineup(Lineup lineup) {
         requireNonNull(lineup);
         return this.list.contains(lineup);
