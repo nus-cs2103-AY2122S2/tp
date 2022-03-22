0b0cf46e73234395f066a6cbca799301f11a76e1
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2022-03-22 19:16:07 +0800

    Add filter command
    * minor updates on DG, PPP
    * Able to filter contacts by tags (filter [tagName])

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,61 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.PersonContainsTagPredicate;
+import seedu.address.model.tag.Tag;
+
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters the current contact list by the tags "
+            + "attached to the contacts.\n"
+            + "Parameters: TAG_NAME\n"
+            + "Example: " + COMMAND_WORD + " family";
+
+    private final Tag tag;
+    private final PersonContainsTagPredicate predicate;
+
+    /**
+     * @param predicate used to filter the current contact list
+     */
+    public FilterCommand(PersonContainsTagPredicate predicate, Tag tag) {
+        this.predicate = predicate;
+        this.tag = tag;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        if (!model.hasTag(tag)) {
+            throw new CommandException("tag does not exist");
+        }
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+
+        //return new CommandResult("Hello from filter");
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        // state check
+        FilterCommand e = (FilterCommand) other;
+        return tag.equals(e.tag);
+    }
+
+}
