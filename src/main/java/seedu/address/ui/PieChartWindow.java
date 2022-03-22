185f1354d30f9975b6346352d0747f5db333ebe9
jiale-c <cheejiale@gmail.com>
2022-03-23 02:47:24 +0800

    Add pie charts for each blocks present in the hall
    Giving pie charts by blocks alongside the chart for the hall overall can
    allow users of Tracey to have a better overview of the covid situation
    in the hall at various locations
    
    Pie charts for each blocks were created first then the overall chart is
    added so that users can look from a micro level to a bigger picture
    
    This makes better use of the data and helps the user to make sense out
    of the data too

--- src/main/java/seedu/address/ui/PieChartWindow.java
+++ src/main/java/seedu/address/ui/PieChartWindow.java
@@ -8,12 +8,13 @@ import java.util.stream.Stream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.person.Faculty;
 
 /**
  * Controller for a pie chart page
@@ -23,11 +24,12 @@ public class PieChartWindow extends UiPart<Stage> {
     private static final Logger logger = LogsCenter.getLogger(PieChartWindow.class);
     private static final String FXML = "PieChartWindow.fxml";
     private ArrayList<String> faculties;
-    private ArrayList<Double> positiveStats;
+    private ArrayList<String> blocks;
+    private TreeMap<String, Double> covidStatsByBlockData;
     private TreeMap<String, Double> positiveStatsByFacultyData;
     private PieChart pieChart;
-    private VBox pieChartContainer;
     private Scene pieChartScene;
+    private HBox charts;
 
     /**
      * Creates a new PieChartWindow.
@@ -40,15 +42,17 @@ public class PieChartWindow extends UiPart<Stage> {
 
     /**
      * Creates a new PieChartWindow.
+     *
+     * @param message Feedback by tracey with summary of the covid data
      */
     public PieChartWindow(String message) {
         this(new Stage());
+        charts = new HBox();
+        covidStatsByBlockData = new TreeMap<>();
+        positiveStatsByFacultyData = new TreeMap<>();
+        blocks = parseBlockLetter(message);
         faculties = parseFacultyName(message);
-        positiveStats = parsePositivePercentage(message);
-        positiveStatsByFacultyData = getPositiveStatsByFacultyData(faculties, positiveStats);
-        pieChart = makePieChart(positiveStatsByFacultyData);
-        pieChartScene = makePieChartScene(pieChart);
-        this.getRoot().setScene(pieChartScene);
+        execute();
     }
 
     /**
@@ -59,127 +63,113 @@ public class PieChartWindow extends UiPart<Stage> {
      */
     private ArrayList<String> parseFacultyName(String message) {
         ArrayList<String> arrayList = new ArrayList<>();
-        String[] splitMessage = message.split(" ");
-        Stream.of(splitMessage).filter(string -> Faculty.isValidFaculty(string))
-                .forEach(string -> arrayList.add(string));
+        String[] splitMessage = message.split("In ");
+        Stream.of(splitMessage).filter(string -> string.contains("faculty")).forEach(arrayList::add);
         return arrayList;
     }
 
     /**
-     * Parse the output message from SummariseCommand and gets the list of student's faculty in Tracey.
+     * Parse the output message from SummariseCommand and gets the list of student's block in Tracey.
      *
      * @param message Feedback message to user from SummariseCommand
-     * @return Array list containing student's faculty present in Tracey
-     */
-    private ArrayList<String> parseFacultyName2(String message) {
-        ArrayList<String> arrayList2 = new ArrayList<>();
-        String[] splitMessage2 = message.split("In ");
-        Stream.of(splitMessage2).filter(string -> string.contains("faculty")).forEach(string -> arrayList2.add(string));
-        System.out.println(arrayList2.get(0));
-        return arrayList2;
-    }
-
-    /**
-     * Parse the output message from SummariseCommand and gets the list of student's faculty in Tracey.
-     *
-     * @param message Feedback message to user from SummariseCommand
-     * @return Array list containing student's faculty present in Tracey
+     * @return Array list containing student's blocks present in Tracey
      */
     private ArrayList<String> parseBlockLetter(String message) {
-        ArrayList<String> arrayList2 = new ArrayList<>();
-        String[] splitMessage2 = message.split("In ");
-        Stream.of(splitMessage2).filter(string -> string.contains("block")).forEach(string -> arrayList2.add(string));
-        System.out.println(arrayList2.get(0));
-        return arrayList2;
-    }
-
-    /**
-     * Parse the output message from SummariseCommand and gets the list of covid positive percentage from each
-     * faculty present in Tracey.
-     *
-     * @param message Feedback message to user from SummariseCommand
-     * @return Array list containing covid positive percentage from each faculty present in Tracey.
-     */
-    private ArrayList<Double> parsePositivePercentage(String message) {
-        ArrayList<Double> arrayList = new ArrayList<>();
-        String[] splitMessage = message.split(" ");
-        String validationRegex = "[0-9]*\\.[0-9]{2}$";
-
-        // filters the index where the split message with REGEX ')' has length of 2 i.e. [student(s, 100.00]
-        Stream.of(splitMessage).filter(msg -> msg.split("\\)").length == 2)
-                // filters the index where the second element is a percentage i.e. 100.00
-                .filter(msg -> msg.split("\\)")[1].trim().matches(validationRegex))
-                // adds each percentage to the array list
-                .forEach(msg -> arrayList.add(Double.parseDouble(msg.split("\\)")[1])));
-
+        ArrayList<String> arrayList = new ArrayList<>();
+        String[] splitMessage = message.split("In ");
+        Stream.of(splitMessage).filter(string -> string.contains("block")).forEach(arrayList::add);
         return arrayList;
     }
 
     /**
-     * Puts the parsed data from the two array lists of faculty and covid positive percentage of faculty present
-     * in Tracey into a treemap for processing of data for the pie chart.
-     *
-     * @param faculties     Array list containing student's faculty present in Tracey
-     * @param positiveStats Array list containing covid positive percentage from each faculty present in Tracey.
-     * @return A treemap containing key value a pair of faculty and covid positive percentage.
+     * Organise the data into Pie Charts in a new window.
      */
-    private TreeMap<String, Double> getPositiveStatsByFacultyData(
-            ArrayList<String> faculties, ArrayList<Double> positiveStats) {
-        assert faculties.size() == positiveStats.size();
-        int numberOfStudents = faculties.size();
-        TreeMap<String, Double> treeMap = new TreeMap<>();
-        for (int i = 0; i < numberOfStudents; i++) {
-            treeMap.put(faculties.get(i), positiveStats.get(i));
-        }
-        return treeMap;
+    private void execute() {
+        collateBlocksChart();
+        HBox facultyChart = createFacultyChartPositive();
+        VBox allPieCharts = new VBox(charts);
+        allPieCharts.getChildren().add(facultyChart);
+        pieChartScene = makePieChartScene(allPieCharts);
+        this.getRoot().setScene(pieChartScene);
     }
 
     /**
-     * Puts the parsed data from the two array lists of faculty and covid positive percentage of faculty present
+     * Collate the pie charts for each block into a H-box.
+     */
+    private void collateBlocksChart() {
+        for (int i = 0; i < blocks.size(); i++) {
+            String title = blocks.get(i).substring(0, 1); // example: A
+            covidStatsByBlockData = getCovidStats(blocks, i);
+            pieChart = makePieChart(covidStatsByBlockData, title);
+            charts.getChildren().add(pieChart);
+        }
+    }
+
+    /**
+     * Create Pie Chart for hall containing Covid Positive statistics only, by Faculty.
+     */
+    private HBox createFacultyChartPositive() {
+        HBox facChart = new HBox();
+        for (int i = 0; i < faculties.size(); i++) {
+            getPositiveStatsByFaculty(positiveStatsByFacultyData, faculties, i);
+        }
+        pieChart = makePieChart(positiveStatsByFacultyData, "Hall");
+        facChart.getChildren().add(pieChart);
+        facChart.setAlignment(Pos.CENTER);
+        return facChart;
+    }
+
+    /**
+     * Puts the parsed data from the array list of blocks in the hall
      * in Tracey into a treemap for processing of data for the pie chart.
      *
-     * @param form     Array list containing student's faculty present in Tracey
-     * @return A treemap containing key value a pair of faculty and covid positive percentage.
+     * @param forms Array list containing students' covid status types in a block present in Tracey
+     * @param index The specific form number to find the covid status types and number of students
+     * @return A treemap containing key value a pair of status type and number of students
      */
-    private TreeMap<String, Double> getPositiveStats(ArrayList<String> form, int i) {
+    private TreeMap<String, Double> getCovidStats(ArrayList<String> forms, int index) {
         TreeMap<String, Double> treeMap = new TreeMap<>();
-        String targetForm = form.get(i);
-        String[] arr = targetForm.split(" ", 2);
-        String categoryName = arr[0];
-        targetForm = arr[1];
+        String targetForm = forms.get(index);
+        // spilt the form by lines
         String[] spiltTargetForm = targetForm.split(System.lineSeparator());
-        for (int j = 0; j < 3; j++) {
+        // get data from the standardised forms
+        for (int j = 1; j < 4; j++) {
             String[] spiltLine = spiltTargetForm[j].split(": ", 2);
-            String status = spiltLine[0];
+            String status = spiltLine[0]; // Type of status
             int numOfStudents = Integer.parseInt(spiltLine[1].substring(0, 1));
             treeMap.put(status, (double) numOfStudents);
         }
-
         return treeMap;
-    }
-    private boolean isInt(String str) {
-        try {
-            @SuppressWarnings("unused")
-            int x = Integer.parseInt(str);
-            return true; //String is an Integer
-        } catch (NumberFormatException e) {
-            return false; //String is not an Integer
-        }
-
     }
 
     /**
-     * A method to produce the pie chart that shows positive percentage by faculty.
+     * Puts the parsed data from the array list of faculty into a tree map
+     * in Tracey into a treemap for processing of data for the pie chart.
+     *
+     * @param treeMap The treemap containing the faculty and number of students
+     * @param form Array list containing student's faculty present in Tracey
+     * @param i the specific form number to find the number of students who are positive
+     */
+    private void getPositiveStatsByFaculty(TreeMap<String, Double> treeMap, ArrayList<String> form, int i) {
+        String targetForm = form.get(i);
+        String faculty = targetForm.split(" ", 2)[0];
+        String[] spiltTargetForm = targetForm.split("Covid Positive: ", 2);
+        int numOfStudents = Integer.parseInt(spiltTargetForm[1].substring(0, 1));
+        treeMap.put(faculty, (double) numOfStudents);
+    }
+
+    /**
+     * A method to produce the pie chart that shows covid status breakdown.
      *
      * @param data Covid positive percentage for each faculty present in Tracey
-     * @return A pie chart that shows covid positive percentage for each faculty
+     * @param title Label of this particular pie chart
+     * @return A pie chart that shows covid status breakdown
      */
-    private PieChart makePieChart(TreeMap<String, Double> data) {
+    private PieChart makePieChart(TreeMap<String, Double> data, String title) {
         TreeMap<String, Double> treeMap = data;
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
         PieChart pieChart = new PieChart(pieChartData);
-        pieChart.setTitle("Covid positive percentage by faculty");
+        pieChart.setTitle("Covid overview in " + title);
         for (Map.Entry<String, Double> entry : treeMap.entrySet()) {
             pieChartData.add(new PieChart.Data(entry.getKey(), entry.getValue()));
         }
@@ -188,17 +178,18 @@ public class PieChartWindow extends UiPart<Stage> {
 
     /**
      * Creates the scene which contains the pie chart.
-     * @param pieChart
+     *
+     * @param pieCharts collated group of charts to be displayed
      * @return Scene containing the pie chart
      */
-    private Scene makePieChartScene(PieChart pieChart) {
-        pieChartContainer = new VBox(pieChart);
-        pieChartScene = new Scene(pieChartContainer);
+    private Scene makePieChartScene(VBox pieCharts) {
+        pieChartScene = new Scene(pieCharts);
         return pieChartScene;
     }
 
     /**
      * Shows the pie chart window.
+     *
      * @throws IllegalStateException
      * <ul>
      *     <li>
