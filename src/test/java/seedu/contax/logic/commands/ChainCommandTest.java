55db5f711581ec67cd30e958c28a3a3aee1e4d83
HanJiyao <hanjiyao0@gmail.com>
2022-03-22 21:58:12 +0800

    checkstyle

--- src/test/java/seedu/contax/logic/commands/ChainCommandTest.java
+++ src/test/java/seedu/contax/logic/commands/ChainCommandTest.java
@@ -1,8 +1,6 @@
 package seedu.contax.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.contax.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.contax.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.contax.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -12,7 +10,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.contax.commons.core.GuiListContentType;
 import seedu.contax.logic.commands.exceptions.CommandException;
 import seedu.contax.model.AddressBook;
 import seedu.contax.model.Model;
@@ -55,10 +52,10 @@ public class ChainCommandTest {
         commandList.add(editPersonCommand);
         commandList.add(new ListPersonCommand());
 
-        ChainCommand testChainCommand =  new ChainCommand(commandList);
-        assertEquals(testChainCommand.execute(model).getFeedbackToUser().trim(), ("" +
-                "Edited Person: Amy Bee; Phone: 85355255; Email: amy@gmail.com; " +
-                "Address: 123, Jurong West Ave 6, #08-111\n" +
-                "Listed all persons"));
+        ChainCommand testChainCommand = new ChainCommand(commandList);
+        assertEquals(testChainCommand.execute(model).getFeedbackToUser().trim(), (
+                "Edited Person: Amy Bee; Phone: 85355255; Email: amy@gmail.com; "
+                + "Address: 123, Jurong West Ave 6, #08-111\n"
+                + "Listed all persons"));
     }
 }
