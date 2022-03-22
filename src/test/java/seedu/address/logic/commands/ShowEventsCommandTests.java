61385af1ac0d24bcb5187603542fbb952ca77a0a
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-03-22 09:53:24 +0800

    Add test cases for all the new functionalities (Event,Person compares)

--- src/test/java/seedu/address/logic/commands/ShowEventsCommandTests.java
+++ src/test/java/seedu/address/logic/commands/ShowEventsCommandTests.java
@@ -3,14 +3,19 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertEventCommandSuccess;
+import static seedu.address.model.util.SampleDataUtil.getSampleAddressBook;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.ObservableList;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.event.Event;
+
+
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for showfriends command.
@@ -22,7 +27,7 @@ public class ShowEventsCommandTests {
 
     @BeforeEach
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model = new ModelManager(getSampleAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
     }
 
@@ -45,5 +50,21 @@ public class ShowEventsCommandTests {
         // Model has persons but no events, thus event list should be empty.
         assertFalse(noEventModel.getFilteredPersonList().isEmpty());
         assertTrue(noEventModel.getEventsList().isEmpty());
+    }
+
+    /**
+     * Model's internal list should be sorted.
+     */
+    @Test
+    public void execute_listIsFiltered_showsOrderedList() {
+        assertEventCommandSuccess(new ShowEventsCommand(), model, ShowEventsCommand.MESSAGE_SUCCESS, expectedModel);
+
+        ObservableList<Event> checkSorted = model.getEventsList();
+
+        Event temp = checkSorted.get(0);
+        for (int i = 1; i < checkSorted.size(); i++) {
+            assertTrue(temp.compareTo(checkSorted.get(i)) < 0);
+            temp = checkSorted.get(0);
+        }
     }
 }
