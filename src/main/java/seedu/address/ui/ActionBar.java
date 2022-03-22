58deb93bef449bba9136a0da6490b6c1cbc57a8c
DannyDakota <51451719+DannyDakota@users.noreply.github.com>
2022-03-22 18:32:37 +0800

    add EmailWindow button and window to copy email

---
+++ src/main/java/seedu/address/ui/ActionBar.java
@@ -0,0 +1,14 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+
+public class ActionBar extends UiPart<Region> {
+
+    private static final String FXML = "ActionBar.fxml";
+
+    public ActionBar() {
+        super(FXML);
+    }
+
+}
