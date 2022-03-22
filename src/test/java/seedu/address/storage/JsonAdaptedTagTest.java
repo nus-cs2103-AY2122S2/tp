f05320d19f3f60584067be26d46c091637ded7e2
ddx-510 <dai.tianle@dhs.sg>
2022-03-22 19:46:08 +0800

    Include test for tagList and fix typical valid tags

---
+++ src/test/java/seedu/address/storage/JsonAdaptedTagTest.java
@@ -0,0 +1,27 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTags.FRIEND;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+public class JsonAdaptedTagTest {
+
+    private static final String INVALID_TAG = "#friend";
+
+    @Test
+    public void toModelType_validTagDetails_returnsTag() throws Exception {
+        JsonAdaptedTag tag = new JsonAdaptedTag(FRIEND);
+        assertEquals(FRIEND, tag.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidTags_throwsIllegalValueException() {
+        JsonAdaptedTag tag = new JsonAdaptedTag(INVALID_TAG);
+        assertThrows(IllegalValueException.class, tag::toModelType);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/TypicalTags.java
@@ -0,0 +1,8 @@
+package seedu.address.testutil;
+
+import seedu.address.model.tag.Tag;
+
+public class TypicalTags {
+    public static final Tag FRIEND = new TagBuilder().withTagName("friends").build();
+    public static final Tag OWEMONEY = new TagBuilder().withTagName("owesMoney").build();
+}
