17dad732d200f6c31d800bfe1c9f69950b724f86
Daffa Fathani Adila <daffa26@gmail.com>
2022-03-24 03:47:25 +0800

    add tests for transaction

--- src/test/java/seedu/address/model/transaction/TransactionTest.java
+++ src/test/java/seedu/address/model/transaction/TransactionTest.java
@@ -1,4 +1,62 @@
 package seedu.address.model.transaction;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TransactionUtil.VALID_AMOUNT_ONE;
+import static seedu.address.testutil.TransactionUtil.VALID_TRANSACTION_DATE_ONE;
+import static seedu.address.testutil.TransactionUtil.TRANSACTION_ONE_COMPLETE;
+import static seedu.address.testutil.TransactionUtil.TRANSACTION_TWO_COMPLETE;
+import static seedu.address.testutil.TransactionUtil.TRANSACTION_ONE_INCOMPLETE;
+
 public class TransactionTest {
+
+    @Test
+    public void constructor_null_throwsNullArgumentException() {
+        assertThrows(NullPointerException.class, () -> new Transaction((Collection<TransactionField>) null));
+    }
+
+    @Test
+    public void constructor_missingRequiredField_throwsIllegalArgumentException() {
+        // Missing Transaction Date Field
+        List<TransactionField> incompleteArgumentsOne = List.of(
+                new Amount(VALID_AMOUNT_ONE)
+        );
+        assertThrows(IllegalArgumentException.class, () -> new Transaction(incompleteArgumentsOne));
+
+        // Missing Amount Field
+        List<TransactionField> incompleteArgumentsTwo = List.of(
+                    new TransactionDate(VALID_TRANSACTION_DATE_ONE)
+        );
+        assertThrows(IllegalArgumentException.class, () -> new Transaction(incompleteArgumentsTwo));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Transaction transactionCopy = new Transaction(TRANSACTION_ONE_COMPLETE);
+        assertEquals(TRANSACTION_ONE_COMPLETE, transactionCopy);
+
+        // same object -> returns true
+        assertEquals(TRANSACTION_ONE_COMPLETE, TRANSACTION_ONE_COMPLETE);
+
+        // null -> returns false
+        assertNotEquals(null, TRANSACTION_ONE_COMPLETE);
+
+        // different type -> returns false
+        assertNotEquals("TRANSACTION_ONE_COMPLETE", TRANSACTION_ONE_COMPLETE);
+
+        // different Transaction -> returns false
+        assertNotEquals(TRANSACTION_ONE_COMPLETE, TRANSACTION_TWO_COMPLETE);
+
+        // some fields are missing -> return false
+        assertNotEquals(TRANSACTION_ONE_COMPLETE, TRANSACTION_ONE_INCOMPLETE);
+    }
 }
---
+++ src/test/java/seedu/address/testutil/TransactionUtil.java
@@ -0,0 +1,46 @@
+package seedu.address.testutil;
+
+import seedu.address.model.transaction.Amount;
+import seedu.address.model.transaction.DueDate;
+import seedu.address.model.transaction.Note;
+import seedu.address.model.transaction.Transaction;
+import seedu.address.model.transaction.TransactionDate;
+
+import java.util.List;
+
+public class TransactionUtil {
+    public static final String VALID_AMOUNT_ONE = "123.45";
+    public static final String VALID_TRANSACTION_DATE_ONE = "2020-11-11";
+    public static final String VALID_DUE_DATE_ONE = "2021-11-11";
+    public static final String VALID_NOTE_ONE = "This is a valid note";
+
+    public static final String VALID_AMOUNT_TWO = "11.99";
+    public static final String VALID_TRANSACTION_DATE_TWO = "2020-11-12";
+    public static final String VALID_DUE_DATE_TWO = "2021-11-12";
+    public static final String VALID_NOTE_TWO = "This is a valid note as well";
+
+    public static final Transaction TRANSACTION_ONE_COMPLETE = new Transaction(
+            List.of(
+                    new Amount(VALID_AMOUNT_ONE),
+                    new TransactionDate(VALID_TRANSACTION_DATE_ONE),
+                    new DueDate(VALID_DUE_DATE_ONE),
+                    new Note(VALID_NOTE_ONE)
+            )
+    );
+
+    public static final Transaction TRANSACTION_ONE_INCOMPLETE = new Transaction(
+            List.of(
+                    new Amount(VALID_AMOUNT_ONE),
+                    new TransactionDate(VALID_TRANSACTION_DATE_ONE)
+            )
+    );
+
+    public static final Transaction TRANSACTION_TWO_COMPLETE = new Transaction(
+            List.of(
+                    new Amount(VALID_AMOUNT_TWO),
+                    new TransactionDate(VALID_TRANSACTION_DATE_TWO),
+                    new DueDate(VALID_DUE_DATE_TWO),
+                    new Note(VALID_NOTE_TWO)
+            )
+    );
+}
