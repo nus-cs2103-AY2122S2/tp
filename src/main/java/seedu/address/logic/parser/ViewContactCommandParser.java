602fda5d1f7b520cc752e5dd730211b924269a7c
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-23 20:41:18 +0800

    Implement viewcontact command parser

--- src/main/java/seedu/address/logic/parser/ViewContactCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewContactCommandParser.java
@@ -1,2 +1,47 @@
-package seedu.address.logic.parser;public class ViewContactCommandParser {
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.ViewContactCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupName;
+
+/**
+ * Parses input arguments and creates a new ViewContactCommand object.
+ */
+public class ViewContactCommandParser implements Parser<ViewContactCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code ViewContactCommand}
+     * and returns an {@code ViewContactCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    @Override
+    public ViewContactCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_GROUP_NAME);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_GROUP_NAME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewContactCommand.MESSAGE_USAGE));
+        }
+
+        GroupName groupName = ParserUtil.parseGroupName(argMultimap.getValue(PREFIX_GROUP_NAME).orElse(""));
+        Group group = new Group(groupName);
+
+        return new ViewContactCommand(group);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
