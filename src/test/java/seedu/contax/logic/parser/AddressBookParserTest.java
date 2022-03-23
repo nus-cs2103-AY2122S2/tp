b29420e343b4f5600a5ee70fefdb1278c1d7dd7b
Wei-Xinn <E0544325@u.nus.edu>
2022-03-23 15:45:18 +0800

    Update AddressBookParserTest

--- src/test/java/seedu/contax/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/contax/logic/parser/AddressBookParserTest.java
@@ -16,33 +16,10 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import seedu.contax.commons.core.index.Index;
-import seedu.contax.logic.commands.AddAppointmentCommand;
-import seedu.contax.logic.commands.AddPersonCommand;
-import seedu.contax.logic.commands.AddTagCommand;
-import seedu.contax.logic.commands.AppointmentsBetweenCommand;
-import seedu.contax.logic.commands.BatchCommand;
-import seedu.contax.logic.commands.ChainCommand;
-import seedu.contax.logic.commands.ClearCommand;
-import seedu.contax.logic.commands.Command;
-import seedu.contax.logic.commands.DeleteAppointmentCommand;
-import seedu.contax.logic.commands.DeletePersonCommand;
-import seedu.contax.logic.commands.DeleteTagCommand;
-import seedu.contax.logic.commands.EditAppointmentCommand;
+import seedu.contax.logic.commands.*;
 import seedu.contax.logic.commands.EditAppointmentCommand.EditAppointmentDescriptor;
-import seedu.contax.logic.commands.EditPersonCommand;
 import seedu.contax.logic.commands.EditPersonCommand.EditPersonDescriptor;
-import seedu.contax.logic.commands.EditTagCommand;
 import seedu.contax.logic.commands.EditTagCommand.EditTagDescriptor;
-import seedu.contax.logic.commands.ExitCommand;
-import seedu.contax.logic.commands.ExportCsvCommand;
-import seedu.contax.logic.commands.FindByTagCommand;
-import seedu.contax.logic.commands.FindPersonCommand;
-import seedu.contax.logic.commands.HelpCommand;
-import seedu.contax.logic.commands.ImportCsvCommand;
-import seedu.contax.logic.commands.ListAppointmentCommand;
-import seedu.contax.logic.commands.ListPersonCommand;
-import seedu.contax.logic.commands.ListTagCommand;
-import seedu.contax.logic.commands.RangeCommand;
 import seedu.contax.logic.parser.exceptions.ParseException;
 import seedu.contax.model.IndexedCsvFile;
 import seedu.contax.model.appointment.Appointment;
@@ -107,6 +84,14 @@ public class AddressBookParserTest {
         FindPersonCommand command = (FindPersonCommand) parser.parseCommand(
                 FindPersonCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
         assertEquals(new FindPersonCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    }
+
+    @Test
+    public void parseCommand_findAppointment() throws Exception {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        FindAppointmentCommand command = (FindAppointmentCommand) parser.parseCommand(
+                FindAppointmentCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindAppointmentCommand(new seedu.contax.model.appointment.NameContainsKeywordsPredicate(keywords)), command);
     }
 
     @Test
