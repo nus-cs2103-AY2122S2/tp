fd51af54cb5327db4f08a83a58fea3c48449cffb
Daffa Fathani Adila <daffa26@gmail.com>
2022-03-24 01:31:58 +0800

    add tests for Note

--- src/test/java/seedu/address/model/transaction/NoteTest.java
+++ src/test/java/seedu/address/model/transaction/NoteTest.java
@@ -1,4 +1,21 @@
 package seedu.address.model.transaction;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.model.tag.Tag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
 public class NoteTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Note(null));
+    }
+
+    @Test
+    public void constructor_validArguments_returnsNote() {
+        String argument = "This is a note";
+        assertEquals(argument, new Note(argument).getValue());
+    }
+
 }
