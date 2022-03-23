bccb21bcaabca5a69819c6dcb8d71f46cb0ccd80
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-03-23 11:43:21 +0800

    Test group

--- src/test/java/seedu/address/model/group/GroupTest.java
+++ src/test/java/seedu/address/model/group/GroupTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUP_NAME_NUS_DATA_SCIENCE_SOCIETY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUP_NAME_NUS_FINTECH_SOCIETY;
+import static seedu.address.testutil.TypicalGroups.NUS_DATA_SCIENCE_SOCIETY;
 import static seedu.address.testutil.TypicalGroups.NUS_FINTECH_SOCIETY;
 
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,29 @@ public class GroupTest {
         String nameWithTrailingSpaces = VALID_GROUP_NAME_NUS_FINTECH_SOCIETY + " ";
         editedNusFintechSociety = new GroupBuilder(NUS_FINTECH_SOCIETY).withGroupName(nameWithTrailingSpaces).build();
         assertFalse(NUS_FINTECH_SOCIETY.isSameGroup(editedNusFintechSociety));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Group nusFintechSocietyCopy = new GroupBuilder(NUS_FINTECH_SOCIETY).build();
+        assertTrue(NUS_FINTECH_SOCIETY.equals(nusFintechSocietyCopy));
+
+        // same object -> returns true
+        assertTrue(NUS_FINTECH_SOCIETY.equals(NUS_FINTECH_SOCIETY));
+
+        // null -> returns false
+        assertFalse(NUS_FINTECH_SOCIETY.equals(null));
+
+        // different type -> returns false
+        assertFalse(NUS_FINTECH_SOCIETY.equals(5));
+
+        // different group -> returns false
+        assertFalse(NUS_FINTECH_SOCIETY.equals(NUS_DATA_SCIENCE_SOCIETY));
+
+        // different groupName -> returns false
+        Group editedNusFintechSociety = new GroupBuilder(NUS_FINTECH_SOCIETY)
+                .withGroupName(VALID_GROUP_NAME_NUS_DATA_SCIENCE_SOCIETY).build();
+        assertFalse(NUS_FINTECH_SOCIETY.equals(editedNusFintechSociety));
     }
 }
