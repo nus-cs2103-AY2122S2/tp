bf8a7c3d75d2325fb6b9ed7b73128ba2afbfa042
Benjykoh <benjaminkoh0909@gmail.com>
2022-03-23 20:04:49 +0800

    Update ManualCommandTest.java
    Updated method names to include "success" and "failure" at the end
    to make it sound clearer.

--- src/test/java/seedu/address/logic/commands/ManualCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ManualCommandTest.java
@@ -15,96 +15,96 @@ public class ManualCommandTest {
     private Model expectedModel = new ModelManager();
 
     @Test
-    public void execute_manual_all() {
+    public void execute_manualAll_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ALL_COMMANDS);
         assertCommandSuccess(new ManualCommand(""), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_add() {
+    public void execute_manualAdd_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ADD_COMMAND);
         assertCommandSuccess(new ManualCommand("add"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_delete() {
+    public void execute_manualDelete_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_DELETE_COMMAND);
         assertCommandSuccess(new ManualCommand("delete"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_find() {
+    public void execute_manualFind_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_FIND_COMMAND);
         assertCommandSuccess(new ManualCommand("find"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_edit() {
+    public void execute_manualEdit_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_EDIT_COMMAND);
         assertCommandSuccess(new ManualCommand("edit"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_clear() {
+    public void execute_manualClear_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_CLEAR_COMMAND);
         assertCommandSuccess(new ManualCommand("clear"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_list() {
+    public void execute_manualList_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_LIST_COMMAND);
         assertCommandSuccess(new ManualCommand("list"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_manual() {
+    public void execute_manualManual_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_MANUAL_COMMAND);
         assertCommandSuccess(new ManualCommand("manual"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_help() {
+    public void execute_manualHelp_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_HELP_COMMAND);
         assertCommandSuccess(new ManualCommand("help"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_exit() {
+    public void execute_manualExit_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_EXIT_COMMAND);
         assertCommandSuccess(new ManualCommand("exit"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_task() {
+    public void execute_manualTask_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_TASK_COMMAND);
         assertCommandSuccess(new ManualCommand("task"), model, expectedCommandResult, expectedModel);
     }
     @Test
-    public void execute_manual_mark() {
+    public void execute_manualMark_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_MARK_COMMAND);
         assertCommandSuccess(new ManualCommand("mark"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_unmark() {
+    public void execute_manualUnmark_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_UNMARK_COMMAND);
         assertCommandSuccess(new ManualCommand("unmark"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_archive() {
+    public void execute_manualArchive_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ARCHIVE_COMMAND);
         assertCommandSuccess(new ManualCommand("archive"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_assign() {
+    public void execute_manualAssign_success() {
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ASSIGN_COMMAND);
         assertCommandSuccess(new ManualCommand("assign"), model, expectedCommandResult, expectedModel);
     }
 
     @Test
-    public void execute_manual_invalid() {
+    public void execute_manualInvalid_success() {
         ManualCommand manualCommand = new ManualCommand(INVALID_COMMAND_NAME);
         CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_UNKNOWN_COMMANDS);
         assertCommandFailure(manualCommand, model, ManualMessages.MANUAL_MESSAGE_UNKNOWN_COMMANDS);
