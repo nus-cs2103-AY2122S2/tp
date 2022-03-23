5da71f2426916217977c07cc19caa4733e012a48
Benjykoh <benjaminkoh0909@gmail.com>
2022-03-23 18:34:55 +0800

    Add test cases for manual command
    Added test cases for manual command, handling all success cases and
    one failure test case.

---
+++ src/test/java/seedu/address/commons/core/ManualMessages.java
@@ -0,0 +1,95 @@
+package seedu.address.commons.core;
+
+/**
+ * Container for user visible messages regarding the manual commands.
+ */
+
+public class ManualMessages {
+
+    public static final String MANUAL_MESSAGE_ADD_COMMAND = "Adds a student to TAPA. \n"
+            + "Format: add i/STUDENT_ID n/STUDENT_NAME m/MODULE_CODE [p/PHONE_NUMBER] [h/TELEGRAM_HANDLE] "
+            + "[e/EMAIL_ADDRESS] \n"
+            + "Example: add i/A6942069R n/john m/CS2103T p/98765432 t/johnnn e/e0123456@u.nus.edu";
+
+    public static final String MANUAL_MESSAGE_DELETE_COMMAND = "Deletes a student from TAPA. \n"
+            + "Format: delete STUDENT_INDEX \n"
+            + "Example: delete 1";
+
+    public static final String MANUAL_MESSAGE_FIND_COMMAND = "Allows the user to look up the details of a particular "
+            + "student. \n"
+            + "Format: find n/STUDENT_NAME (or) find i/STUDENT_ID \n"
+            + "Example: find n/John (or) find i/AXXXXXXXR";
+
+    public static final String MANUAL_MESSAGE_EDIT_COMMAND = "Edits a student's information in TAPA. \n"
+            + "Format : edit STUDENT_INDEX [i/STUDENT_ID] [n/STUDENT_NAME] [m/MODULE_CODE] [p/PHONE_NUMBER] "
+            + "[h/TELEGRAM_HANDLE] [e/EMAIL_ADDRESS] \n"
+            + "Example: edit 1 m/CS2103T p/98765432 t/johnnn e/e0123456z@u.nus.edu";
+
+    public static final String MANUAL_MESSAGE_CLEAR_COMMAND = "Clears all students from TAPA. "
+            + "Additionally, the user will be prompted to confirm clear command. \n"
+            + "Format : clear \n"
+            + "Example: clear";
+
+    public static final String MANUAL_MESSAGE_LIST_COMMAND = "Displays all the students enrolled in a list. \n"
+            + "Format : list \n"
+            + "Example: list";
+
+    public static final String MANUAL_MESSAGE_MANUAL_COMMAND = "Display the format for a specified command and a short"
+            + " description for a particular command. \n"
+            + "Format : manual [COMMAND_NAME] \n"
+            + "Example: manual add \n"
+            + "Kind of weird that you have to use 'manual'... to find out what 'manual' does... :( "
+            + "Trying to find a bug?";
+
+    public static final String MANUAL_MESSAGE_HELP_COMMAND = "Shows a pop-up window explaining how to access the user "
+            + "guide. \n"
+            + "Format : help \n"
+            + "Example: help";
+
+    public static final String MANUAL_MESSAGE_EXIT_COMMAND = "Exits the program. \n"
+            + "Format : exit \n"
+            + "Example: exit";
+
+    public static final String MANUAL_MESSAGE_TASK_COMMAND = "Displays all the tasks that are allocated to a "
+            + "particular student. \n"
+            + "Format: task i/STUDENT_ID \n"
+            + "Example: task i/A6942069R";
+
+    public static final String MANUAL_MESSAGE_MARK_COMMAND = "Marks a specific undone task as done for a particular "
+            + "student. \n"
+            + "Format : mark i/STUDENT_ID UNDONE_TASK_INDEX \n"
+            + "Example: mark i/A6942069R 1";
+
+    public static final String MANUAL_MESSAGE_UNMARK_COMMAND = "Marks a specific done task as undone for a particular "
+            + "student. \n"
+            + "Format : unmark i/STUDENT_ID DONE_TASK_INDEX \n"
+            + "Example: unmark i/A6942069R 1";
+
+    public static final String MANUAL_MESSAGE_ARCHIVE_COMMAND = "Saves a copy of the details currently saved in the "
+            + "address book into a separate file. \n"
+            + "Format : archive \n"
+            + "Example: archive";
+
+    public static final String MANUAL_MESSAGE_ASSIGN_COMMAND = "Assigns a task to a particular student. \n"
+            + "Format : assign i/STUDENT_ID tn/TASK_NAME \n"
+            + "Example: assign i/A6942069R tn/Watch Lecture";
+
+    public static final String MANUAL_MESSAGE_ALL_COMMANDS = "Here are all the commands: \n"
+            + "1. add \n"
+            + "2. delete \n"
+            + "3. find \n"
+            + "4. task \n"
+            + "5. mark \n"
+            + "6. unmark \n"
+            + "7. edit \n"
+            + "8. clear \n"
+            + "9. archive \n"
+            + "10. list \n"
+            + "11. assign \n"
+            + "12. manual \n"
+            + "13. help \n"
+            + "14. exit \n"
+            + "Use 'manual [COMMAND_NAME] to display the format and a short description for the specified command";
+
+    public static final String MANUAL_MESSAGE_UNKNOWN_COMMANDS = "Unknown command!";
+}
---
+++ src/test/java/seedu/address/logic/commands/ManualCommandTest.java
@@ -0,0 +1,113 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.ManualMessages;
+
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_COMMAND_NAME;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class ManualCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_manual_all() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ALL_COMMANDS);
+        assertCommandSuccess(new ManualCommand(""), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_add() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ADD_COMMAND);
+        assertCommandSuccess(new ManualCommand("add"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_delete() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_DELETE_COMMAND);
+        assertCommandSuccess(new ManualCommand("delete"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_find() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_FIND_COMMAND);
+        assertCommandSuccess(new ManualCommand("find"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_edit() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_EDIT_COMMAND);
+        assertCommandSuccess(new ManualCommand("edit"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_clear() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_CLEAR_COMMAND);
+        assertCommandSuccess(new ManualCommand("clear"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_list() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_LIST_COMMAND);
+        assertCommandSuccess(new ManualCommand("list"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_manual() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_MANUAL_COMMAND);
+        assertCommandSuccess(new ManualCommand("manual"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_help() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_HELP_COMMAND);
+        assertCommandSuccess(new ManualCommand("help"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_exit() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_EXIT_COMMAND);
+        assertCommandSuccess(new ManualCommand("exit"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_task() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_TASK_COMMAND);
+        assertCommandSuccess(new ManualCommand("task"), model, expectedCommandResult, expectedModel);
+    }
+    @Test
+    public void execute_manual_mark() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_MARK_COMMAND);
+        assertCommandSuccess(new ManualCommand("mark"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_unmark() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_UNMARK_COMMAND);
+        assertCommandSuccess(new ManualCommand("unmark"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_archive() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ARCHIVE_COMMAND);
+        assertCommandSuccess(new ManualCommand("archive"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_assign() {
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_ASSIGN_COMMAND);
+        assertCommandSuccess(new ManualCommand("assign"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_manual_invalid() {
+        ManualCommand manualCommand = new ManualCommand(INVALID_COMMAND_NAME);
+        CommandResult expectedCommandResult = new CommandResult(ManualMessages.MANUAL_MESSAGE_UNKNOWN_COMMANDS);
+        assertCommandFailure(manualCommand, model, ManualMessages.MANUAL_MESSAGE_UNKNOWN_COMMANDS);
+    }
+}
