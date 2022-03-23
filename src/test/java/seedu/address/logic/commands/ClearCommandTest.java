f6db13cea93bb63c895d95f0296eac80a9621366
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-03-23 22:35:10 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -6,7 +6,11 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.*;
+import seedu.address.model.AddressBook;
+import seedu.address.model.InterviewSchedule;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
 
