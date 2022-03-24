70d204b33a63deb34d0d8b2873c14886c598ff0c
Daffa Fathani Adila <daffa26@gmail.com>
2022-03-25 02:22:27 +0800

    modify transactionlist

--- src/main/java/seedu/address/model/transaction/TransactionList.java
+++ src/main/java/seedu/address/model/transaction/TransactionList.java
@@ -1,18 +1,14 @@
-package seedu.address.model.person;
+package seedu.address.model.transaction;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.model.transaction.Transaction;
-import seedu.address.ui.TransactionCard;
 
 /**
  * A list of Transactions that does not allow nulls.
