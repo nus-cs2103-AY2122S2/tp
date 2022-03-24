bed9279dabb91f0456b3bf3102d32e7fd82134ec
Korochii <yanlong21@live.com.sg>
2022-03-24 22:03:18 +0800

    Fix testcase in ClearCommandTest.java

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,25 +1,37 @@
 package seedu.address.logic.commands;
 
-public class ClearCommandTest {
-    // TODO: Fix this testcase
-    //    @Test
-    //    public void execute_emptyAddressBook_success() {
-    //        Model model = new ModelManager();
-    //        Model expectedModel = new ModelManager();
-    //
-    //        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_REQUEST_USER_CONFIRMATION,
-    //          expectedModel);
-    //    }
+import org.junit.jupiter.api.Test;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
-    // TODO: Fix this testcase
-    //    @Test
-    //    public void execute_nonEmptyAddressBook_success() {
-    //        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    //        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    //        expectedModel.setAddressBook(new AddressBook());
-    //
-    //        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_REQUEST_USER_CONFIRMATION,
-    //          expectedModel);
-    //    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+public class ClearCommandTest {
+    @Test
+    public void execute_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        CommandResult expectedCommandResult
+                = new CommandResult(ClearCommand.MESSAGE_REQUEST_USER_CONFIRMATION, true);
+
+        assertCommandSuccess(new ClearCommand(), model, expectedCommandResult,
+          expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        CommandResult result = new ClearCommand().execute(model);
+        CommandResult expectedCommandResult
+                = new CommandResult(ClearCommand.MESSAGE_REQUEST_USER_CONFIRMATION, true);
+
+        assertEquals(expectedCommandResult, result);
+    }
 
 }
