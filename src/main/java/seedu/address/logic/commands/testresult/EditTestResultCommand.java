7d644f13081be31663cc7d7cac545c54aab36eae
Joey Chance <92976979+joey-chance@users.noreply.github.com>
2022-03-25 18:22:52 +0800

    Update src/main/java/seedu/address/logic/commands/testresult/EditTestResultCommand.java
    Co-authored-by: Ho Si Binh <97666199+sibinhho99-nus@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/testresult/EditTestResultCommand.java
+++ src/main/java/seedu/address/logic/commands/testresult/EditTestResultCommand.java
@@ -31,7 +31,7 @@ public class EditTestResultCommand extends Command {
     public static final CommandType COMMAND_TYPE = CommandType.TEST;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Edits the details of the medical information identified "
+            + ": Edits the details of the test result identified "
             + "by the index number used in the displayed test result information list. "
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
