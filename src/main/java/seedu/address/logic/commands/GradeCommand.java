d3045f4e9527a785ac2ff1009eaf9e2cdbe1367e
lkldev <lkldev@outlook.com>
2022-03-26 01:29:50 +0800

    Add missing field to grade command

--- src/main/java/seedu/address/logic/commands/GradeCommand.java
+++ src/main/java/seedu/address/logic/commands/GradeCommand.java
@@ -5,20 +5,23 @@ import java.util.Optional;
 
 import seedu.address.model.Model;
 import seedu.address.model.assessment.Assessment;
+import seedu.address.model.assessment.Grade;
 import seedu.address.model.student.Student;
 
 public class GradeCommand extends Command {
 
     private Assessment assessment;
+    private Optional<Grade> grade;
     private Optional<List<Student>> students;
     private boolean isAllStudents;
 
     /**
      * Creates an EnrollCommand to enroll the students into class group and module
      */
-    public GradeCommand(Assessment assessment, Optional<List<Student>> students,
+    public GradeCommand(Assessment assessment, Optional<Grade> grade, Optional<List<Student>> students,
                         boolean isAllStudents) {
         this.assessment = assessment;
+        this.grade = grade;
         this.students = students;
         this.isAllStudents = isAllStudents;
     }
