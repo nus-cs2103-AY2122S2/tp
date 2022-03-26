6677c162fec78a5d0cd4c7755d8d187fdd98a7aa
Dane Bagaoisan <dane0515@gmail.com>
2022-03-26 21:20:40 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -128,24 +128,25 @@ public class CommandBox extends UiPart<Region> {
     private int editDistance(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
-        int [][]DP = new int[2][len1 + 1];
+        int [][]dp = new int[2][len1 + 1];
 
-        for (int i = 0; i <= len1; i++)
-            DP[0][i] = i;
+        for (int i = 0; i <= len1; i++) {
+            dp[0][i] = i;
+        }
 
         for (int i = 1; i <= len2; i++) {
             for (int j = 0; j <= len1; j++) {
                 if (j == 0) {
-                    DP[i % 2][j] = i;
+                    dp[i % 2][j] = i;
                 } else if (str1.charAt(j - 1) == str2.charAt(i - 1)) {
-                    DP[i % 2][j] = DP[(i - 1) % 2][j - 1];
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
                 } else {
-                    DP[i % 2][j] = 1 + Math.min(DP[(i - 1) % 2][j], Math.min(DP[i % 2][j - 1], DP[(i - 1) % 2][j - 1]));
+                    dp[i % 2][j] = 1 + Math.min(dp[(i - 1) % 2][j], Math.min(dp[i % 2][j - 1], dp[(i - 1) % 2][j - 1]));
                 }
             }
         }
 
-        return DP[len2 % 2][len1];
+        return dp[len2 % 2][len1];
     }
 
     /**
