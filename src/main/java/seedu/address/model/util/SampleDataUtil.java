614e56418a00c708e86c375919aca251778d4c92
Teo Hao Yu <thstoneyes@gmail.com>
2022-03-26 17:00:28 +0800

    Add spacing between UI elements

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,22 +28,22 @@ public class SampleDataUtil {
         return new Pet[] {
             new Pet(new Name("Bagel"), new OwnerName("Alex Yeo"), new Phone("87438807"),
                     new Address("Blk 30 Geylang Street 29, #06-40"),
-                    getTagSet("friends"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
+                    getTagSet("Japanese Spitz"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
             new Pet(new Name("Tofu"), new OwnerName("Bernice Yu"), new Phone("99272758"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("colleagues", "friends"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
+                    getTagSet("Golden Retriever"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
             new Pet(new Name("PeePee"), new OwnerName("Charlotte Oliveiro"), new Phone("93210283"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("neighbours"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
+                    getTagSet("Bulldog"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
             new Pet(new Name("Waffle"), new OwnerName("David Li"), new Phone("91031282"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("family"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
+                    getTagSet("British Shorthair"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
             new Pet(new Name("Sesame"), new OwnerName("Irfan Ibrahim"), new Phone("92492021"),
                     new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("classmates"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
+                    getTagSet("Poodle"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap()),
             new Pet(new Name("Avocado"), new OwnerName("Roy Balakrishnan"), new Phone("92624417"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("colleagues"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap())
+                    getTagSet("Dachshund"), EMPTY_DIET, EMPTY_APPOINTMENT, new AttendanceHashMap())
         };
     }
 
