d7e9e93aa42ac8fee79298751bd1c952331d3560
tiewweijian <tiewweijian@u.nus.edu>
2022-03-27 01:08:36 +0800

    Fix checkstyle violations

--- src/test/java/seedu/address/logic/commands/FocusCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FocusCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalCandidates.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalInterviews.getTypicalInterviewSchedule;
@@ -11,7 +10,11 @@ import static seedu.address.testutil.TypicalInterviews.getTypicalInterviewSchedu
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.model.*;
+import seedu.address.model.AddressBook;
+import seedu.address.model.InterviewSchedule;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 
 public class FocusCommandTest {
