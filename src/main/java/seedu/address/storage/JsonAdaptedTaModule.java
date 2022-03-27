ee156dfd9c749ca4345eae31f6a63cfce9d6eacb
Tan Jia Xin <e0556267@u.nus.edu>
2022-03-27 11:46:38 +0800

    Update JsonAdaptedModule to store a list of Student

--- src/main/java/seedu/address/storage/JsonAdaptedTaModule.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTaModule.java
@@ -1,9 +1,14 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.Student;
 import seedu.address.model.tamodule.AcademicYear;
 import seedu.address.model.tamodule.ModuleCode;
 import seedu.address.model.tamodule.ModuleName;
@@ -15,10 +20,12 @@ import seedu.address.model.tamodule.TaModule;
 class JsonAdaptedTaModule {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "TaModule's %s field is missing!";
+    public static final String NONEXISTENT_STUDENT = "Student does not exist!";
 
     private final String moduleName;
     private final String moduleCode;
     private final String academicYear;
+    private final List<JsonAdaptedStudent> students = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedTaModule} with the given module details.
@@ -26,10 +33,14 @@ class JsonAdaptedTaModule {
     @JsonCreator
     public JsonAdaptedTaModule(@JsonProperty("moduleName") String moduleName,
                                @JsonProperty("moduleCode") String moduleCode,
-                               @JsonProperty("academicYear") String academicYear) {
+                               @JsonProperty("academicYear") String academicYear,
+                               @JsonProperty("students") List<JsonAdaptedStudent> students) {
         this.moduleName = moduleName;
         this.moduleCode = moduleCode;
         this.academicYear = academicYear;
+        if (students != null) {
+            this.students.addAll(students);
+        }
     }
 
     /**
@@ -39,14 +50,18 @@ class JsonAdaptedTaModule {
         moduleName = source.getModuleName().value;
         moduleCode = source.getModuleCode().value;
         academicYear = source.getAcademicYear().value;
+        students.addAll(source.getStudents().stream()
+                .map(JsonAdaptedStudent::new)
+                .collect(Collectors.toList()));
     }
 
     /*
      * Converts this Jackson-friendly adapted TAModule object into the model's {@code TaModule} object.
+     * Checks that the student tied to the module already exists.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted module.
      */
-    public TaModule toModelType() throws IllegalValueException {
+    public TaModule toModelType(List<Student> studentList) throws IllegalValueException {
         if (moduleName == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     ModuleName.class.getSimpleName()));
@@ -74,7 +89,15 @@ class JsonAdaptedTaModule {
         }
         final AcademicYear modelAcademicYear = new AcademicYear(academicYear);
 
-        return new TaModule(modelModuleName, modelModuleCode, modelAcademicYear);
-    }
+        final List<Student> modelStudents = new ArrayList<>();
+        for (JsonAdaptedStudent s : students) {
+            Student sObj = s.toModelType();
+            if (!studentList.contains(s)) {
+                throw new IllegalValueException(NONEXISTENT_STUDENT);
+            }
+            modelStudents.add(sObj);
+        }
 
+        return new TaModule(modelModuleName, modelModuleCode, modelAcademicYear, modelStudents);
+    }
 }
