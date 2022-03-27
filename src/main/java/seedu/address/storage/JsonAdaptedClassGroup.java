6c6c614cef7566a99782ffef7b7873c58fa062a0
Tan Jia Xin <e0556267@u.nus.edu>
2022-03-27 11:56:50 +0800

    Update JsonAdaptedClassGroup to store a list of Student

--- src/main/java/seedu/address/storage/JsonAdaptedClassGroup.java
+++ src/main/java/seedu/address/storage/JsonAdaptedClassGroup.java
@@ -1,6 +1,8 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,6 +11,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.classgroup.ClassGroup;
 import seedu.address.model.classgroup.ClassGroupId;
 import seedu.address.model.classgroup.ClassGroupType;
+import seedu.address.model.student.Student;
 import seedu.address.model.tamodule.TaModule;
 
 //@@author jxt00
@@ -19,21 +22,27 @@ class JsonAdaptedClassGroup {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "ClassGroup's %s field is missing!";
     public static final String NONEXISTENT_MODULE = "Module does not exist!";
+    public static final String NONEXISTENT_STUDENT = "Student does not exist!";
 
     private final String classGroupId;
     private final String classGroupType;
     private final JsonAdaptedTaModule module;
+    private final List<JsonAdaptedStudent> students = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedClassGroup} with the given class group details.
      */
     @JsonCreator
     public JsonAdaptedClassGroup(@JsonProperty("classGroupId") String classGroupId,
-            @JsonProperty("classGroupType") String classGroupType,
-            @JsonProperty("module") JsonAdaptedTaModule module) {
+                                 @JsonProperty("classGroupType") String classGroupType,
+                                 @JsonProperty("module") JsonAdaptedTaModule module,
+                                 @JsonProperty("students") List<JsonAdaptedStudent> students) {
         this.classGroupId = classGroupId;
         this.classGroupType = classGroupType;
         this.module = module;
+        if (students != null) {
+            this.students.addAll(students);
+        }
     }
 
     /**
@@ -43,15 +52,19 @@ class JsonAdaptedClassGroup {
         classGroupId = source.getClassGroupId().value;
         classGroupType = source.getClassGroupType().toString();
         module = new JsonAdaptedTaModule(source.getModule());
+        students.addAll(source.getStudents().stream()
+                .map(JsonAdaptedStudent::new)
+                .collect(Collectors.toList()));
     }
 
     /**
      * Converts this Jackson-friendly adapted class group object into the model's {@code ClassGroup} object.
      * Checks that the module the class group is added to already exists.
+     * Checks that the student tied to the class group already exists.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted class group.
      */
-    public ClassGroup toModelType(List<TaModule> taModuleList) throws IllegalValueException {
+    public ClassGroup toModelType(List<TaModule> taModuleList, List<Student> studentList) throws IllegalValueException {
         if (classGroupId == null) {
             throw new IllegalValueException(
                     String.format(MISSING_FIELD_MESSAGE_FORMAT, ClassGroupId.class.getSimpleName()));
@@ -92,6 +105,15 @@ class JsonAdaptedClassGroup {
             throw new IllegalValueException(NONEXISTENT_MODULE);
         }
 
-        return new ClassGroup(modelClassGroupId, modelClassGroupType, modelModule);
+        final List<Student> modelStudents = new ArrayList<>();
+        for (JsonAdaptedStudent s : students) {
+            Student sObj = s.toModelType();
+            if (!studentList.contains(s)) {
+                throw new IllegalValueException(NONEXISTENT_STUDENT);
+            }
+            modelStudents.add(sObj);
+        }
+
+        return new ClassGroup(modelClassGroupId, modelClassGroupType, modelModule, modelStudents);
     }
 }
