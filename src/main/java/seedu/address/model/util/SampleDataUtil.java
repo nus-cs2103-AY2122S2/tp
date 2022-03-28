08c054be7f1b77ba351bf6e1d1c3b2bf047a2aaa
Frederino <failfred98@gmail.com>
2022-03-28 16:24:14 +0800

    Update UG

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,23 +1,45 @@
 package seedu.address.model.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.event.Event;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Cca;
+import seedu.address.model.person.Education;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Internship;
+import seedu.address.model.person.Module;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
+    private static List<Tag> educations = new ArrayList<>();
+    private static List<Tag> ccas = new ArrayList<>();
+    private static List<Tag> modules = new ArrayList<>();
+    private static List<Tag> internships = new ArrayList<>();
+
     public static Person[] getSamplePersons() {
+        educations.add(new Education("computer science"));
+        ccas.add(new Cca("bouldering"));
+        modules.add(new Module("cs2040s"));
+        modules.add(new Module("cs2030s"));
+        internships.add(new Internship("shopee"));
+
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40")),
+                new Address("Blk 30 Geylang Street 29, #06-40"), educations, new ArrayList<>(),
+                    modules, ccas),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18")),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new ArrayList<>(), internships,
+                    new ArrayList<>(), new ArrayList<>()),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
