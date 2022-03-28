9b897238b8eff2fbc4ef7b0e01bde1afe40cd1c7
dineshraj555 <e0652525@u.nus.edu>
2022-03-28 20:19:24 +0800

    Add comparator classes to compare pet drop off and pick up time

---
+++ src/main/java/seedu/address/model/pet/comparator/PetDropOffTimeComparator.java
@@ -0,0 +1,52 @@
+package seedu.address.model.pet.comparator;
+
+import seedu.address.model.attendance.AttendanceEntry;
+import seedu.address.model.attendance.PresentAttendanceEntry;
+import seedu.address.model.pet.Pet;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Comparator;
+
+/**
+ * Comparator class to compare pets based on pet's Drop Off Time.
+ */
+public class PetDropOffTimeComparator implements Comparator<Pet> {
+    LocalDate today = LocalDate.now();
+
+    /**
+     * Compares pet objects based on their drop off time attribute.
+     * @param first Pet to be compared with.
+     * @param second Pet to be compared with.
+     * @return Value signifying in the difference between the comparing attribute.
+     */
+    @Override
+    public int compare(Pet first, Pet second) {
+        AttendanceEntry firstAttendanceEntry = first.getAttendanceHashMap().getAttendanceEntry(today);
+        AttendanceEntry secondAttendanceEntry = second.getAttendanceHashMap().getAttendanceEntry(today);
+
+        boolean isFirstPresent = firstAttendanceEntry instanceof PresentAttendanceEntry;
+        boolean isSecondPresent = secondAttendanceEntry instanceof PresentAttendanceEntry;
+
+        if (isFirstPresent && isSecondPresent) {
+            LocalTime firstDropOffTime = firstAttendanceEntry.getDropOffTime().get();
+            LocalTime secondDropOffTime = secondAttendanceEntry.getDropOffTime().get();
+
+            if (firstDropOffTime != null && secondDropOffTime != null) {
+                return firstDropOffTime.compareTo(secondDropOffTime);
+            } else if (firstDropOffTime != null && secondDropOffTime == null) {
+                return 1;
+            } else if (firstDropOffTime == null && secondDropOffTime != null) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else if (isFirstPresent && !isSecondPresent) {
+            return -1;
+        } else if (!isFirstPresent && isSecondPresent) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/seedu/address/model/pet/comparator/PetPickUpTimeComparator.java
@@ -0,0 +1,53 @@
+package seedu.address.model.pet.comparator;
+
+import seedu.address.model.attendance.AttendanceEntry;
+import seedu.address.model.attendance.PresentAttendanceEntry;
+import seedu.address.model.pet.Pet;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Comparator;
+
+/**
+ * Comparator class to compare pets based on pet's Pick Up Time.
+ */
+public class PetPickUpTimeComparator implements Comparator<Pet> {
+    LocalDate today = LocalDate.now();
+
+    /**
+     * Compares pet objects based on their pick up time attribute.
+     * @param first Pet to be compared with.
+     * @param second Pet to be compared with.
+     * @return Value signifying in the difference between the comparing attribute.
+     */
+    @Override
+    public int compare(Pet first, Pet second) {
+        AttendanceEntry firstAttendanceEntry = first.getAttendanceHashMap().getAttendanceEntry(today);
+        AttendanceEntry secondAttendanceEntry = second.getAttendanceHashMap().getAttendanceEntry(today);
+
+        boolean isFirstPresent = firstAttendanceEntry instanceof PresentAttendanceEntry;
+        boolean isSecondPresent = secondAttendanceEntry instanceof PresentAttendanceEntry;
+
+        if (isFirstPresent && isSecondPresent) {
+            LocalTime firstPickUpTime = firstAttendanceEntry.getPickUpTime().get();
+            LocalTime secondPickUpTime = secondAttendanceEntry.getPickUpTime().get();
+
+            if (firstPickUpTime != null && secondPickUpTime != null) {
+                return firstPickUpTime.compareTo(secondPickUpTime);
+            } else if (firstPickUpTime != null && secondPickUpTime == null) {
+                return 1;
+            } else if (firstPickUpTime == null && secondPickUpTime != null) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else if (isFirstPresent && !isSecondPresent) {
+            return -1;
+        } else if (!isFirstPresent && isSecondPresent) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
