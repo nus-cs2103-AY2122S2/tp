bc1bd46c837aaa55b68ec763d82eed006229ffa8
Albert Sutiono <albertsutz123@gmail.com>
2022-03-28 22:22:20 +0800

    Improve logic part of FindCompanyCommand to include searching for tags

--- src/main/java/seedu/address/logic/commands/FindCompanyCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCompanyCommand.java
@@ -1,33 +1,59 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.model.Model;
-import seedu.address.model.entry.predicate.NameContainsKeywordsPredicate;
+import seedu.address.model.entry.predicate.CompanyContainsKeywordsPredicate;
+
+import java.util.Arrays;
 
 /**
- * Finds and lists all companies in address book whose Name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Keyword matching is case insensitive
+ * Find company command uses User input to specify what attributes (name and/or tags) the displayed companies
+ * should have.
+ * The relationship between attributes is "AND" while the relationship between keywords of the same attribute
+ * is "OR".
+ * For example, "finde n/ shopsg dbsss" will return shopsg and dbsss companies. Furthermore, "finde t/ applied test"
+ * will return all companies that has tags "applied" or "test".
  */
 public class FindCompanyCommand extends Command {
 
     public static final String COMMAND_WORD = "findc";
+    public static final String MESSAGE_NOT_QUERIED = "At least one field to find must be provided.";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all companies whose Name contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " shopee";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds companies with given details of the company "
+            + "by name and tag "
+            + "Parameters: "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + " sgshop dbsss ";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final ArgumentMultimap argumentMultimap;
 
-    public FindCompanyCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    /**
+     * Constructs FindCompanyCommand object
+     * @param argumentMultimap A hashmap containing company prefixes and its value from user input
+     */
+    public FindCompanyCommand(ArgumentMultimap argumentMultimap) {
+        requireNonNull(argumentMultimap);
+        this.argumentMultimap = argumentMultimap;
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        String[] nameKeywords = argumentMultimap.getValue(PREFIX_NAME).orElse("").split("\\s+");
+        String[] tagKeywords = argumentMultimap.getValue(PREFIX_TAG).orElse("").split("\\s+");
+
+        CompanyContainsKeywordsPredicate predicate = new CompanyContainsKeywordsPredicate(Arrays.asList(nameKeywords),
+                Arrays.asList(tagKeywords));
+
         model.updateFilteredCompanyList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_COMPANIES_LISTED_OVERVIEW, model.getFilteredCompanyList().size()),
@@ -38,6 +64,6 @@ public class FindCompanyCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCompanyCommand // instanceof handles nulls
-                && predicate.equals(((FindCompanyCommand) other).predicate)); // state check
+                && argumentMultimap.equals(((FindCompanyCommand) other).argumentMultimap)); // state check
     }
 }
