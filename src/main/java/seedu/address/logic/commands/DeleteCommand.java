e0fb40d90a6d6b9ae3ef9ae285fe375a4527b8b9
dionegoh <e0439283@u.nus.edu>
2022-03-29 17:57:24 +0800

    Make deletefriend extend from by ByIndexByNameCommand

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -3,24 +3,16 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
-import java.util.List;
-
-import javafx.collections.ObservableList;
-import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.person.FriendName;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
  */
-public class DeleteCommand extends Command {
+public class DeleteCommand extends ByIndexByNameCommand {
 
     public static final String COMMAND_WORD = "deletefriend";
 
@@ -33,12 +25,6 @@ public class DeleteCommand extends Command {
             + "Example 2: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted friend: %1$s";
-
-    //these dummy fields are needed to instantiate a person to be used to check if such a name entered exists
-    // in the address book
-    private static final Phone dummyPhone = new Phone("12345678");
-    private static final Email dummyEmail = new Email("dummyemail@gmail.com");
-    private static final Address dummyAddress = new Address("Dummy Address");
 
     private final FriendName nameOfPersonToDelete;
     private final Index targetIndex;
@@ -66,44 +52,21 @@ public class DeleteCommand extends Command {
         this.isDeletionByIndex = true;
     }
 
-
     @Override
     public CommandResult execute(Model model) throws CommandException {
 
         requireNonNull(model);
+        Person personToDelete;
 
         if (isDeletionByIndex) { //deletion by index
-            List<Person> lastShownList = model.getFilteredPersonList();
-            if (targetIndex.getZeroBased() >= lastShownList.size()) {
-                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-            }
-            Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
-            model.deletePerson(personToDelete);
-            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+            personToDelete = this.getPersonByFilteredIndex(model, targetIndex);
 
         } else { //deletion by name
-            Person personWithNameToDelete = new Person(nameOfPersonToDelete);
-
-            if (!model.hasPerson(personWithNameToDelete)) { //model.hasPerson considers 2 Persons with same name
-                //to be the same Person
-                throw new CommandException(Messages.MESSAGE_PERSON_DOES_NOT_EXIST);
-            }
-            //todo : make this more oop
-            ObservableList<Person> personList = model.getAddressBook().getPersonList();
-            Person personToDelete = personWithNameToDelete; //dummy person
-
-            for (Person personFromList : personList) {
-                if (personFromList.isSamePerson(personWithNameToDelete)) {
-                    personToDelete = new Person(personFromList.getName(),
-                            personFromList.getPhone(), personFromList.getEmail(),
-                            personFromList.getAddress(), personFromList.getDescription(),
-                            personFromList.getTags(), personFromList.getLogs());
-                    model.deletePerson(personFromList);
-                    break;
-                }
-            }
-            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+            personToDelete = this.getPersonByName(model, nameOfPersonToDelete);
         }
+        assert(personToDelete != null);
+        model.deletePerson(personToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
 
     @Override
