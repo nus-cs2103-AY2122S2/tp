10e680af46e37f00ab4c1dea769c2f5370fdb758
lowkaiwei98 <77216013+lowkaiwei98@users.noreply.github.com>
2022-03-29 20:21:49 +0800

    Fix minor typo

--- src/main/java/seedu/trackermon/logic/parser/ParserUtil.java
+++ src/main/java/seedu/trackermon/logic/parser/ParserUtil.java
@@ -23,7 +23,7 @@ import seedu.trackermon.model.tag.Tag;
 public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
-    public static final String MESSAGE_INVALID_ORDER = "Invalid Valid!! Need to asc or dsc. For example: n/asc";
+    public static final String MESSAGE_INVALID_ORDER = "Invalid value!! Need to asc or dsc. For example: n/asc";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
