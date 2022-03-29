3c5c43c1f377910b865f526cc64b634e1114b8dd
Zachary Chan <trollinsir@hotmail.com>
2022-03-29 21:27:03 +0800

    Fix Gradle Error
    Removed unused import.

--- src/test/java/seedu/trackermon/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/trackermon/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,5 @@
 package seedu.trackermon.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.trackermon.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.trackermon.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.trackermon.logic.parser.CommandParserTestUtil.assertParseSuccess;
