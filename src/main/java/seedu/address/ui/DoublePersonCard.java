0b66a4efcc6a88a0fd5808cfc87666e15a1ce061
cindyangXQ <yangc2105@gmail.com>
2022-03-30 19:14:10 +0800

    Update UI of MatchWindow
    MatchWindow does not show coloured UserType tags as the MainWindow does.
    
    MatchWindow does not show favourite status of Persons.
    
    Update UI accordingly to solve the problems.

--- src/main/java/seedu/address/ui/DoublePersonCard.java
+++ src/main/java/seedu/address/ui/DoublePersonCard.java
@@ -4,7 +4,6 @@ import java.util.StringJoiner;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 import seedu.address.model.property.Property;
@@ -29,7 +28,11 @@ public class DoublePersonCard extends UiPart<Region> {
     @FXML
     private Label preference;
     @FXML
-    private FlowPane userType;
+    private Label favourite;
+    @FXML
+    private Label tagBuyer;
+    @FXML
+    private Label tagSeller;
 
     @FXML
     private Label name2;
@@ -44,7 +47,11 @@ public class DoublePersonCard extends UiPart<Region> {
     @FXML
     private Label preference2;
     @FXML
-    private FlowPane userType2;
+    private Label favourite2;
+    @FXML
+    private Label tagBuyer2;
+    @FXML
+    private Label tagSeller2;
 
     /**
      * Creates a {@code DoublePersonCard} with the given two persons.
@@ -60,17 +67,35 @@ public class DoublePersonCard extends UiPart<Region> {
         address.setText(seller.getAddress().value);
         email.setText(seller.getEmail().value);
 
-        StringJoiner propertyJoiner = new StringJoiner("\n");
-        seller.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
-        property.setText(propertyJoiner.toString());
+        if (seller.getUserType().isBuyer()) {
+            tagBuyer.setText(seller.getUserType().value);
+            tagSeller.setVisible(false);
+            tagSeller.setManaged(false);
+        } else {
+            tagSeller.setText(seller.getUserType().value);
+            tagBuyer.setVisible(false);
+            tagBuyer.setManaged(false);
+        }
+
+        if (!seller.getFavourite().isUnfavourited()) {
+            favourite.setText(seller.getFavourite().toString());
+        } else {
+            favourite.setManaged(false);
+        }
+
+        if (seller.getProperties().isEmpty()) {
+            property.setManaged(false);
+        } else {
+            StringJoiner propertyJoiner = new StringJoiner("\n");
+            seller.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
+            property.setText(propertyJoiner.toString());
+        }
 
         if (seller.getPreference().isPresent()) {
             preference.setText(seller.getPreference().get().toString());
         } else {
-            preference.setVisible(false);
+            preference.setManaged(false);
         }
-
-        userType.getChildren().add(new Label(seller.getUserType().value));
 
         // buyer card
         name2.setText(buyer.getName().fullName);
@@ -78,16 +103,34 @@ public class DoublePersonCard extends UiPart<Region> {
         address2.setText(buyer.getAddress().value);
         email2.setText(buyer.getEmail().value);
 
-        StringJoiner propertyJoiner2 = new StringJoiner("\n");
-        buyer.getProperties().stream().map(Property::toString).forEach(propertyJoiner2::add);
-        property2.setText(propertyJoiner2.toString());
+        if (buyer.getUserType().isBuyer()) {
+            tagBuyer2.setText(buyer.getUserType().value);
+            tagSeller2.setVisible(false);
+            tagSeller2.setManaged(false);
+        } else {
+            tagSeller2.setText(buyer.getUserType().value);
+            tagBuyer2.setVisible(false);
+            tagBuyer2.setManaged(false);
+        }
+
+        if (!buyer.getFavourite().isUnfavourited()) {
+            favourite2.setText(buyer.getFavourite().toString());
+        } else {
+            favourite2.setManaged(false);
+        }
+
+        if (buyer.getProperties().isEmpty()) {
+            property2.setManaged(false);
+        } else {
+            StringJoiner propertyJoiner = new StringJoiner("\n");
+            buyer.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
+            property2.setText(propertyJoiner.toString());
+        }
 
         if (buyer.getPreference().isPresent()) {
             preference2.setText(buyer.getPreference().get().toString());
         } else {
-            preference2.setVisible(false);
+            preference2.setManaged(false);
         }
-
-        userType2.getChildren().add(new Label(buyer.getUserType().value));
     }
 }
