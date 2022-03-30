7c9374e92f1d3e50ea71a14464ab3275d186b112
Ashuh <ashuh223@gmail.com>
2022-03-30 19:11:33 +0800

    Change getFilteredPersonList() to getFilteredAndSortedPersonList()

--- src/main/java/seedu/address/logic/commands/UploadCommand.java
+++ src/main/java/seedu/address/logic/commands/UploadCommand.java
@@ -48,7 +48,7 @@ public class UploadCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
+        List<Person> lastShownList = model.getFilteredAndSortedPersonList();
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
