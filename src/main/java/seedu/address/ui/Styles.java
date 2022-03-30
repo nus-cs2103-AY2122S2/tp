2ff169d4d21d3f17329e949b7a9196af4a0f7411
LeeEnEn <leeenen@outlook.com>
2022-03-30 19:30:33 +0800

    Remove random color generator

--- src/main/java/seedu/address/ui/Styles.java
+++ src/main/java/seedu/address/ui/Styles.java
@@ -11,6 +11,35 @@ public class Styles {
     public static final String YELLOW = "#9D7A02";
     public static final String GREY = "#808080";
     public static final String BRIGHT_GREEN = "#227F0F";
+    public static final String[] COLORS = {
+        "#FDDFB1",
+        "#DE8B8B",
+        "#AA4758",
+        "#4FEA90",
+        "#AEE821",
+        "#D2E6ED",
+        "#9EE5EC",
+        "#F3C1DE",
+        "#DDA7AC",
+        "#BCC6E2",
+        "#EEB88B",
+        "#FBC6E2",
+        "#C1F3E5",
+        "#EFF3C1",
+        "#CEF3C1",
+        "#FDC1E8",
+        "#D4D9A1",
+        "#B9D0F3",
+        "#A1EBA6",
+        "#E6F34B",
+        "#F38B4B",
+        "#4BBCF3",
+        "#EFF2B3",
+        "#B9ED74",
+        "#E87021",
+        "#FE489A",
+        "#F7B5C5"
+    };
 
     // Font Style
     public static final String WHITE_FONT_INLINE = "-fx-text-fill: #FFFFFF;";
