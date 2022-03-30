b48251aa3d226ea7614cdb3b49d60d4a0ddd7f53
cindyangXQ <yangc2105@gmail.com>
2022-03-30 23:13:45 +0800

    Modify according to reviews

--- src/main/java/seedu/address/ui/DoublePersonCard.java
+++ src/main/java/seedu/address/ui/DoublePersonCard.java
@@ -16,23 +16,23 @@ public class DoublePersonCard extends UiPart<Region> {
     private Person buyer;
 
     @FXML
-    private Label name;
+    private Label name1;
     @FXML
-    private Label phone;
+    private Label phone1;
     @FXML
-    private Label address;
+    private Label address1;
     @FXML
-    private Label email;
+    private Label email1;
     @FXML
-    private Label property;
+    private Label property1;
     @FXML
-    private Label preference;
+    private Label preference1;
     @FXML
-    private Label favourite;
+    private Label favourite1;
     @FXML
-    private Label tagBuyer;
+    private Label tagBuyer1;
     @FXML
-    private Label tagSeller;
+    private Label tagSeller1;
 
     @FXML
     private Label name2;
@@ -58,79 +58,52 @@ public class DoublePersonCard extends UiPart<Region> {
      */
     public DoublePersonCard(Person seller, Person buyer) {
         super(FXML);
+
         this.seller = seller;
         this.buyer = buyer;
 
-        // seller card
-        name.setText(seller.getName().fullName);
-        phone.setText(seller.getPhone().value);
-        address.setText(seller.getAddress().value);
-        email.setText(seller.getEmail().value);
+        setPersonToCard(seller, name1, phone1, address1, email1,
+                property1, preference1, favourite1, tagBuyer1, tagSeller1);
+        setPersonToCard(buyer, name2, phone2, address2, email2,
+                property2, preference2, favourite2, tagBuyer2, tagSeller2);
+    }
 
-        if (seller.getUserType().isBuyer()) {
-            tagBuyer.setText(seller.getUserType().value);
+    private void setPersonToCard(Person person, Label name, Label phone, Label address, Label email,
+                                 Label property, Label preference, Label favourite, Label tagBuyer, Label tagSeller) {
+        name.setText(person.getName().fullName);
+        phone.setText(person.getPhone().value);
+        address.setText(person.getAddress().value);
+
+        email.setText(person.getEmail().value);
+
+        if (person.getUserType().isBuyer()) {
+            tagBuyer.setText(person.getUserType().value);
             tagSeller.setVisible(false);
             tagSeller.setManaged(false);
         } else {
-            tagSeller.setText(seller.getUserType().value);
+            tagSeller.setText(person.getUserType().value);
             tagBuyer.setVisible(false);
             tagBuyer.setManaged(false);
         }
 
-        if (!seller.getFavourite().isUnfavourited()) {
-            favourite.setText(seller.getFavourite().toString());
+        if (!person.getFavourite().isUnfavourited()) {
+            favourite.setText(person.getFavourite().toString());
         } else {
             favourite.setManaged(false);
         }
 
-        if (seller.getProperties().isEmpty()) {
+        if (person.getProperties().isEmpty()) {
             property.setManaged(false);
         } else {
             StringJoiner propertyJoiner = new StringJoiner("\n");
-            seller.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
+            person.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
             property.setText(propertyJoiner.toString());
         }
 
-        if (seller.getPreference().isPresent()) {
-            preference.setText(seller.getPreference().get().toString());
+        if (person.getPreference().isPresent()) {
+            preference.setText(person.getPreference().get().toString());
         } else {
             preference.setManaged(false);
-        }
-
-        // buyer card
-        name2.setText(buyer.getName().fullName);
-        phone2.setText(buyer.getPhone().value);
-        address2.setText(buyer.getAddress().value);
-        email2.setText(buyer.getEmail().value);
-
-        if (buyer.getUserType().isBuyer()) {
-            tagBuyer2.setText(buyer.getUserType().value);
-            tagSeller2.setVisible(false);
-            tagSeller2.setManaged(false);
-        } else {
-            tagSeller2.setText(buyer.getUserType().value);
-            tagBuyer2.setVisible(false);
-            tagBuyer2.setManaged(false);
-        }
-
-        if (!buyer.getFavourite().isUnfavourited()) {
-            favourite2.setText(buyer.getFavourite().toString());
-        } else {
-            favourite2.setManaged(false);
-        }
-
-        if (buyer.getProperties().isEmpty()) {
-            property2.setManaged(false);
-        } else {
-            StringJoiner propertyJoiner = new StringJoiner("\n");
-            buyer.getProperties().stream().map(Property::toString).forEach(propertyJoiner::add);
-            property2.setText(propertyJoiner.toString());
-        }
-
-        if (buyer.getPreference().isPresent()) {
-            preference2.setText(buyer.getPreference().get().toString());
-        } else {
-            preference2.setManaged(false);
         }
     }
 }
