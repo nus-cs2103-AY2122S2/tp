01c4f08e374309b85616ea1961d20fbe9421e362
jiale-c <cheejiale@gmail.com>
2022-03-30 20:51:11 +0800

    Add comment on the type of test being done and only contain tests that are white box testings.

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -22,30 +22,28 @@ public class DeleteCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
+        // Tests valid input number 1
         assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
+        // Tests invalid input alphabet
         assertParseFailure(parser, "a",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_invalidArgs_numberZero_throwsParseException() {
+        // Tests invalid input number 0
         assertParseFailure(parser, "0",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_invalidArgs_numberNegative_throwsParseException() {
+        // Tests invalid negative number
         assertParseFailure(parser, "-1",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_invalidArgs_numberAndLetter_throwsParseException() {
-        assertParseFailure(parser, "1a",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
