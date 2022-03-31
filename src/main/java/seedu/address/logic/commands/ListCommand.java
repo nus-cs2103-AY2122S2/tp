908543d502836ebda32a20a47d19587d6f91a682
Decaxical <e0544599@u.nus.edu>
2022-03-31 23:54:26 +0800

    Update ListCommand
    List Command is currently only used to display clients after running find and clients will always only see flagged clients at the top.
    
    Using list with an additional argument will allow users to view all flagged or unflagged clients
    
    Let's
    - add a parser for the list command
    - let list command take in a Predicate to run updateFilteredPersonList on
    - Update the test cases to reflect ParseException thrown with invalid argument

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,9 +1,12 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.function.Predicate;
 
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
 
 /**
  * Lists all persons in the hustle book to the user.
@@ -16,11 +19,15 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    private final Predicate<Person> listFilterPredicate;
+    public ListCommand(Predicate<Person> listFilterPredicate) {
+        this.listFilterPredicate = listFilterPredicate;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredPersonList(listFilterPredicate);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,49 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Person;
+
+
+
+public class ListCommandParser implements Parser<ListCommand> {
+
+    public static final Predicate<Person> PREDICATE_SHOW_FLAGGED_PERSONS = person -> person.getFlag().isFlagged;
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SortCommand
+     * and returns a Sort Command object for execution.
+     *
+     * @param args arguments to be parsed for SortCommand.
+     * @return SortCommand with parsed Comparator.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    public ListCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            return new ListCommand(PREDICATE_SHOW_ALL_PERSONS);
+        }
+
+        String[] sortVariable = trimmedArgs.split("\\s+");
+
+
+        if (sortVariable.length != 1) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+        }
+        switch(sortVariable[0].toLowerCase()) {
+        case "flag":
+            return new ListCommand(PREDICATE_SHOW_FLAGGED_PERSONS);
+        case "unflag":
+            return new ListCommand(PREDICATE_SHOW_FLAGGED_PERSONS.negate());
+        default:
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+
+        }
+    }
+}
