1e7b8c885b80ca186a36eff3b1fb849d2333a203
charmainehly <65525578+charmainehly@users.noreply.github.com>
2022-03-31 13:34:41 +0800

    Fefactor logging message in MainApp to TalentAssistant

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -183,7 +183,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting TalentAssistant " + MainApp.VERSION);
         model.deletePastInterviewsForInterviewList(LocalDateTime.now());
         ui.start(primaryStage);
     }
