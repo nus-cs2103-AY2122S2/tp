7c11303dbc7af55d57b6cb6d47c7bbad00310f9d
weiming <weiming.law@gmail.com>
2022-03-31 14:18:18 +0800

    Change AddressBookParser exception message

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -116,7 +117,7 @@ public class AddressBookParser {
             }
             return new ArchiveCommandParser("UNARCHIVE").parse(arguments);
         default:
-            throw new ParseException("MESSAGE_UNKNOWN_COMMAND");
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 
--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
@@ -49,11 +50,11 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
     }
 
-    //    @Test
-    //    public void execute_invalidCommandFormat_throwsParseException() {
-    //        String invalidCommand = "uicfhmowqewca";
-    //        assertParseException(invalidCommand, MESSAGE_UNKNOWN_COMMAND);
-    //    }
+    @Test
+    public void execute_invalidCommandFormat_throwsParseException() {
+        String invalidCommand = "uicfhmowqewca";
+        assertParseException(invalidCommand, MESSAGE_UNKNOWN_COMMAND);
+    }
 
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
