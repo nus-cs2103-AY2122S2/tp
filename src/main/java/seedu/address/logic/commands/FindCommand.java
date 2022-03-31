22d7820cb3d2fb7d3f0f9d69e84eb52fc33ba45a
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-04-01 01:42:44 +0800

    Update description find

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -21,6 +21,7 @@ public class FindCommand extends Command {
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Attributes can be accessed by adding prefixes before the keywords.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Note that [MORE_KEYWORDS] only apply for syntax n/ a/ t/ \n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "alice bob charlie "
             + "or " + COMMAND_WORD + " " + PREFIX_ACADEMIC_MAJOR + "Computer Science";
 
