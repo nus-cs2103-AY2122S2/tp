a83f7e356fbbaa66c7e79e3d088dd829cef089b9
Bryan Ong <bryan.o@u.nus.edu>
2022-03-31 14:36:38 +0800

    Valid user input for sort argument

--- src/main/java/seedu/address/logic/SortArgument.java
+++ src/main/java/seedu/address/logic/SortArgument.java
@@ -1,8 +1,13 @@
 package seedu.address.logic;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class SortArgument {
+
+    public static final String VALIDATION_REGEX = "asc|dsc";
+
+    public static final String MESSAGE_CONSTRAINTS = "Invalid sort argument! Sort argument is either asc or dsc";
 
     public final String argument;
 
@@ -13,7 +18,15 @@ public class SortArgument {
      */
     public SortArgument(String argument) {
         requireNonNull(argument);
+        checkArgument(isValidSortArgument(argument), MESSAGE_CONSTRAINTS);
         this.argument = argument;
+    }
+
+    /**
+     * Tests is a given string is a valid {@code SortArgument} according to the regular expression.
+     */
+    public static boolean isValidSortArgument(String test) {
+        return test.toLowerCase().matches(VALIDATION_REGEX);
     }
 
     @Override
