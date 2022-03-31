5c7a910a5a6beb9b918ef0e946c482b7820a81d9
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-04-01 00:22:09 +0800

    Update archiveBook to start with empty addressBook

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -87,7 +87,7 @@ public class MainApp extends Application {
                 logger.info("Data file not found. Will be starting with a sample AddressBook");
             }
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
-            initialArchiveData = archiveBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
+            initialArchiveData = archiveBookOptional.orElse(new AddressBook());
         } catch (DataConversionException e) {
             logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
             initialData = new AddressBook();
