4fddd4b5f8c1dc108634c6d9c50998d5a7de0327
ZF <zifoongl@gmail.com>
2022-03-31 19:37:32 +0800

    Checkstyle changes

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -15,14 +15,14 @@ public class HelpCommand extends Command {
     public static final String SHOWING_HELP_MESSAGE = "Opened help window, meanwhile here are the commands "
             + "available for your use! \n"
             + "1. add: Adds a new User\n"
-            + "     Format: add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS cc/CLASS s/STATUS [act/ACTIVITIES] " +
-            "[act/MORE ACTIVITIES]\n"
+            + "     Format: add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS cc/CLASS s/STATUS [act/ACTIVITIES] "
+            + "[act/MORE ACTIVITIES]\n"
             + "2. clear: Clears List\n"
             + "3. delete: Deletes a Student by their Index\n"
             + "     Format: delete INDEX\n"
             + "4. edit: Edit User\n"
-            + "     Format: edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [cc/CLASS] [s/STATUS] " +
-            "[act/ACTIVITIES] [act/MORE ACTIVITIES]\n"
+            + "     Format: edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [cc/CLASS] [s/STATUS] "
+            + "[act/ACTIVITIES] [act/MORE ACTIVITIES]\n"
             + "5. list: Lists all users\n"
             + "6. findactivity: Find Students by their Activity\n"
             + "     Format: findactivity ACTIVITY [MORE ACTIVITIES]\n"
