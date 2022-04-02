1e3529bcd9059255dee34e9e0fc8db98acc62377
ChanWeiJie <30587813+ChanWeiJie@users.noreply.github.com>
2022-04-03 01:08:01 +0800

    Update test cases.

--- src/test/java/manageezpz/model/task/DeadlineTest.java
+++ src/test/java/manageezpz/model/task/DeadlineTest.java
@@ -1,32 +1,71 @@
 package manageezpz.model.task;
 
+import manageezpz.model.person.Email;
+
+import static manageezpz.testutil.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
 
-    private final Deadline testDealine = new Deadline(new Description("testing"),
+    private final Deadline testDeadline = new Deadline(new Description("testing"),
             new Date("2022-11-08"), new Time("1800"));
+
     @Test
-    public void testGetters() {
-        assertEquals("testing", testDealine.getDescription().toString());
-        assertEquals("2022-11-08", testDealine.getDate().getParsedDate().toString());
-        assertEquals("18:00", testDealine.getTime().getParsedTime().toString());
-        assertEquals("deadline", testDealine.getType());
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Deadline(null));
     }
 
     @Test
-    public void testSetters() {
-        assertEquals(" ", testDealine.getStatusIcon());
-        testDealine.setTaskDone();
-        assertEquals("X", testDealine.getStatusIcon());
+    public void getDate_validDate_success() {
+        assertEquals("2022-11-08", testDeadline.getDate().getParsedDate().toString());
     }
+
+    @Test
+    public void getDescription_validDescription_success() {
+        assertEquals("testing", testDeadline.getDescription().toString());
+    }
+
+    @Test
+    public void getTime_validTime_success() {
+        assertEquals("18:00", testDeadline.getTime().getParsedTime().toString());
+    }
+
+    @Test
+    public void getType_validType_success() {
+        assertEquals("deadline", testDeadline.getType());
+    }
+
+    @Test
+    public void getDateTime_validDateTime_success() {
+        String expected = "by Nov 08 2022 6:00 PM";
+        assertEquals(expected, testDeadline.getDateTime());
+    }
+
+    @Test
+    public void setDescription_validDescription_success() {
+        testDeadline.setDescription(new Description("Finish Report"));
+        assertEquals(testDeadline.getDescription().toString(), "Finish Report");
+    }
+
+    @Test
+    public void setDate_validDate_success() {
+        testDeadline.setDate(new Date("2022-08-08"));
+        assertEquals(testDeadline.getDate().getDate(), "2022-08-08");
+    }
+
+    @Test
+    public void setTime_validTime_success() {
+        testDeadline.setTime(new Time("1800"));
+        assertEquals(testDeadline.getTime().getTime(), "1800");
+    }
+
 
     @Test
     public void testToString() {
         String expected = "[D][ ] testing (by: Nov 08 2022 6:00 PM)";
-        assertEquals(expected, testDealine.toString());
+        assertEquals(expected, testDeadline.toString());
     }
 
 }
