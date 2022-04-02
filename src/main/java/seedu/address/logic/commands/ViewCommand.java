348d0b4b019034db35271f86a61c4992a435e57e
LAPTOP-M47J04V8\loock <loo.ck5848@outlook.com>
2022-04-02 22:42:02 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -8,7 +8,6 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.patient.Nric;
-import seedu.address.model.patient.NricPredicate;
 
 /**
  * Lists all persons in the address book to the user.
