348ecc58c71089a3ac2155c3ccc9c4c63375f85e
yokester98 <yokeyue.loy@u.nus.edu>
2022-04-03 14:20:56 +0800

    Edit Tag to accept at most 20 characters.
    Edit tests to fit the new constraints of Name and Phone.

--- src/test/java/unibook/model/person/PhoneTest.java
+++ src/test/java/unibook/model/person/PhoneTest.java
@@ -34,8 +34,8 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
 
         // valid phone numbers
-        assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
+        assertTrue(Phone.isValidPhone("1234567")); // exactly 7 numbers
         assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+        assertTrue(Phone.isValidPhone("124293842033123")); // exactly 15 numbers
     }
 }
