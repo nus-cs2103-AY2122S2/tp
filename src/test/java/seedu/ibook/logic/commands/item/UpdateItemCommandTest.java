c5ef32303cbc887d6f5f8068b0775bda0a121c28
Lai Mei Tin <e0550504@u.nus.edu>
2022-04-05 00:40:57 +0800

    Add tests for UpdateItemCommand

--- src/test/java/seedu/ibook/logic/commands/item/UpdateItemCommandTest.java
+++ src/test/java/seedu/ibook/logic/commands/item/UpdateItemCommandTest.java
@@ -1,9 +1,18 @@
 package seedu.ibook.logic.commands.item;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.ibook.logic.commands.CommandTestUtil.ITEM_DESCRIPTOR_A;
+import static seedu.ibook.logic.commands.CommandTestUtil.ITEM_DESCRIPTOR_B;
+import static seedu.ibook.logic.commands.item.UpdateItemCommand.UpdateItemDescriptor;
+import static seedu.ibook.testutil.Assert.assertThrows;
+import static seedu.ibook.testutil.TypicalIndexes.INDEX_FIRST_PRODUCT_FIRST_ITEM;
+import static seedu.ibook.testutil.TypicalIndexes.INDEX_SECOND_PRODUCT_FIRST_ITEM;
 import static seedu.ibook.testutil.TypicalProducts.getTypicalIBookWithItems;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.ibook.logic.commands.ClearCommand;
 import seedu.ibook.model.Model;
 import seedu.ibook.model.ModelManager;
 import seedu.ibook.model.UserPrefs;
@@ -13,10 +22,39 @@ class UpdateItemCommandTest {
     private final Model model = new ModelManager(getTypicalIBookWithItems(), new UserPrefs());
 
     @Test
+    void constructor_nullArguments_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new UpdateItemCommand(INDEX_FIRST_PRODUCT_FIRST_ITEM, null));
+        assertThrows(NullPointerException.class, () -> new UpdateItemCommand(null, new UpdateItemDescriptor()));
+    }
+
+    @Test
     void execute() {
     }
 
     @Test
     void equals() {
+        final UpdateItemCommand standardCommand = new UpdateItemCommand(
+                INDEX_FIRST_PRODUCT_FIRST_ITEM, ITEM_DESCRIPTOR_A);
+
+        // same values -> returns true
+        UpdateItemDescriptor copyDescriptor = new UpdateItemDescriptor(ITEM_DESCRIPTOR_A);
+        UpdateItemCommand commandWithSameValues = new UpdateItemCommand(
+                INDEX_FIRST_PRODUCT_FIRST_ITEM, copyDescriptor);
+        assertEquals(standardCommand, commandWithSameValues);
+
+        // same object -> returns true
+        assertEquals(standardCommand, standardCommand);
+
+        // null -> returns false
+        assertNotEquals(null, standardCommand);
+
+        // different types -> returns false
+        assertNotEquals(standardCommand, new ClearCommand());
+
+        // different index -> returns false
+        assertNotEquals(standardCommand, new UpdateItemCommand(INDEX_SECOND_PRODUCT_FIRST_ITEM, ITEM_DESCRIPTOR_A));
+
+        // different descriptor -> returns false
+        assertNotEquals(standardCommand, new UpdateItemCommand(INDEX_FIRST_PRODUCT_FIRST_ITEM, ITEM_DESCRIPTOR_B));
     }
 }
