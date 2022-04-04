b9cee42c1416a53ba160b5bfcbb23d57b8cfd7b1
louisdavinlie <louisdavinlie@gmail.com>
2022-04-04 22:53:22 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/FindClassCodeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindClassCodeCommandTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.DANIEL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.GEORGE;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,7 +22,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.activity.ActivityContainsKeywordsPredicate;
 import seedu.address.model.person.ClassCodeContainsKeywordsPredicate;
 
 public class FindClassCodeCommandTest {
