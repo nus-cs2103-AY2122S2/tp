e3f90154d4a3b6316dc749946376763f34057c96
Benjykoh <benjaminkoh0909@gmail.com>
2022-04-05 02:38:29 +0800

    Add test cases for AssignCommandParser
    Added test cases for AssignCommandParser.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -8,6 +8,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM_HANDLE;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -40,6 +41,8 @@ public class CommandTestUtil {
     public static final String VALID_TELEGRAM_HANDLE_BOB = "bobbb";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
+    public static final String VALID_TASK_AMY = "Task A";
+    public static final String VALID_TASK_BOB = "Task B";
     public static final String VALID_INDEX = "1";
 
     public static final String ID_DESC_AMY = " " + PREFIX_ID + VALID_ID_AMY;
@@ -54,6 +57,8 @@ public class CommandTestUtil {
     public static final String TELEGRAM_HANDLE_DESC_BOB = " " + PREFIX_TELEGRAM_HANDLE + VALID_TELEGRAM_HANDLE_BOB;
     public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
     public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
+    public static final String TASK_DESC_AMY = " " + PREFIX_TASK_NAME + VALID_TASK_AMY;
+    public static final String TASK_DESC_BOB = " " + PREFIX_TASK_NAME + VALID_TASK_BOB;
     public static final String INDEX_DESC = " " + PREFIX_INDEX + VALID_INDEX;
 
     public static final String INVALID_ID_DESC = " " + PREFIX_ID + "A&&&&&&&Z"; // '&' not allowed in studentId
@@ -64,6 +69,7 @@ public class CommandTestUtil {
     public static final String INVALID_TELEGRAM_HANDLE_DESC = " " + PREFIX_TELEGRAM_HANDLE
             + "xxx"; // telegramHandles should have 5 to 32 characters
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
+    public static final String INVALID_TASK_DESC = " " + PREFIX_TASK_NAME + "*"; // '*' not allowed in task name
     public static final String INVALID_COMMAND_NAME = "poop";
     public static final String INVALID_INDEX = " " + PREFIX_INDEX + "index"; // alphabets and symbols not allowed
 
