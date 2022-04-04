df985b6d6d167daa5bdbf164560934f82fcd453a
hsiaojietng <hsiaojietng@gmail.com>
2022-04-04 17:17:37 +0800

    Provide more detailed success message when opening Statistics window

--- src/main/java/seedu/address/logic/commands/StatisticsWindowCommand.java
+++ src/main/java/seedu/address/logic/commands/StatisticsWindowCommand.java
@@ -5,8 +5,9 @@ import seedu.address.model.Model;
 
 public class StatisticsWindowCommand extends Command {
     public static final String COMMAND_WORD = "stats";
-    public static final String MESSAGE_STATISTICS_WINDOW_SUCCESS =
-            "Successfully opened Statistics window!";
+    public static final String MESSAGE_STATISTICS_WINDOW_SUCCESS = "Opened Statistics window!\n"
+        + "If the Statistics window is already open, close it and reopen it\n"
+        + "(via 'stats' command, 'Stats' button from the 'File' tab, or press 'F2' key) to refresh the data!\n";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Opens up the Statistics window "
             + "that displays a pie chart with data of buyers and sellers in an area.\n"
