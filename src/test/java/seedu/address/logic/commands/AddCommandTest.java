883fa6e84d4576eac8fa91962ca44273780e3058
Teo Hao Yu <thstoneyes@gmail.com>
2022-04-05 20:59:26 +0800

    Add more test cases

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -51,6 +51,24 @@ public class AddCommandTest {
     }
 
     @Test
+    public void execute_duplicatePetDifferentTag_throwsCommandException() {
+        Pet validPet = new PetBuilder().withTags("Poodle").build();
+        AddCommand addCommand = new AddCommand(new PetBuilder().build());
+        ModelStub modelStub = new ModelStubWithPet(validPet);
+
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PET, () -> addCommand.execute(modelStub));
+    }
+
+    @Test
+    public void execute_duplicatePetDifferentDiet_throwsCommandException() {
+        Pet validPet = new PetBuilder().withDiet("Vegetarian").build();
+        AddCommand addCommand = new AddCommand(new PetBuilder().build());
+        ModelStub modelStub = new ModelStubWithPet(validPet);
+
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PET, () -> addCommand.execute(modelStub));
+    }
+
+    @Test
     public void equals() {
         Pet alice = new PetBuilder().withName("Alice").build();
         Pet bob = new PetBuilder().withName("Bob").build();
