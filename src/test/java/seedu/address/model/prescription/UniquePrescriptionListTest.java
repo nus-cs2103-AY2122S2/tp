4ef59c148dad60b90e2ff0664da4555d8e2dba7e
LAPTOP-M47J04V8\loock <loo.ck5848@outlook.com>
2022-04-05 23:44:47 +0800

    Fix Checkstyles

--- src/test/java/seedu/address/model/prescription/UniquePrescriptionListTest.java
+++ src/test/java/seedu/address/model/prescription/UniquePrescriptionListTest.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.prescription.exceptions.DuplicatePrescriptionException;
