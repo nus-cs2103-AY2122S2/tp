d3ce4b86d3c54f880b88cf3ac0effd0fcedf4e4c
nnmoq <77339343+nnmoq@users.noreply.github.com>
2022-04-07 01:26:39 +0800

    Bugfix limit email to 100 chars

--- src/main/java/seedu/address/model/student/Email.java
+++ src/main/java/seedu/address/model/student/Email.java
@@ -22,7 +22,7 @@ public class Email {
             + "    - end with a domain label at least 2 characters long\n"
             + "    - have each domain label start and end with alphanumeric characters\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.\n"
-            + "3. Capped at 320 characters";
+            + "3. Capped at 100 characters";
 
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
@@ -58,7 +58,7 @@ public class Email {
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
-        return test.matches(VALIDATION_REGEX) && test.length() <= 320 || test.equals(EMPTY_EMAIL.value);
+        return test.matches(VALIDATION_REGEX) && test.length() <= 100 || test.equals(EMPTY_EMAIL.value);
     }
 
     @Override
