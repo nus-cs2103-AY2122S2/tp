9642c6ebca4c6c8c8229606e6932e7dadd24b10f
lowkaiwei98 <77216013+lowkaiwei98@users.noreply.github.com>
2022-04-06 17:15:21 +0800

    Update clear, sort test cases

--- src/main/java/seedu/trackermon/model/show/RatingComparator.java
+++ src/main/java/seedu/trackermon/model/show/RatingComparator.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.Comparator;
 
 /**
- * Compares the two {@code Show}'s {@code rating} and return an integer value based on the ordering.
+ * Compares the two {@code Show}'s {@code Rating} and return an integer value based on the ordering.
  */
 public class RatingComparator implements Comparator<Show> {
 
@@ -14,6 +14,11 @@ public class RatingComparator implements Comparator<Show> {
         requireNonNull(show);
         requireNonNull(other);
         return show.compareRating(other);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return (other instanceof RatingComparator);
     }
 
 }
