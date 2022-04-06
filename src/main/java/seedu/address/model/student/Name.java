13a05e2724ae669a69bc37d92554f736bfffb3ae
nnmoq <77339343+nnmoq@users.noreply.github.com>
2022-04-06 18:04:27 +0800

    Update constraint message style for Student Name

--- src/main/java/seedu/address/model/student/Name.java
+++ src/main/java/seedu/address/model/student/Name.java
@@ -11,7 +11,7 @@ public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain alphanumeric characters, spaces and are limited to 50 characters."
-                    + "Names should not contain only numbers and should not be blank";
+                    + "Names should not contain only numbers and should not be blank.";
 
     /*
      * The first character of the address must not be a whitespace,
