e7fc207bee46b94a47c3147e9b67c2086f806eb0
DESKTOP-6G3IIBM\User <arcornior>
2022-04-06 13:29:08 +0800

    Remove JsonFormat image Fix Checkstyle issue

--- src/main/java/seedu/trackermon/model/show/Comment.java
+++ src/main/java/seedu/trackermon/model/show/Comment.java
@@ -4,6 +4,11 @@ public class Comment {
 
     public final String comment;
 
+    /**
+     * Constructs a {@code Comment}.
+     *
+     * @param comment A valid comment.
+     */
     public Comment(String comment) {
         String emojiFilter = "[(\\u00a9|\\u00ae|[\\u2000-\\u3300]|\\ud83c[\\ud000-\\udfff]|"
                 + "\\ud83d[\\ud000-\\udfff]|\\ud83e[\\ud000-\\udfff])]";
