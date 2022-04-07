7cd270afa6b85382fa6e57aaadd0131012c48e7d
Wang Jun Hong <e0544005@u.nus.edu>
2022-04-07 16:23:39 +0800

    Add property test util, and typical properties

--- src/test/java/seedu/address/logic/parser/AddPtbCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddPtbCommandParserTest.java
@@ -1,11 +1,18 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.BuyerCommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.address.logic.commands.PropertyCommandsTestUtil.HOUSE_TYPE_DESC_1;
+import static seedu.address.logic.commands.PropertyCommandsTestUtil.LOCATION_DESC_1;
+import static seedu.address.logic.commands.PropertyCommandsTestUtil.PR_DESC_1;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.AddPropertyToBuyCommand;
+import seedu.address.model.property.PropertyToBuy;
+import seedu.address.testutil.PropertyToBuyBuilder;
 
 public class AddPtbCommandParserTest {
 
@@ -25,6 +32,14 @@ public class AddPtbCommandParserTest {
     @Test
     public void parse_weirdString_failure() {
         assertParseFailure(parser, "This is weird", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        PropertyToBuy validProperty = new PropertyToBuyBuilder().withHouse()
+
+        //assertParseSuccess(parser, PREAMBLE_WHITESPACE + HOUSE_TYPE_DESC_1 + LOCATION_DESC_1
+          //      + PR_DESC_1, new AddPropertyToBuyCommand())
     }
 
 }
