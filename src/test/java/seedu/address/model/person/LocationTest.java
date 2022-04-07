dcd28a29139445a91ac2ac4ae8620e801425acf8
Steven Chua <stevengkc714@protonmail.com>
2022-04-07 23:29:14 +0800

    Add tests for date, time, and location

--- src/test/java/seedu/address/model/person/LocationTest.java
+++ src/test/java/seedu/address/model/person/LocationTest.java
@@ -1,4 +1,37 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.entry.Location;
+
 public class LocationTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Location(null));
+    }
+
+    @Test
+    public void constructor_invalidLocation_throwsIllegalArgumentException() {
+        String invalidLocation = "";
+        assertThrows(IllegalArgumentException.class, () -> new Location(invalidLocation));
+    }
+
+    @Test
+    public void isValidLocation() {
+        // null location
+        assertThrows(NullPointerException.class, () -> Location.isValidLocation(null));
+
+        // invalid locations
+        assertFalse(Location.isValidLocation(""));
+        assertFalse(Location.isValidLocation(" "));
+
+        // valid locations
+        assertTrue(Location.isValidLocation("Company Headquarters at 129 St."));
+        assertTrue(Location.isValidLocation(">")); // one character
+        assertTrue(Location.isValidLocation("Down the Road; Across the Street; To the Left; Behind the Lamppost"));
+    }
 }
