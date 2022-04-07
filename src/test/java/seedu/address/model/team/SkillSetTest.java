6f166972415e5726210730e94e337a27ad42ac2e
alexteo98 <alexteo98@gmail.com>
2022-04-08 01:08:34 +0800

    Add tests to improve coverage
    Tests added for:
    1. `GithubUsername`
    2. `Person`
    3. `SkillSet`
    
    Remove unused code in `UniquePersonList`
    Add sample `Skill` and `SkillSet` to use in tests

--- src/test/java/seedu/address/model/team/SkillSetTest.java
+++ src/test/java/seedu/address/model/team/SkillSetTest.java
@@ -2,25 +2,31 @@ package seedu.address.model.team;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalSkills.SKILL_BASH_70;
+import static seedu.address.testutil.TypicalSkills.SKILL_C_1;
+import static seedu.address.testutil.TypicalSkills.SKILL_C_20;
+import static seedu.address.testutil.TypicalSkills.SKILL_JAVA_40;
+import static seedu.address.testutil.TypicalSkills.SKILL_JAVA_50;
+import static seedu.address.testutil.TypicalSkills.SKILL_LINUX_60;
+import static seedu.address.testutil.TypicalSkills.SKILL_PYTHON_30;
+import static seedu.address.testutil.TypicalSkills.getTypicalSkillSet;
 
 import org.junit.jupiter.api.Test;
 
 public class SkillSetTest {
 
-    private Skill java100 = new Skill("Java", 100);
-    private Skill java90 = new Skill("Java", 90);
-    private Skill python100 = new Skill("python", 100);
-    private Skill c1 = new Skill("C");
-    private SkillSet skillSet1 = new SkillSet();
+    private SkillSet skillSet1 = getTypicalSkillSet();
     private SkillSet skillSet2 = new SkillSet();
     private SkillSet skillSet3 = new SkillSet();
 
     @Test
     public void equals() {
-        skillSet1.add(java90);
-        skillSet2.add(python100);
-        skillSet3.add(java100);
+        //
+        skillSet1.add(SKILL_JAVA_40);
+        skillSet2.add(SKILL_PYTHON_30);
+        skillSet3.add(SKILL_JAVA_50);
         SkillSet copyOfSkillSet1 = new SkillSet(skillSet1);
         assertFalse(skillSet3.equals(skillSet1));
         assertTrue(copyOfSkillSet1.equals(skillSet1));
@@ -29,30 +35,33 @@ public class SkillSetTest {
 
     @Test
     public void addSkill() {
-        skillSet1.add(java100);
-        assertTrue(skillSet1.hasSkill(java90));
-        assertTrue(skillSet1.hasSkill(java100));
-        assertFalse(skillSet1.hasSkill(python100));
+        //
+        skillSet1.add(SKILL_BASH_70);
+        assertTrue(skillSet1.hasSkill(SKILL_BASH_70));
+        assertTrue(skillSet1.hasSkill(SKILL_JAVA_40));
+        assertFalse(skillSet1.hasSkill(SKILL_LINUX_60));
     }
 
     @Test
     public void addSkillSet() {
-        skillSet1.add(java100);
-        skillSet1.add(python100);
         skillSet2.addAll(skillSet1);
-        assertTrue(skillSet2.hasSkill(java100));
-        assertTrue(skillSet2.hasSkill(python100));
-        assertTrue(skillSet2.hasSkill(java90));
-        assertFalse(skillSet2.hasSkill(c1));
+        assertTrue(skillSet2.hasSkill(SKILL_JAVA_40));
+        assertTrue(skillSet2.hasSkill(SKILL_PYTHON_30));
+        assertTrue(skillSet2.hasSkill(SKILL_JAVA_50));
+        assertFalse(skillSet2.hasSkill(SKILL_BASH_70));
     }
 
     @Test
     public void getSkillProficiency() {
-        skillSet1.add(java90);
-        skillSet1.add(c1);
-        skillSet1.add(python100);
-        assertEquals(90, skillSet1.getSkillProficiency(java100));
-        assertEquals(90, skillSet1.getSkillProficiency(java90));
-        assertEquals(1, skillSet1.getSkillProficiency(c1));
+        assertEquals(40, skillSet1.getSkillProficiency(SKILL_JAVA_50));
+        assertEquals(40, skillSet1.getSkillProficiency(SKILL_JAVA_40));
+        assertEquals(20, skillSet1.getSkillProficiency(SKILL_C_1));
+        assertEquals(0, skillSet1.getSkillProficiency(SKILL_BASH_70));
+    }
+
+    @Test
+    public void getSkill() {
+        assertEquals(SKILL_C_20, skillSet1.getSkill(new Skill("C")));
+        assertNull(skillSet1.getSkill(SKILL_BASH_70));
     }
 }
