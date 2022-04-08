335e50e90e612b16d5e6b7110cca6127983a3d70
Steven Chua <stevengkc714@protonmail.com>
2022-04-08 16:55:27 +0800

    Add UnarchiveCommand tests

--- src/test/java/seedu/address/logic/commands/UnarchiveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnarchiveCommandTest.java
@@ -22,40 +22,40 @@ import seedu.address.model.entry.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
- * {@code ArchiveCommand}.
+ * {@code UnarchiveCommand}.
  */
-public class ArchiveCommandTest {
+public class UnarchiveCommandTest {
 
-    private Model model;
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @BeforeEach
     public void fixPersonsList() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        model.showPersonList(Model.PREDICATE_SHOW_UNARCHIVED_ONLY);
+        showArchivedPersons(model);
     }
 
     @Test
     public void execute_validIndexUnfilteredPersonList_success() throws CommandException {
         Person personToArchive = model.getFilteredPersonList().get(INDEX_FIRST_ENTRY.getZeroBased());
-        ArchiveCommand archiveCommand = new ArchiveCommand(INDEX_FIRST_ENTRY);
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(INDEX_FIRST_ENTRY);
 
-        String expectedMessage = String.format(ArchiveCommand.MESSAGE_ARCHIVE_ENTRY_SUCCESS, personToArchive);
+        String expectedMessage = String.format(UnarchiveCommand.MESSAGE_UNARCHIVE_ENTRY_SUCCESS, personToArchive);
 
         ModelManager expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        showUnarchivedPersons(expectedModel);
-        expectedModel.archiveEntry(INDEX_FIRST_ENTRY.getZeroBased(), true);
+        showArchivedPersons(expectedModel);
+        expectedModel.archiveEntry(INDEX_FIRST_ENTRY.getZeroBased(), false);
         expectedModel.showPersonList(Model.PREDICATE_SHOW_ALL);
-        showUnarchivedPersons(expectedModel);
+        showArchivedPersons(expectedModel);
 
-        assertCommandSuccess(archiveCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(unarchiveCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        ArchiveCommand archiveCommand = new ArchiveCommand(outOfBoundIndex);
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(outOfBoundIndex);
 
-        assertCommandFailure(archiveCommand, model, Messages.MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX);
+        assertCommandFailure(unarchiveCommand, model, Messages.MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX);
     }
 
     @Test
@@ -63,17 +63,17 @@ public class ArchiveCommandTest {
         showPersonAtIndex(model, INDEX_FIRST_ENTRY);
 
         Person personToArchive = model.getFilteredPersonList().get(INDEX_FIRST_ENTRY.getZeroBased());
-        ArchiveCommand archiveCommand = new ArchiveCommand(INDEX_FIRST_ENTRY);
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(INDEX_FIRST_ENTRY);
 
-        String expectedMessage = String.format(ArchiveCommand.MESSAGE_ARCHIVE_ENTRY_SUCCESS, personToArchive);
+        String expectedMessage = String.format(UnarchiveCommand.MESSAGE_UNARCHIVE_ENTRY_SUCCESS, personToArchive);
 
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        showUnarchivedPersons(expectedModel);
+        showArchivedPersons(expectedModel);
         showPersonAtIndex(expectedModel, INDEX_FIRST_ENTRY);
-        expectedModel.archiveEntry(INDEX_FIRST_ENTRY.getZeroBased(), true);
-        showUnarchivedPersons(expectedModel);
+        expectedModel.archiveEntry(INDEX_FIRST_ENTRY.getZeroBased(), false);
+        showArchivedPersons(expectedModel);
 
-        assertCommandSuccess(archiveCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(unarchiveCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
@@ -84,46 +84,47 @@ public class ArchiveCommandTest {
         // ensures that outOfBoundIndex is still in bounds of address book list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
 
-        ArchiveCommand archiveCommand = new ArchiveCommand(outOfBoundIndex);
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(outOfBoundIndex);
 
-        assertCommandFailure(archiveCommand, model, Messages.MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX);
+        assertCommandFailure(unarchiveCommand, model, Messages.MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX);
     }
 
     @Test
-    public void execute_alreadyArchivedPerson_throwsCommandException() {
-        model.showPersonList(Model.PREDICATE_SHOW_ARCHIVED_ONLY);
-        Person archivedPerson = model.getFilteredPersonList().get(0);
-        ArchiveCommand archiveCommand = new ArchiveCommand(INDEX_FIRST_ENTRY);
+    public void execute_alreadyUnarchivedPerson_throwsCommandException() {
+        model.showPersonList(Model.PREDICATE_SHOW_UNARCHIVED_ONLY);
+        Person unarchivedPerson = model.getFilteredPersonList().get(0);
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(INDEX_FIRST_ENTRY);
 
-        assertCommandFailure(archiveCommand, model, archivedPerson.getName().toString() + " is already Archived");
+        assertCommandFailure(unarchiveCommand, model,
+                unarchivedPerson.getName().toString() + " is already not Archived");
     }
 
     @Test
     public void equals() {
-        ArchiveCommand archiveFirstCommand = new ArchiveCommand(INDEX_FIRST_ENTRY);
-        ArchiveCommand archiveSecondCommand = new ArchiveCommand(INDEX_SECOND_ENTRY);
+        UnarchiveCommand unarchiveFirstCommand = new UnarchiveCommand(INDEX_FIRST_ENTRY);
+        UnarchiveCommand unarchiveSecondCommand = new UnarchiveCommand(INDEX_SECOND_ENTRY);
 
         // same object -> returns true
-        assertTrue(archiveFirstCommand.equals(archiveFirstCommand));
+        assertTrue(unarchiveFirstCommand.equals(unarchiveFirstCommand));
 
         // same values -> returns true
-        ArchiveCommand archiveFirstCommandCopy = new ArchiveCommand(INDEX_FIRST_ENTRY);
-        assertTrue(archiveFirstCommand.equals(archiveFirstCommandCopy));
+        UnarchiveCommand archiveFirstCommandCopy = new UnarchiveCommand(INDEX_FIRST_ENTRY);
+        assertTrue(unarchiveFirstCommand.equals(archiveFirstCommandCopy));
 
         // different types -> returns false
-        assertFalse(archiveFirstCommand.equals(1));
+        assertFalse(unarchiveFirstCommand.equals(1));
 
         // null -> returns false
-        assertFalse(archiveFirstCommand.equals(null));
+        assertFalse(unarchiveFirstCommand.equals(null));
 
         // different index -> returns false
-        assertFalse(archiveFirstCommand.equals(archiveSecondCommand));
+        assertFalse(unarchiveFirstCommand.equals(unarchiveSecondCommand));
     }
 
     /**
      * Updates {@code model}'s filtered list to show all unarchived persons
      */
-    private void showUnarchivedPersons(Model model) {
-        model.showPersonList(Model.PREDICATE_SHOW_UNARCHIVED_ONLY);
+    private void showArchivedPersons(Model model) {
+        model.showPersonList(Model.PREDICATE_SHOW_ARCHIVED_ONLY);
     }
 }
