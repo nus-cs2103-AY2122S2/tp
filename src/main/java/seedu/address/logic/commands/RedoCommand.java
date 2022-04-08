1d31ad5e4d39ce9469b8e9edce7672a235490d30
junlee1991 <77482097+junlee1991@users.noreply.github.com>
2022-04-08 10:13:23 +0800

    Update RedoCommand.java

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -15,10 +15,12 @@ public class RedoCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Redo success!";
     public static final String MESSAGE_FAILURE = "No more commands to redo!";
 
-    @Override
-	@@ -23,12 +23,12 @@ public CommandResult execute(Model model) throws CommandException {
-            throw new CommandException(MESSAGE_FAILURE);
-        }
+    	@Override
+	public CommandResult execute(Model model) throws CommandException {
+		if (!undoRedoStack.canRedo()) {
+		    throw new CommandException(MESSAGE_FAILURE);
+		}
+	}
 
         RedoableCommand command = undoRedoStack.popRedo();
         if (command instanceof SwitchCommand) {
