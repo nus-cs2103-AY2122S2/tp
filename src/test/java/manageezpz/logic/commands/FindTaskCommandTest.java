a3a2d907691ae7a657dc5f248f047aba319be37a
denniszedead <dennisng2255@gmail.com>
2022-04-08 16:26:22 +0800

    Fix code to follow coding standard

--- src/test/java/manageezpz/logic/commands/FindTaskCommandTest.java
+++ src/test/java/manageezpz/logic/commands/FindTaskCommandTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import manageezpz.model.Model;
