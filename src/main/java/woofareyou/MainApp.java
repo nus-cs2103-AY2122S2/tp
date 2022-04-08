fd5ac21f11a291ddcdc869487d65beb2c814b703
Teo Hao Yu <thstoneyes@gmail.com>
2022-04-08 21:14:29 +0800

    Refactor more AddressBook references to PetBook

--- src/main/java/woofareyou/MainApp.java
+++ src/main/java/woofareyou/MainApp.java
@@ -56,7 +56,7 @@ public class MainApp extends Application {
 
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
         UserPrefs userPrefs = initPrefs(userPrefsStorage);
-        PetBookStorage petBookStorage = new JsonPetBookStorage(userPrefs.getAddressBookFilePath());
+        PetBookStorage petBookStorage = new JsonPetBookStorage(userPrefs.getPetBookFilePath());
         storage = new StorageManager(petBookStorage, userPrefsStorage);
 
         initLogging(config);
--- src/main/java/woofareyou/model/ModelManager.java
+++ src/main/java/woofareyou/model/ModelManager.java
@@ -85,13 +85,13 @@ public class ModelManager implements Model {
 
     @Override
     public Path getAddressBookFilePath() {
-        return userPrefs.getAddressBookFilePath();
+        return userPrefs.getPetBookFilePath();
     }
 
     @Override
     public void setAddressBookFilePath(Path addressBookFilePath) {
         requireNonNull(addressBookFilePath);
-        userPrefs.setAddressBookFilePath(addressBookFilePath);
+        userPrefs.setPetBookFilePath(addressBookFilePath);
     }
 
     //=========== AddressBook ================================================================================
