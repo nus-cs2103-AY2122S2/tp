534bb985f567b650bd2843a4372918b2a465defd
junlee1991 <77482097+junlee1991@users.noreply.github.com>
2022-04-08 10:10:06 +0800

    Update RedoCommand.java

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -12,23 +12,20 @@ import seedu.address.model.Model;
 public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
-    public static final String MESSAGE_SUCCESS = "Redo Command : ";
+    public static final String MESSAGE_SUCCESS = "Redo success!";
     public static final String MESSAGE_FAILURE = "No more commands to redo!";
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireAllNonNull(model, undoRedoStack);
-
-        if (!undoRedoStack.canRedo()) {
+	@@ -23,12 +23,12 @@ public CommandResult execute(Model model) throws CommandException {
             throw new CommandException(MESSAGE_FAILURE);
         }
 
-        StackUndoRedo.ModelStack modelStack = undoRedoStack.popRedo();
-        if (modelStack.getRedoableCommand() instanceof SwitchCommand) {
+        RedoableCommand command = undoRedoStack.popRedo();
+        if (command instanceof SwitchCommand) {
             return new CommandResult(MESSAGE_SUCCESS, false, false, false, false, false, true);
         }
-        modelStack.getRedoableCommand().redo(model);
-        return new CommandResult(MESSAGE_SUCCESS + modelStack.getCommandText());
+        command.redo(model);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
     @Override
