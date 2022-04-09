5118ac10aa415adb5e0d52a0b6a115c56fd3f909
Ang Wei Jun <weijun.ang1999@gmail.com>
2022-04-09 15:36:40 +0800

    test

--- src/main/java/seedu/trackermon/model/show/Comment.java
+++ src/main/java/seedu/trackermon/model/show/Comment.java
@@ -9,17 +9,18 @@ public class Comment {
 
     public static final Integer MAX_COMMENT_LENGTH = 300000;
 
+    public static final String VALIDATION_REGEX_COMMENT = "[[\\p{Alnum}]*[\\p{Digit}]*[\\p{Punct}]*]{0,300000}";
+
     public final String comment;
 
     /**
      * Constructs a {@code Comment}.
-     *
      * @param comment A valid comment.
      */
     public Comment(String comment) {
         checkArgument(isValidComment(comment), MESSAGE_CONSTRAINTS);
         String emojiFilter = "[(\\u00a9|\\u00ae|[\\u2000-\\u3300]|\\ud83c[\\ud000-\\udfff]|"
-                        + "\\ud83d[\\ud000-\\udfff]|\\ud83e[\\ud000-\\udfff])]";
+            + "\\ud83d[\\ud000-\\udfff]|\\ud83e[\\ud000-\\udfff])]";
         this.comment = comment.replaceAll(emojiFilter, "");
     }
 
