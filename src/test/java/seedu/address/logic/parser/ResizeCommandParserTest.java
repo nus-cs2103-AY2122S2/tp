62c65a3959aef02b841b1551d0a5212c524b58e3
jiale-c <cheejiale@gmail.com>
2022-04-09 18:03:06 +0800

    Added more Boundary Value Analysis and Equivalence Partitioning testings

--- src/test/java/seedu/address/logic/parser/ResizeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ResizeCommandParserTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.ResizeCommand;
 
+/**
+ * These test cases will cover path variations when the command is input with both numbers and characters.
+ * Boundary Value Analysis will also be tested to ensure that the methods are bug free.
+ */
 public class ResizeCommandParserTest {
 
     private ResizeCommandParser parser = new ResizeCommandParser();
@@ -31,5 +35,29 @@ public class ResizeCommandParserTest {
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "test 1",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidBoundaryWindowSize_returnsResizeCommand() {
+        assertParseFailure(parser, "0",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "4",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidNumbersWindowSize_returnsResizeCommand() {
+        assertParseFailure(parser, "1.5",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "4.001",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "0.5",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResizeCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validBoundaryWindowSize_returnsResizeCommand() {
+        assertParseSuccess(parser, "1", new ResizeCommand((double) 1));
+        assertParseSuccess(parser, "3", new ResizeCommand((double) 1));
     }
 }
