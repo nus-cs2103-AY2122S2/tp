363a86f16281d27dfeaf458a90a75cd1237bfda3
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2022-04-09 20:27:59 +0800

    change name regex

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -9,15 +9,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Name {
 
-    public static final String MESSAGE_CONSTRAINTS = "Names should only contain alphanumeric characters"
-            + " and spaces (maximum 50 characters including spaces),"
+    public static final String MESSAGE_CONSTRAINTS = "Names can take any values,"
             + " and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "([\\p{Alnum}][\\p{Alnum} ]*)";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     public final String fullName;
 
