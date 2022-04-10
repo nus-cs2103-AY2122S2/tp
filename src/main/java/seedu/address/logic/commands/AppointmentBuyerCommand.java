fec83df15ddfd6e1cfbe171e8fc7e863f8ef8e01
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-04-11 00:25:15 +0800

    Change the message usage of appt-b and appt-s commands

--- src/main/java/seedu/address/logic/commands/AppointmentBuyerCommand.java
+++ src/main/java/seedu/address/logic/commands/AppointmentBuyerCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_BUYERS;
 
 import java.util.List;
@@ -19,8 +20,12 @@ public class AppointmentBuyerCommand extends Command {
     public static final String MESSAGE_ADD_APPOINTMENT_SUCCESS = "You have made an appointment with Buyer: %1$s";
     public static final String MESSAGE_DELETE_APPOINTMENT_SUCCESS = "You have removed an appointment with Buyer: %1$s";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": The appointment time should be specified to minutes\n"
-            + "with the format 'yyyy-MM-dd-HH-mm'\n"
+            + ": Makes an appointment with a buyer\n"
+            + "Parameters: INDEX " + PREFIX_APPOINTMENT
+            + "TIME\n"
+            + "Must include: INDEX time/ \n"
+            + "The appointment time should be specified to minutes "
+            + "with the format 'yyyy-MM-dd-HH-mm.'\n"
             + "Example:  " + COMMAND_WORD + " 1 "
             + "time/ 2022-05-04-14-00";
     public static final String MESSAGE_TIME_IN_PAST = "The time you entered is in the past\n"
