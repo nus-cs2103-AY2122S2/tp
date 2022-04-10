71a3a4e5397bbaa1ceb3512b2c2efa93169bbad5
goalfix <weihowe99@gmail.com>
2022-04-10 20:04:49 +0800

    Fix bug allowing candidate to have interview after hiring

--- src/main/java/seedu/address/logic/commands/interview/AddInterviewCommand.java
+++ src/main/java/seedu/address/logic/commands/interview/AddInterviewCommand.java
@@ -30,6 +30,7 @@ public class AddInterviewCommand extends AddCommand {
     public static final String MESSAGE_SUCCESS = "New interview added: %1$s";
     public static final String POSITION_NO_OPENING = "The position the applicant is interviewing for has no openings, "
             + "so an interview cannot be scheduled.";
+    public static final String APPLICANT_HAS_JOB = "The applicant already has a job, so an interview cannot be scheduled.";
 
     private final Index applicantIndex;
     private final LocalDateTime date;
@@ -56,6 +57,9 @@ public class AddInterviewCommand extends AddCommand {
             throw new CommandException(Messages.MESSAGE_INVALID_APPLICANT_DISPLAYED_INDEX);
         }
         Applicant applicantInInterview = lastShownApplicantList.get(applicantIndex.getZeroBased());
+        if (applicantInInterview.isHired()) {
+            throw new CommandException(APPLICANT_HAS_JOB);
+        }
 
         List<Position> lastShownPositionList = model.getFilteredPositionList();
         if (positionIndex.getZeroBased() >= lastShownPositionList.size()) {
