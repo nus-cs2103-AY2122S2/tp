bda779a7c94d4158ca8174b763484afac7549240
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-04-11 00:50:32 +0800

    Add tests for sort parser

--- src/test/java/seedu/address/logic/parser/SortBuyerCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortBuyerCommandParserTest.java
@@ -13,6 +13,9 @@ import seedu.address.logic.commands.SortBuyerCommand;
 
 public class SortBuyerCommandParserTest {
 
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortBuyerCommand.MESSAGE_USAGE);
+
     private SortBuyerCommandParser parser = new SortBuyerCommandParser();
 
 
@@ -26,7 +29,6 @@ public class SortBuyerCommandParserTest {
 
     @Test
     public void parse_allFieldsPresent_success() {
-        System.out.println(PREFIX_COMPARE + "name " + PREFIX_ORDER + "asc");
         assertParseSuccess(parser, " " + PREFIX_COMPARE + "name " + PREFIX_ORDER + "asc",
                 new SortBuyerCommand("name", "asc"));
         assertParseSuccess(parser, " " + PREFIX_COMPARE + "name " + PREFIX_ORDER + "desc",
@@ -35,6 +37,32 @@ public class SortBuyerCommandParserTest {
                 new SortBuyerCommand("time", "asc"));
         assertParseSuccess(parser, " " + PREFIX_COMPARE + "time " + PREFIX_ORDER + "desc",
                 new SortBuyerCommand("time", "desc"));
+    }
+
+    @Test
+    public void parse_compulsoryFieldMissing_failure() {
+        assertParseFailure(parser, PREFIX_COMPARE + "name", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, PREFIX_COMPARE + "time", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, PREFIX_ORDER + "asc", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, PREFIX_ORDER + "desc", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_wrongComparedItem_failure() {
+        assertParseFailure(parser, " " + PREFIX_COMPARE + "money " + PREFIX_ORDER + "asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        SortBuyerCommand.MESSAGE_NOT_SORTABLE));
+    }
+
+    @Test
+    public void parse_wrongOrder_failure() {
+        assertParseFailure(parser, " " + PREFIX_COMPARE + "time " + PREFIX_ORDER + "ascending",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        SortBuyerCommand.MESSAGE_INCORRECT_ORDER));
+
+        assertParseFailure(parser, " " + PREFIX_COMPARE + "name " + PREFIX_ORDER + "descending",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        SortBuyerCommand.MESSAGE_INCORRECT_ORDER));
     }
 
 
