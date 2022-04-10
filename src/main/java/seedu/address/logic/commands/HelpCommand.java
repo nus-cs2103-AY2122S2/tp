1b34dd5e521de0ca9507c6295818eb2633960b2a
Wee Jun Hong <weejunhong@gmail.com>
2022-04-10 21:58:25 +0800

    Fix UI and UG

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -15,26 +15,26 @@ public class HelpCommand extends Command {
     public static final String SHOWING_HELP_MESSAGE = "Opened help window, meanwhile here are the commands "
             + "available for your use! \n"
             + "1. add: Adds a new User\n"
-            + "     Format: add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS cc/CLASS s/STATUS [act/ACTIVITIES] "
+            + "    Format: add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS cc/CLASS s/STATUS [act/ACTIVITIES] "
             + "[act/MORE ACTIVITIES]\n"
             + "2. clear: Clears List\n"
             + "3. delete: Deletes a Student by their Index\n"
-            + "     Format: delete INDEX\n"
+            + "    Format: delete INDEX\n"
             + "4. edit: Edit User\n"
-            + "     Format: edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [cc/CLASS] [s/STATUS] "
+            + "    Format: edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [cc/CLASS] [s/STATUS] "
             + "[act/ACTIVITIES] [act/MORE ACTIVITIES]\n"
             + "5. list: Lists all users\n"
             + "6. findactivity: Find Students by their Activity\n"
-            + "     Format: findactivity ACTIVITY [MORE ACTIVITIES]\n"
+            + "    Format: findactivity ACTIVITY [MORE ACTIVITIES]\n"
             + "7. findstatus: Find Students by their COVID-19 Status\n"
-            + "     Format: findstatus STATUS\n"
+            + "    Format: findstatus STATUS\n"
             + "8. findclasscode: Find Students by their class\n"
-            + "     Format: findclasscode CLASS\n"
+            + "    Format: findclasscode CLASS\n"
             + "9. help: Displays User Guide and commands to use\n"
             + "10. exit: Exit Program";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(SHOWING_HELP_MESSAGE, true, false, false);
     }
 }
