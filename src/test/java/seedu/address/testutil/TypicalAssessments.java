47f4e378167b717b31f5fcd02622c320528b359b
lkldev <lkldev@outlook.com>
2022-04-10 23:24:43 +0800

    Update typical classes to create non static objects

--- src/test/java/seedu/address/testutil/TypicalAssessments.java
+++ src/test/java/seedu/address/testutil/TypicalAssessments.java
@@ -8,7 +8,6 @@ import static seedu.address.testutil.TypicalModules.CS2103T_WITH_STUDENT;
 import static seedu.address.testutil.TypicalModules.CS2105;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +19,7 @@ import seedu.address.model.assessment.Grade;
  */
 public class TypicalAssessments {
 
+    public static final int TOTAL_ASSESSMENTS = 11;
     public static final String PARTICIPATION = "Class Participation";
     public static final String PARTICIPATION_SIMPLE_NAME = "part";
     public static final String LAB1 = "Lab 1";
@@ -31,53 +31,76 @@ public class TypicalAssessments {
     public static final Grade GRADE2 = new Grade(2);
     public static final Grade GRADE3 = new Grade(3);
 
-    public static final Assessment CS2101_PARTICIPATION_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(PARTICIPATION).withSimpleName(PARTICIPATION_SIMPLE_NAME).withTaModule(CS2101)
-            .withAttempts(Map.of(TypicalStudents.BENSON, GRADE1)).build();
-    public static final Assessment CS2103T_NO_STUDENT_PARTICIPATION_NO_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(PARTICIPATION).withSimpleName(PARTICIPATION_SIMPLE_NAME).withTaModule(CS2103T).build();
-    public static final Assessment CS2103T_PARTICIPATION_NO_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(PARTICIPATION).withSimpleName(PARTICIPATION_SIMPLE_NAME)
-            .withTaModule(CS2103T_WITH_STUDENT).build();
-    public static final Assessment CS2030_LAB1_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB1).withSimpleName(LAB1_SIMPLE_NAME).withTaModule(CS2030)
-            .withAttempts(Map.of(TypicalStudents.ALICE, GRADE2, TypicalStudents.ELLE, GRADE2,
-                    TypicalStudents.FIONA, GRADE2)).build();
-    public static final Assessment CS2030_LAB2_WITH_SOME_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB2).withSimpleName(LAB2_SIMPLE_NAME).withTaModule(CS2030)
-            .withAttempts(Map.of(TypicalStudents.ALICE, GRADE2, TypicalStudents.FIONA, GRADE3)).build();
-    public static final Assessment CS2040_PARTICIPATION_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(PARTICIPATION).withTaModule(CS2040)
-            .withAttempts(Map.of(TypicalStudents.ALICE, GRADE1)).build();
-    public static final Assessment CS2040_LAB1_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB1).withTaModule(CS2040)
-            .withAttempts(Map.of(TypicalStudents.ALICE, GRADE1)).build();
-    public static final Assessment CS2040_LAB2_NO_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB2).withTaModule(CS2040).build();
-    public static final Assessment CS2105_LAB1_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB1).withTaModule(CS2105)
-            .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
-    public static final Assessment CS2105_LAB2_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB2).withTaModule(CS2105)
-            .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
-    public static final Assessment CS2105_LAB3_WITH_ATTEMPT = new AssessmentBuilder()
-            .withAssessmentName(LAB3).withTaModule(CS2105)
-            .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
+    public static final Assessment CS2101_PARTICIPATION_WITH_ATTEMPT = getAssessment(0);
+    public static final Assessment CS2103T_NO_STUDENT_PARTICIPATION_NO_ATTEMPT = getAssessment(1);
+    public static final Assessment CS2103T_PARTICIPATION_NO_ATTEMPT = getAssessment(2);
+    public static final Assessment CS2030_LAB1_WITH_ATTEMPT = getAssessment(3);
+    public static final Assessment CS2030_LAB2_WITH_SOME_ATTEMPT = getAssessment(4);
+    public static final Assessment CS2040_PARTICIPATION_WITH_ATTEMPT = getAssessment(5);
+    public static final Assessment CS2040_LAB1_WITH_ATTEMPT = getAssessment(6);
+    public static final Assessment CS2040_LAB2_NO_ATTEMPT = getAssessment(7);
+    public static final Assessment CS2105_LAB1_WITH_ATTEMPT = getAssessment(8);
+    public static final Assessment CS2105_LAB2_WITH_ATTEMPT = getAssessment(9);
+    public static final Assessment CS2105_LAB3_WITH_ATTEMPT = getAssessment(10);
 
     private TypicalAssessments() {} // prevents instantiation
 
     public static List<Assessment> getTypicalAssessments() {
-        return new ArrayList<>(Arrays.asList(
-                CS2101_PARTICIPATION_WITH_ATTEMPT,
-                CS2103T_NO_STUDENT_PARTICIPATION_NO_ATTEMPT,
-                CS2103T_PARTICIPATION_NO_ATTEMPT,
-                CS2030_LAB1_WITH_ATTEMPT,
-                CS2030_LAB2_WITH_SOME_ATTEMPT,
-                CS2040_PARTICIPATION_WITH_ATTEMPT,
-                CS2040_LAB1_WITH_ATTEMPT,
-                CS2040_LAB2_NO_ATTEMPT,
-                CS2105_LAB1_WITH_ATTEMPT,
-                CS2105_LAB2_WITH_ATTEMPT,
-                CS2105_LAB3_WITH_ATTEMPT));
+        List<Assessment> assessmentList = new ArrayList<>();
+        for (int i = 0; i < TOTAL_ASSESSMENTS; i++) {
+            assessmentList.add(getAssessment(i));
+        }
+        return assessmentList;
+    }
+
+    public static Assessment getAssessment(int i) {
+        switch (i) {
+        case 0:
+            return new AssessmentBuilder()
+                    .withAssessmentName(PARTICIPATION).withSimpleName(PARTICIPATION_SIMPLE_NAME).withTaModule(CS2101)
+                    .withAttempts(Map.of(TypicalStudents.BENSON, GRADE1)).build();
+        case 1:
+            return new AssessmentBuilder()
+                    .withAssessmentName(PARTICIPATION)
+                    .withSimpleName(PARTICIPATION_SIMPLE_NAME).withTaModule(CS2103T).build();
+        case 2:
+            return new AssessmentBuilder()
+                    .withAssessmentName(PARTICIPATION).withSimpleName(PARTICIPATION_SIMPLE_NAME)
+                    .withTaModule(CS2103T_WITH_STUDENT).build();
+        case 3:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB1).withSimpleName(LAB1_SIMPLE_NAME).withTaModule(CS2030)
+                    .withAttempts(Map.of(TypicalStudents.ALICE, GRADE2, TypicalStudents.ELLE, GRADE2,
+                            TypicalStudents.FIONA, GRADE2)).build();
+        case 4:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB2).withSimpleName(LAB2_SIMPLE_NAME).withTaModule(CS2030)
+                    .withAttempts(Map.of(TypicalStudents.ALICE, GRADE2, TypicalStudents.FIONA, GRADE3)).build();
+        case 5:
+            return new AssessmentBuilder()
+                    .withAssessmentName(PARTICIPATION).withTaModule(CS2040)
+                    .withAttempts(Map.of(TypicalStudents.ALICE, GRADE1)).build();
+        case 6:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB1).withTaModule(CS2040)
+                    .withAttempts(Map.of(TypicalStudents.ALICE, GRADE1)).build();
+        case 7:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB2).withTaModule(CS2040).build();
+        case 8:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB1).withTaModule(CS2105)
+                    .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
+        case 9:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB2).withTaModule(CS2105)
+                    .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
+        case 10:
+            return new AssessmentBuilder()
+                    .withAssessmentName(LAB3).withTaModule(CS2105)
+                    .withAttempts(Map.of(TypicalStudents.CARL, GRADE3, TypicalStudents.DANIEL, GRADE3)).build();
+        default:
+            return new AssessmentBuilder().build();
+        }
     }
 }
--- src/test/java/seedu/address/testutil/TypicalClassGroups.java
+++ src/test/java/seedu/address/testutil/TypicalClassGroups.java
@@ -1,7 +1,6 @@
 package seedu.address.testutil;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.classgroup.ClassGroup;
@@ -10,30 +9,55 @@ import seedu.address.model.classgroup.ClassGroup;
  * A utility class containing a list of {@code ClassGroup} objects to be used in tests.
  */
 public class TypicalClassGroups {
-    public static final ClassGroup CS2101G09 = new ClassGroupBuilder().withClassGroupId("G09")
-            .withClassGroupType("SECTIONAL").withModule(TypicalModules.CS2101)
-            .withUniqueStudentList(TypicalStudents.BENSON)
-            .withLessons(TypicalLessons.LESSON2).build();
-    public static final ClassGroup CS2101G10 = new ClassGroupBuilder().withClassGroupId("G10")
-            .withClassGroupType("SECTIONAL").withModule(TypicalModules.CS2101).build();
-    public static final ClassGroup CS2040B10A = new ClassGroupBuilder().withClassGroupId("B10A")
-            .withClassGroupType("LAB").withModule(TypicalModules.CS2040)
-            .withUniqueStudentList(TypicalStudents.ALICE)
-            .withLessons(TypicalLessons.LESSON1).build();
-    public static final ClassGroup CS2105T05 = new ClassGroupBuilder().withClassGroupId("T05")
-            .withClassGroupType("TUTORIAL").withModule(TypicalModules.CS2101)
-            .withUniqueStudentList(TypicalStudents.CARL, TypicalStudents.DANIEL)
-            .withLessons(TypicalLessons.LESSON12).build();
-    public static final ClassGroup CS2030R08 = new ClassGroupBuilder().withClassGroupId("R08")
-            .withClassGroupType("RECITATION").withModule(TypicalModules.CS2030)
-            .withUniqueStudentList(TypicalStudents.ALICE, TypicalStudents.ELLE, TypicalStudents.FIONA)
-            .withLessons(TypicalLessons.LESSON1, TypicalLessons.LESSON13).build();
-    public static final ClassGroup CS2103TT13 = new ClassGroupBuilder().withClassGroupId("T13")
-            .withClassGroupType("TUTORIAL").withModule(TypicalModules.CS2103T_WITH_STUDENT)
-            .withUniqueStudentList(TypicalStudents.getTypicalStudents()).build();
+    public static final int TOTAL_CLASS_GROUPS = 6;
+    public static final ClassGroup CS2101G09 = getClassGroup(0);
+    public static final ClassGroup CS2101G10 = getClassGroup(1);
+    public static final ClassGroup CS2040B10A = getClassGroup(2);
+    public static final ClassGroup CS2105T05 = getClassGroup(3);
+    public static final ClassGroup CS2030R08 = getClassGroup(4);
+    public static final ClassGroup CS2103TT13 = getClassGroup(5);
+
     private TypicalClassGroups() {} // prevents instantiation
 
     public static List<ClassGroup> getTypicalClassGroups() {
-        return new ArrayList<>(Arrays.asList(CS2101G09, CS2101G10, CS2040B10A, CS2105T05, CS2030R08));
+        List<ClassGroup> cgList = new ArrayList<>();
+        for (int i = 0; i < TOTAL_CLASS_GROUPS; i++) {
+            cgList.add(getClassGroup(i));
+        }
+        return cgList;
+    }
+
+    public static ClassGroup getClassGroup(int i) {
+        switch (i) {
+        case 0:
+            return new ClassGroupBuilder().withClassGroupId("G09")
+                .withClassGroupType("SECTIONAL").withModule(TypicalModules.CS2101)
+                .withUniqueStudentList(TypicalStudents.BENSON)
+                .withLessons(TypicalLessons.LESSON2).build();
+        case 1:
+            return new ClassGroupBuilder().withClassGroupId("G10")
+                .withClassGroupType("SECTIONAL").withModule(TypicalModules.CS2101).build();
+        case 2:
+            return new ClassGroupBuilder().withClassGroupId("B10A")
+                .withClassGroupType("LAB").withModule(TypicalModules.CS2040)
+                .withUniqueStudentList(TypicalStudents.ALICE)
+                .withLessons(TypicalLessons.LESSON1).build();
+        case 3:
+            return new ClassGroupBuilder().withClassGroupId("T05")
+                .withClassGroupType("TUTORIAL").withModule(TypicalModules.CS2101)
+                .withUniqueStudentList(TypicalStudents.CARL, TypicalStudents.DANIEL)
+                .withLessons(TypicalLessons.LESSON12).build();
+        case 4:
+            return new ClassGroupBuilder().withClassGroupId("R08")
+                .withClassGroupType("RECITATION").withModule(TypicalModules.CS2030)
+                .withUniqueStudentList(TypicalStudents.ALICE, TypicalStudents.ELLE, TypicalStudents.FIONA)
+                .withLessons(TypicalLessons.LESSON1, TypicalLessons.LESSON13).build();
+        case 5:
+            return new ClassGroupBuilder().withClassGroupId("T13")
+                .withClassGroupType("TUTORIAL").withModule(TypicalModules.CS2103T_WITH_STUDENT)
+                .withUniqueStudentList(TypicalStudents.getTypicalStudents()).build();
+        default:
+            return new ClassGroupBuilder().build();
+        }
     }
 }
