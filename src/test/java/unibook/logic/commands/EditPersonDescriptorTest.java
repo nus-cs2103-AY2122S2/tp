acee3435d96eec6d68b209edc9655b6780fa69ed
sophiien <sophieng1366@gmail.com>
2022-04-11 16:07:28 +0800

    Fix EditPersonDescriptorTest

--- src/test/java/unibook/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/unibook/logic/commands/EditPersonDescriptorTest.java
@@ -1,48 +1,29 @@
-//package unibook.logic.commands;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//
-//import org.junit.jupiter.api.Test;
-//import unibook.logic.commands.EditCommand.EditPersonDescriptor;
-//import unibook.testutil.EditPersonDescriptorBuilder;
-//
-//public class EditPersonDescriptorTest {
-//
-//    @Test
-//    public void equals() {
-//        // same values -> returns true
-//        EditPersonDescriptor descriptorWithSameValues = new EditPersonDescriptor(DESC_);
-//        assertTrue(DESC_A.equals(descriptorWithSameValues));
-//
-//        // same object -> returns true
-//        assertTrue(DESC_ALEX.equals(DESC_ALEX));
-//
-//        // null -> returns false
-//        assertFalse(DESC_ALEX.equals(null));
-//
-//        // different types -> returns false
-//        assertFalse(DESC_ALEX.equals(5));
-//
-//        // different values -> returns false
-//        assertFalse(DESC_ALEX.equals(DESC_BERNICE));
-//
-//        // different name -> returns false
-//        EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_ALEX)
-//              .withName(VALID_NAME_STUDENT_BERNICE).build();
-//        assertFalse(DESC_ALEX.equals(editedAmy));
-//
-//        // different phone -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_ALEX).withPhone(VALID_PHONE_STUDENT_BERNICE).build();
-//        assertFalse(DESC_ALEX.equals(editedAmy));
-//
-//        // different email -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_ALEX).withEmail(VALID_EMAIL_STUDENT_BERNICE).build();
-//        assertFalse(DESC_ALEX.equals(editedAmy));
-//
-//        // different tags -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_ALEX).withTags(VALID_TAG_FRIEND, VALID_TAG_ROOMMATE).build();
-//        assertFalse(DESC_ALEX.equals(editedAmy));
-//    }
-//}
+package unibook.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static unibook.logic.commands.CommandTestUtil.DESC_AMY;
+
+import org.junit.jupiter.api.Test;
+
+import unibook.logic.commands.EditCommand.EditPersonDescriptor;
+
+public class EditPersonDescriptorTest {
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        EditPersonDescriptor descriptorWithSameValues = new EditPersonDescriptor(DESC_AMY);
+        assertTrue(DESC_AMY.equals(descriptorWithSameValues));
+
+        // same object -> returns true
+        assertTrue(DESC_AMY.equals(DESC_AMY));
+
+        // null -> returns false
+        assertFalse(DESC_AMY.equals(null));
+
+        // different types -> returns false
+        assertFalse(DESC_AMY.equals(5));
+
+    }
+}
