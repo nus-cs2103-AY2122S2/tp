39e8997d2eb7e466f8aa5654fe4d3789e27c8d81
j4ck990 <jackng990@gmail.com>
2022-04-11 18:54:47 +0800

    Add HelpCommandParser testcase

--- src/test/java/seedu/address/logic/commands/CommandEnumTest.java
+++ src/test/java/seedu/address/logic/commands/CommandEnumTest.java
@@ -12,19 +12,20 @@ public class CommandEnumTest {
     @Test
     public void getMessageUsage_validCommand_success() {
         // valid commands
-        assertEquals(AddCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("add").getMessageUsage());
-        assertEquals(ClearCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("clear").getMessageUsage());
-        assertEquals(DeleteCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("delete").getMessageUsage());
-        assertEquals(EditCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("edit").getMessageUsage());
-        assertEquals(ExitCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("exit").getMessageUsage());
-        assertEquals(FindCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("find").getMessageUsage());
-        assertEquals(FlagCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("flag").getMessageUsage());
-        assertEquals(HelpCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("help").getMessageUsage());
-        assertEquals(ListCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("list").getMessageUsage());
-        assertEquals(MeetCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("meet").getMessageUsage());
-        assertEquals(RedoCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("redo").getMessageUsage());
-        assertEquals(SortCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("sort").getMessageUsage());
-        assertEquals(UndoCommand.MESSAGE_USAGE, Command.CommandEnum.valueOf("undo").getMessageUsage());
+        assertEquals(AddCommand.MESSAGE_USAGE, Command.CommandEnum.add.getMessageUsage());
+        assertEquals(ClearCommand.MESSAGE_USAGE, Command.CommandEnum.clear.getMessageUsage());
+        assertEquals(DeleteCommand.MESSAGE_USAGE, Command.CommandEnum.delete.getMessageUsage());
+        assertEquals(EditCommand.MESSAGE_USAGE, Command.CommandEnum.edit.getMessageUsage());
+        assertEquals(ExitCommand.MESSAGE_USAGE, Command.CommandEnum.exit.getMessageUsage());
+        assertEquals(FindCommand.MESSAGE_USAGE, Command.CommandEnum.find.getMessageUsage());
+        assertEquals(FlagCommand.MESSAGE_USAGE, Command.CommandEnum.flag.getMessageUsage());
+        assertEquals(FlagCommand.MESSAGE_USAGE, Command.CommandEnum.unflag.getMessageUsage());
+        assertEquals(HelpCommand.MESSAGE_USAGE, Command.CommandEnum.help.getMessageUsage());
+        assertEquals(ListCommand.MESSAGE_USAGE, Command.CommandEnum.list.getMessageUsage());
+        assertEquals(MeetCommand.MESSAGE_USAGE, Command.CommandEnum.meet.getMessageUsage());
+        assertEquals(RedoCommand.MESSAGE_USAGE, Command.CommandEnum.redo.getMessageUsage());
+        assertEquals(SortCommand.MESSAGE_USAGE, Command.CommandEnum.sort.getMessageUsage());
+        assertEquals(UndoCommand.MESSAGE_USAGE, Command.CommandEnum.undo.getMessageUsage());
     }
 
     @Test
@@ -42,6 +43,7 @@ public class CommandEnumTest {
         assertFalse(Command.CommandEnum.isValidCommand(" exit"));
         assertFalse(Command.CommandEnum.isValidCommand(" find"));
         assertFalse(Command.CommandEnum.isValidCommand(" flag"));
+        assertFalse(Command.CommandEnum.isValidCommand(" unflag"));
         assertFalse(Command.CommandEnum.isValidCommand(" help"));
         assertFalse(Command.CommandEnum.isValidCommand(" list"));
         assertFalse(Command.CommandEnum.isValidCommand(" meet"));
@@ -57,6 +59,7 @@ public class CommandEnumTest {
         assertTrue(Command.CommandEnum.isValidCommand("exit"));
         assertTrue(Command.CommandEnum.isValidCommand("find"));
         assertTrue(Command.CommandEnum.isValidCommand("flag"));
+        assertTrue(Command.CommandEnum.isValidCommand("unflag"));
         assertTrue(Command.CommandEnum.isValidCommand("help"));
         assertTrue(Command.CommandEnum.isValidCommand("list"));
         assertTrue(Command.CommandEnum.isValidCommand("meet"));
--- src/test/java/seedu/address/logic/parser/HelpCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/HelpCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.FlagCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.model.person.Flag;
@@ -22,6 +23,24 @@ public class HelpCommandParserTest {
     @Test
     public void parse_noArgs_returnsEmptyHelpCommand() {
         assertParseSuccess(parser, "", new HelpCommand());
+    }
+
+    @Test
+    public void parse_validArgs_returnsHelpCommand() {
+        assertParseSuccess(parser, "add", new HelpCommand(Command.CommandEnum.add));
+        assertParseSuccess(parser, "clear", new HelpCommand(Command.CommandEnum.clear));
+        assertParseSuccess(parser, "delete", new HelpCommand(Command.CommandEnum.delete));
+        assertParseSuccess(parser, "edit", new HelpCommand(Command.CommandEnum.edit));
+        assertParseSuccess(parser, "exit", new HelpCommand(Command.CommandEnum.exit));
+        assertParseSuccess(parser, "find", new HelpCommand(Command.CommandEnum.find));
+        assertParseSuccess(parser, "flag", new HelpCommand(Command.CommandEnum.flag));
+        assertParseSuccess(parser, "unflag", new HelpCommand(Command.CommandEnum.unflag));
+        assertParseSuccess(parser, "help", new HelpCommand(Command.CommandEnum.help));
+        assertParseSuccess(parser, "list", new HelpCommand(Command.CommandEnum.list));
+        assertParseSuccess(parser, "meet", new HelpCommand(Command.CommandEnum.meet));
+        assertParseSuccess(parser, "redo", new HelpCommand(Command.CommandEnum.redo));
+        assertParseSuccess(parser, "sort", new HelpCommand(Command.CommandEnum.sort));
+        assertParseSuccess(parser, "undo", new HelpCommand(Command.CommandEnum.undo));
     }
 
     @Test
