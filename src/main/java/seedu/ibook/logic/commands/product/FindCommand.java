43ce077923b4a5b20084f7917b4699de764a0b28
MechFroG88 <38691488+MechFroG88@users.noreply.github.com>
2022-04-11 14:25:31 +0800

    Fix styling issue

--- src/main/java/seedu/ibook/logic/commands/product/FindCommand.java
+++ src/main/java/seedu/ibook/logic/commands/product/FindCommand.java
@@ -30,7 +30,7 @@ public class FindCommand extends Command {
             + "Example 1: " + COMMAND_WORD + " " + PREFIX_NAME + " bottle " + PREFIX_PRICE + " 3.00\n"
             + "Example 2: " + COMMAND_WORD + " " + PREFIX_START_PRICE + " 3.00 " + PREFIX_END_PRICE + " 4.00";
 
-    public static final String MESSAGE_REQUIRE_START_END_PRICE = 
+    public static final String MESSAGE_REQUIRE_START_END_PRICE =
             "Please specify price only or specify both the start price and end price";
 
     private final List<AttributeFilter> filterList;
