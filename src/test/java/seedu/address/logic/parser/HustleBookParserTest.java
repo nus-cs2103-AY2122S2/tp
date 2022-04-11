45e474cb75476872048b3df697d57a10e18321a4
j4ck990 <jackng990@gmail.com>
2022-04-11 19:54:54 +0800

    Fix checkstyle imports

--- src/test/java/seedu/address/logic/parser/HustleBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/HustleBookParserTest.java
@@ -26,7 +26,6 @@ import seedu.address.logic.commands.FlagCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Flag;
