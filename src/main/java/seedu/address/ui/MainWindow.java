b0dceee07f9398787e68c0324de34cb239d04f31
Korochii <yanlong21@live.com.sg>
2022-04-11 12:00:50 +0800

    Removed unused code

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -175,10 +175,6 @@ public class MainWindow extends UiPart<Stage> {
         primaryStage.hide();
     }
 
-    public PersonListPanel getPersonListPanel() {
-        return personListPanel;
-    }
-
     /**
      * Executes the command and returns the result.
      *
