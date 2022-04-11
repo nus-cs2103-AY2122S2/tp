d0316acf3bd9c316d4e393e2b2a6e07dc9ae0bef
Wu Weiye <63904614+TeddYE@users.noreply.github.com>
2022-04-11 11:09:32 +0800

    Update ViewCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewCommandParserTest.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_ACTIVE_SCHEDULE_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ALL_SCHEDULE;
@@ -16,6 +14,8 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS_WITH_LINEUP;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SCHEDULES;
 import static seedu.address.model.Model.PREDICATE_SHOW_ARCHIVED_SCHEDULES;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
