1b5a8d1aa01317cf895cefc3d582541f64000a9b
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-04-11 13:32:06 +0800

    Add missing tests

---
+++ src/test/java/seedu/address/storage/JsonAdaptedGroupTest.java
@@ -0,0 +1,42 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedGroup.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalGroups.NUS_FINTECH_SOCIETY;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.group.GroupName;
+
+public class JsonAdaptedGroupTest {
+
+    private static final String INVALID_GROUP_NAME = "";
+
+    @Test
+    public void toModelType_validGroupName_returnsGroup() throws Exception {
+        JsonAdaptedGroup group = new JsonAdaptedGroup(NUS_FINTECH_SOCIETY);
+        assertEquals(NUS_FINTECH_SOCIETY, group.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidGroupName_throwsIllegalValueException() {
+        JsonAdaptedGroup group =
+                new JsonAdaptedGroup(INVALID_GROUP_NAME
+                        , new ArrayList<JsonAdaptedPerson>(), new ArrayList<JsonAdaptedTask>());
+        String expectedMessage = GroupName.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, group::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullGroupName_throwsIllegalValueException() {
+        JsonAdaptedGroup group = new JsonAdaptedGroup(null
+                , new ArrayList<JsonAdaptedPerson>(), new ArrayList<JsonAdaptedTask>());
+
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, GroupName.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, group::toModelType);
+    }
+}
---
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -0,0 +1,29 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.MEETING;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.task.TaskName;
+
+public class JsonAdaptedTaskTest {
+
+    private static final String INVALID_TASK_NAME = "";
+
+    @Test
+    public void toModelType_validTaskName_returnsTask() throws Exception {
+        JsonAdaptedTask task = new JsonAdaptedTask(MEETING);
+        assertEquals(MEETING, task.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidTaskName_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask(INVALID_TASK_NAME);
+        String expectedMessage = TaskName.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+}
